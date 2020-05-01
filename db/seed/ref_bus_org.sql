be14948f3b0457f7eccbc27cc101dbf64677d93c
Jason C. Leach <jason.leach@fullboar.ca>
2020-05-01 12:13:23 -0700

    Extend project profile data model

---
+++ db/seed/ref_bus_org.sql
@@ -0,0 +1,34 @@
+
+BEGIN TRANSACTION;
+
+INSERT INTO ref_bus_org (code, name) VALUES
+  ('AEST', 'Advanced Education, Skills & Training'),
+  ('AGRI', 'Agriculture'),
+  ('ALC', 'Agriculture Land Commission'),
+  ('AG', 'Attorney General'),
+  ('MCF', 'Children & Family Development'),
+  ('CITZ', 'Citizens''s Services'),
+  ('DBC', 'Destination BC'),
+  ('EMBC', 'Emergency Management BC'),
+  ('EAO', 'Environmental Assessment Office'),
+  ('EDUC', 'Education'),
+  ('EMPR', 'Energy, Mines & Petroleum Resources'),
+  ('ENV', 'Environment & Climate Change Strategy'),
+  ('FIN', 'Finance'),
+  ('FLNR', 'Forests, Lands, Natural Resource Operations & Rural Development'),
+  ('HLTH', 'Health'),
+  ('IRR', 'Indigenous Relations & Reconciliation'),
+  ('JEDC', 'Jobs, Economic Development & Competitiveness'),
+  ('LBR', 'Labour Policy & Legislation'),
+  ('LDB', 'BC Liquor Distribution Branch'),
+  ('MMHA', 'Mental Health & Addictions'),
+  ('MAH', 'Municipal Affairs & Housing'),
+  ('BCPC', 'Pension Corporation'),
+  ('PSA', 'Public Safety & Solicitor General & Emergency B.C.'),
+  ('SDPR', 'Social Development & Poverty Reduction'),
+  ('TCA', 'Tourism, Arts & Culture'),
+  ('TRAN', 'Transportation & Infrastructure');
+
+
+
+END TRANSACTION;
--- db/sql/0001.sql
+++ db/sql/0001.sql
@@ -8,12 +8,41 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
-CREATE TABLE IF NOT EXISTS profile (
+CREATE TABLE IF NOT EXISTS ref_category (
     id          serial PRIMARY KEY,
-    name        varchar(40) NOT NULL,
-    description varchar(512),
+    name        VARCHAR(32) NOT NULL,
+    description VARCHAR(256) NOT NULL,
     created_at  timestamp DEFAULT CURRENT_TIMESTAMP(3),
     updated_at  timestamp DEFAULT CURRENT_TIMESTAMP(3)
+);
+
+DROP TRIGGER IF EXISTS update_ref_category_changetimestamp on ref_category;
+CREATE TRIGGER update_ref_category_changetimestamp BEFORE UPDATE
+ON ref_category FOR EACH ROW EXECUTE PROCEDURE 
+update_changetimestamp_column();
+
+CREATE TABLE IF NOT EXISTS ref_bus_org (
+    code        varchar(4) PRIMARY KEY,
+    name        varchar(64) NOT NULL,
+    created_at  timestamp DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at  timestamp DEFAULT CURRENT_TIMESTAMP(3)
+);
+
+DROP TRIGGER IF EXISTS update_ref_bus_org_changetimestamp on ref_bus_org;
+CREATE TRIGGER update_ref_bus_org_changetimestamp BEFORE UPDATE
+ON ref_bus_org FOR EACH ROW EXECUTE PROCEDURE 
+update_changetimestamp_column();
+
+CREATE TABLE IF NOT EXISTS profile (
+    id              serial PRIMARY KEY,
+    name            varchar(40) NOT NULL,
+    description     varchar(512),
+    active          boolean NOT NULL DEFAULT true,
+    critical_system boolean NOT NULL DEFAULT false,
+    category        INTEGER REFERENCES ref_category(id) NOT NULL,
+    bus_org_id      VARCHAR(4) REFERENCES ref_bus_org(code) NOT NULL,
+    created_at      timestamp DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at      timestamp DEFAULT CURRENT_TIMESTAMP(3)
 );
 
 DROP TRIGGER IF EXISTS update_profile_changetimestamp on profile;
