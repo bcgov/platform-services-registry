3c68d5dd9436101c4fece8223f9c6f351cbdc522
Jason C. Leach <jason.leach@fullboar.ca>
2020-06-12 13:57:40 -0700

    Work on some common style components
    Co-authored-by: Patrick Simonian

---
+++ web/src/components/UI/shadowContainer.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import { Box, Flex } from 'rebass';
+
+
+const boxShadow = `box-shadow: 1px 3px 4px 4px #cbcbcb7d`;
+
+export const ShadowBox = styled(Box)`
+  ${boxShadow};
+`
+export const ShadowFlex = styled(Flex)`
+  ${boxShadow};
+`
