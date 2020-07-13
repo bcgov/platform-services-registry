e025d0cf915db97da5e28a4f56671e04913757dc
Jason C. Leach <jason.leach@fullboar.ca>
2020-07-13 11:34:22 -0700

    Add model for user profiles

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -2,17 +2,20 @@ import { Pool } from 'pg';
 import ContactModel from './model/contact';
 import NamespaceModel from './model/namespace';
 import ProfileModel from './model/profile';
+import UserProfileModel from './model/userprofile';
 
 export default class DataManager {
   pool: Pool;
   ProfileModel: ProfileModel;
   NamespaceModel: NamespaceModel;
   ContactModel: ContactModel;
+  UserProfileModel: UserProfileModel;
 
   constructor(pool: Pool) {
     this.pool = pool;
     this.ProfileModel = new ProfileModel(pool);
     this.NamespaceModel = new NamespaceModel(pool);
     this.ContactModel = new ContactModel(pool);
+    this.UserProfileModel = new UserProfileModel(pool);
   }
 }
