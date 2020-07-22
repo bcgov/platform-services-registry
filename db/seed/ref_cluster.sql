83de58fd4c66d204a3b4d9bed9c8f9bb5f9bd65a
Jason C. Leach <jason.leach@fullboar.ca>
2020-07-22 13:17:18 -0700

    Add proper cluster names

--- db/seed/ref_cluster.sql
+++ db/seed/ref_cluster.sql
@@ -1,9 +1,14 @@
 
 BEGIN TRANSACTION;
 
-INSERT INTO ref_cluster (name, disaster_recovery, on_prem, default) VALUES
-  ('kam', false, true, true),
-  ('cal', true, true, false),
-  ('az1', false, false, false);
+clab, klab, thetis (clabs temp name), silver, gold, golddr,and platform-services (aro)
+
+INSERT INTO ref_cluster (name, disaster_recovery, on_prem, on_hardware, default) VALUES
+  ('clab', false, true, false, false),
+  ('klab', false, true, false, false),
+  ('silver', false, true, true, true),
+  ('gold', false, true, true, false),
+  ('golddr', true, true, true, false),
+  ('aro', false, false, false, false);
 
 END TRANSACTION;
