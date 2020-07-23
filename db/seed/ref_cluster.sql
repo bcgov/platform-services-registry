77c21671fdd88ee95ee67d1f4b341a09e5dd177b
Jason C. Leach <jason.leach@fullboar.ca>
2020-07-23 13:10:22 -0700

    Fix seed sql

--- db/seed/ref_cluster.sql
+++ db/seed/ref_cluster.sql
@@ -1,9 +1,7 @@
 
 BEGIN TRANSACTION;
 
-clab, klab, thetis (clabs temp name), silver, gold, golddr,and platform-services (aro)
-
-INSERT INTO ref_cluster (name, disaster_recovery, on_prem, on_hardware, default) VALUES
+INSERT INTO ref_cluster (name, disaster_recovery, on_prem, on_hardware, "default") VALUES
   ('clab', false, true, false, false),
   ('klab', false, true, false, false),
   ('silver', false, true, true, true),
