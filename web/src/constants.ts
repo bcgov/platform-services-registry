56caf2c3a419df1e2df0bba8ec6769ff2b4b78a4
Jason C. Leach <jason.leach@fullboar.ca>
2020-07-24 15:33:39 -0700

    Revert manual override of sso url

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -17,7 +17,7 @@
 //
 
 export const SSO_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://sso-dev.pathfinder.gov.bc.ca'
+  ? '{{env "SSO_BASE_URL"}}'
   : 'https://sso-dev.pathfinder.gov.bc.ca';
 export const SSO_CLIENT_ID = 'registry-web';
 export const SSO_REALM_NAME = 'devhub';
