3ca0f30ef89ff2c085931159f18ad9e9b04220e8
Jason C. Leach <jason.leach@fullboar.ca>
2020-09-02 11:01:34 -0700

    Fix up keycloak initalization

--- web/src/keycloak.ts
+++ web/src/keycloak.ts
@@ -1,11 +1,12 @@
 import Keycloak from 'keycloak-js';
 import { SSO_BASE_URL, SSO_CLIENT_ID, SSO_REALM_NAME } from './constants';
 
-// @ts-ignore
-export const keycloak: any = new Keycloak({
+const keycloak: any = Keycloak({
   url: `${SSO_BASE_URL}/auth`,
   realm: SSO_REALM_NAME,
   clientId: SSO_CLIENT_ID,
 });
+
+// keycloak.init({ checkLoginIframe: true });
 
 export default keycloak;
