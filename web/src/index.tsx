eb8e5b617ba28e6ede3f073bb2fb60adb5838975
Jason C. Leach <jason.leach@fullboar.ca>
2020-09-01 17:00:50 -0700

    Testing 4

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,6 +5,7 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import typography from './typography';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Global
