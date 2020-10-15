a75f74149c2cf95067a391fa3dc38da6b2b4306e
Jason C. Leach <jason.leach@fullboar.ca>
2020-10-15 11:50:23 -0700

    Fix issue in sql cmds

--- db/sql/0004.sql
+++ db/sql/0004.sql
@@ -1,8 +1,7 @@
 BEGIN TRANSACTION;
 
-ALTER TABLE profile
-RENAME COLUMN identity_management_site_minder TO idm_site_minder,
-RENAME COLUMN identity_management_keycloak TO idm_keycloak,
-RENAME COLUMN identity_management_active_dir to idm_active_dir;
+ALTER TABLE profile RENAME COLUMN identity_management_site_minder TO idm_site_minder;
+ALTER TABLE profile RENAME COLUMN identity_management_keycloak TO idm_keycloak;
+ALTER TABLE profile RENAME COLUMN identity_management_active_dir to idm_active_dir;
 
 END TRANSACTION;
