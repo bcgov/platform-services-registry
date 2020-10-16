568238c42feb56ab9880a0ae4753d2f3936d46e1
yhfreeman <yhfreeman12@gmail.com>
2020-10-16 14:33:47 -0700

    add form validation helpers

---
+++ web/src/utils/useValidator.tsx
@@ -0,0 +1,123 @@
+//
+// Copyright © 2020 Province of British Columbia
+//
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import validate from 'validate.js';
+
+const schema = {
+  email: {
+    presence: { allowEmpty: false, message: 'Required' },
+    email: {
+      message: 'Invalid format'
+    },
+    length: {
+      maximum: 32,
+      tooLong: 'Max 32 characters'
+    }
+  },
+  profileName: {
+    presence: { allowEmpty: false, message: 'Required' },
+    length: {
+      maximum: 40,
+      tooLong: 'Max 40 characters'
+    },
+    format: { pattern: '^[a-zA-Z][A-Za-z0-9]+', flags: 'i', message: 'Must be alphanumetic starting with a letter' }
+  },
+  profileDescription: {
+    presence: { allowEmpty: false, message: 'Required' },
+    length: {
+      maximum: 512,
+      tooLong: 'Max 512 chars'
+    },
+    format: { pattern: `^(?! ).+[A-Za-z0-9 ,)(.!?"']+`, flags: 'i', message: 'Invalid format' }
+  },
+  componenentOthers: {
+    length: {
+      maximum: 512,
+      tooLong: 'Max 512 chars'
+    },
+    format: { pattern: `^(?! ).+[A-Za-z0-9 ,)(.!?"']+`, flags: 'i', message: 'Invalid format' }
+  },
+  name: {
+    presence: { allowEmpty: false, message: 'Required' },
+    length: {
+      maximum: 32,
+      tooLong: 'Max 32 characters'
+    },
+    format: { pattern: '[A-Za-z]+', flags: 'i', message: 'Must be alphabetic' }
+  },
+  githubName: {
+    presence: { allowEmpty: false, message: 'Required' },
+    length: {
+      maximum: 32,
+      tooLong: 'Max 32 chars'
+    },
+    format: { pattern: `[A-Za-z0-9-]+`, flags: 'i', message: 'Invalid Github account format' }
+  }
+};
+
+// TODO: the code below can be DRYer
+export default function useValidator() {
+  const mustBeValidEmail = (value: any) => {
+    const errors = validate({ email: value }, schema, { fullMessages: false });
+    if (errors && errors.email) {
+      return errors.email[0];
+    }
+  };
+
+  const mustBeValidProfileName = (value: any) => {
+    const errors = validate({ profileName: value }, schema, { fullMessages: false });
+    if (errors && errors.profileName) {
+      return errors.profileName[0];
+    }
+  };
+
+  const mustBeValidProfileDescription = (value: any) => {
+    const errors = validate({ profileDescription: value }, schema, { fullMessages: false });
+    if (errors && errors.profileDescription) {
+      return errors.profileDescription[0];
+    }
+  };
+
+  const mustBeValidName = (value: any) => {
+    const errors = validate({ name: value }, schema, { fullMessages: false });
+    if (errors && errors.name) {
+      return errors.name[0];
+    }
+  };
+
+  const mustBeValidGithubName = (value: any) => {
+    const errors = validate({ githubName: value }, schema, { fullMessages: false });
+    if (errors && errors.githubName) {
+      return errors.githubName[0];
+    }
+  };
+
+  const mustBeValidComponentOthers = (value: any) => {
+    const errors = validate({ componenentOthers: value }, schema, { fullMessages: false });
+    if (errors && errors.componenentOthers) {
+      return errors.componenentOthers[0];
+    }
+  };
+
+  return {
+    mustBeValidEmail,
+    mustBeValidProfileName,
+    mustBeValidProfileDescription,
+    mustBeValidName,
+    mustBeValidGithubName,
+    mustBeValidComponentOthers
+  };
+};
