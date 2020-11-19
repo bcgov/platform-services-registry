3bdc193206e574b07111758ea1fe11fc5983f50b
Freeman <yfreeman@nem020062.hitronhub.home>
2020-11-18 18:28:58 -0800

    update unit test to reflect changes of encapsulating the entries

--- web/src/__tests__/transformDataHelper.test.tsx
+++ web/src/__tests__/transformDataHelper.test.tsx
@@ -69,7 +69,7 @@ describe("test the helper function getProfileContacts", () => {
 describe("test the helper function transformJsonToCsv", () => {
   test("should return correct csv", () => {
     const stubJson = profiles;
-    const result = 'id,name,description,prioritySystem,criticalSystem,createdAt,updatedAt,userId\r\n2,EPIC,Hello World,false,true,28-10-2020 03:00,28-10-2020 03:00,4\r\n1,Health Gateway,null,false,false,28-04-2020 00:00,28-04-2020 00:00,4\r\n3,Mines Digital Services,This is some description,false,false,18-04-2020 10:10,18-04-2020 10:10\r\n';
+    const result = 'id,name,description,prioritySystem,criticalSystem,createdAt,updatedAt,userId\r\n"2","EPIC","Hello World","false","true","28-10-2020 03:00","28-10-2020 03:00","4"\r\n"1","Health Gateway","null","false","false","28-04-2020 00:00","28-04-2020 00:00","4"\r\n"3","Mines Digital Services","This is some description","false","false","18-04-2020 10:10","18-04-2020 10:10"\r\n';
     expect(transformJsonToCsv(stubJson)).toEqual(result);
   });
 });
