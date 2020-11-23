ed0f43ad0a2770be68fd79e87d0b7ffb959fc978
Shaun Bathgate <shaun.bathgate@gov.bc.ca>
2020-11-23 15:22:31 -0800

    Add project overview, profile and contact edit routes

--- web/src/views/ProfileEditable.tsx
+++ web/src/views/ProfileEditable.tsx
@@ -17,7 +17,9 @@
 import React, { useEffect, useState } from 'react';
 import { Link as RouterLink, Redirect } from 'react-router-dom';
 import { Box, Text } from 'rebass';
+import ContactCard from '../components/ContactCard';
 import Icon from '../components/Icon';
+import ProfileDetailCard from '../components/ProfileDetailCard';
 import ProfileEditableContact from '../components/ProfileEditableContact';
 import ProfileEditableProject from '../components/ProfileEditableProject';
 import ProfileEditableQuota from '../components/ProfileEditableQuota';
@@ -25,6 +27,7 @@ import { ShadowBox } from '../components/UI/shadowContainer';
 import { PROFILE_VIEW_NAMES, RESPONSE_STATUS_CODE, ROUTE_PATHS } from '../constants';
 import theme from '../theme';
 import { promptErrToastWithText } from '../utils/promptToastHelper';
+import { getProfileContacts } from '../utils/transformDataHelper';
 import useRegistryApi from '../utils/useRegistryApi';
 
 interface IProfileEditProps {
@@ -39,16 +42,17 @@ const ProfileEdit: React.FC<IProfileEditProps> = (props) => {
 
     const [initialRender, setInitialRender] = useState(true);
     const [unauthorizedToAccess, setUnauthorizedToAccess] = useState(false);
-    const [profileJson, setProfileJson] = useState({});
+    const [profileJson, setProfileJson] = useState<any>({});
 
     useEffect(() => {
         async function wrap() {
             openBackdropCB();
             try {
                 const response = await api.getProfileByProfileId(profileId);
+                const contactDetails = await api.getContactsByProfileId(profileId);
+
+                response.data = { ...response.data, ...getProfileContacts(contactDetails.data) };
                 setProfileJson(response.data);
-                // TODO: remove console log and start passing profilejson to each profile view
-                console.log(profileJson);
             } catch (err) {
                 if (err.response && err.response.status && err.response.status === RESPONSE_STATUS_CODE.UNAUTHORIZED) {
                     setUnauthorizedToAccess(true);
@@ -74,7 +78,45 @@ const ProfileEdit: React.FC<IProfileEditProps> = (props) => {
 
     // here we divide profile-related views based on query param
     if (viewName === PROFILE_VIEW_NAMES.OVERVIEW) {
-        return (<div>profile overview</div>);
+        return (
+            <>
+            <Box sx={{
+                display: 'grid',
+                gridGap: 4
+              }}>
+                <ShadowBox p={5} style={{ position: 'relative' }}>
+                    <Text as="h1">
+                    {profileJson.name}
+                    </Text>
+                    <Box>
+                        <Box p={3} mt={4} bg={theme.colors.bcblue} style={{ position: 'relative' }}>
+                            <Text as="h3" color={theme.colors.contrast}>
+                                Project Information
+                                <RouterLink className='misc-class-m-dropdown-link' to={`/profile/${profileId}/project`}>
+                                    <Icon hover color={'contrast'} name={'edit'} width={1} height={1} />
+                                </RouterLink>
+                            </Text>
+                        </Box>
+                        <ShadowBox p={3} key={profileJson.id} style={{ position: 'relative' }}>
+                            <ProfileDetailCard title={profileJson.name} textBody={profileJson.description} ministry={profileJson.busOrgId} />
+                        </ShadowBox> 
+                    </Box>
+                    <Box>
+                        <ShadowBox p={3} mt={4} bg={theme.colors.bcblue} style={{ position: 'relative' }}>
+                            <Text as="h3" color={theme.colors.contrast}>Contact Information
+                                <RouterLink className='misc-class-m-dropdown-link' to={`/profile/${profileId}/contact`}>
+                                    <Icon hover color={'contrast'} name={'edit'} width={1} height={1} />
+                                </RouterLink>
+                            </Text>
+                        </ShadowBox>
+                        <ShadowBox p={3} key={profileJson.id} style={{ position: 'relative' }}>
+                            <ContactCard POName={profileJson.POName} POEmail={profileJson.POEmail} TCName={profileJson.TCName} TCEmail={profileJson.TCEmail} />
+                        </ShadowBox> 
+                    </Box>
+                </ShadowBox>
+            </Box>
+            </>
+        );
     } else {
         return (
             <>
