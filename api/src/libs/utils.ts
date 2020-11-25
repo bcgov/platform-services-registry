5e0afb1bd950c203a3f9a183a28bff6f267c01c1
Freeman <yfreeman@nem020062.hitronhub.home>
2020-11-25 11:57:39 -0800

    add util fns for quota-edit-related controller logics

--- api/src/libs/utils.ts
+++ api/src/libs/utils.ts
@@ -16,10 +16,14 @@
 // Created by Jason Leach on 2020-05-14.
 //
 
-
-import { errorWithCode } from '@bcgov/common-nodejs-utils';
+import { errorWithCode, logger } from '@bcgov/common-nodejs-utils';
 import { difference, isEmpty, isUndefined } from 'lodash';
-import { USER_ROLES } from '../constants';
+import { quotaSizeNames, USER_ROLES } from '../constants';
+import DataManager from '../db';
+import { ClusterNamespace } from '../db/model/namespace';
+import shared from '../libs/shared';
+
+const dm = new DataManager(shared.pgPool);
 
 export const validateObjProps = (fields: string[], pojo: object): Error | undefined => {
   const diff = difference(fields, Object.keys(pojo));
@@ -47,7 +51,7 @@ export const validateObjProps = (fields: string[], pojo: object): Error | undefi
   return;
 }
 
-export const isNotAuthorized = (results: any, user: any): Error | undefined  => {
+export const isNotAuthorized = (results: any, user: any): Error | undefined => {
 
   if (!(user.id === results.userId || user.roles.includes(USER_ROLES.ADMINISTRATOR))) {
     return errorWithCode('Unauthorized Access', 401);
@@ -55,3 +59,101 @@ export const isNotAuthorized = (results: any, user: any): Error | undefined  => 
 
   return;
 }
+
+// TODO:(yf) should this method be moved somewhere else?
+export const getQuotaOptions = async (clusterNamespace: ClusterNamespace): Promise<any> =>
+  new Promise(async (resolve, reject) => {
+    const { RequestModel } = dm;
+    const { namespaceId, clusterId, quotaCpu, quotaMemory, quotaStorage } = clusterNamespace;
+    let quotaOptionsObj = {
+      namespaceId,
+      clusterId,
+      quotaCpu: new Array,
+      quotaMemory: new Array,
+      quotaStorage: new Array
+    }
+
+    try {
+      const existingRequest = await RequestModel.findForClusterNamespace(clusterNamespace.id);
+
+      if (!existingRequest) {
+        const spec = { quotaCpu, quotaMemory, quotaStorage };
+        // TODO:(yf) think of a way to use constants
+        for (let i in spec) {
+          const currentSize = spec[i];
+          switch (currentSize) {
+            case 'small': {
+              quotaOptionsObj[i].push('small', 'medium');
+              break;
+            }
+            case 'medium': {
+              quotaOptionsObj[i].push('small', 'medium', 'large');
+              break;
+            }
+            case 'large': {
+              quotaOptionsObj[i].push('small', 'large', 'medium');
+              break;
+            }
+          }
+        }
+      }
+
+      resolve(quotaOptionsObj);
+    } catch (err) {
+      const message = `Unable to provide quota options for cluster namespce quota request`;
+      logger.error(`${message}, err = ${err.message}`);
+      resolve(quotaOptionsObj);
+    }
+  });
+
+export const validateQuotaRequestBody = (quotaOptions: any, body: any): Error | undefined => {
+  try {
+    const spec = ['quotaCpu', 'quotaMemory', 'quotaStorage'];
+    for (let i = 0; i < quotaOptions.length; i++) {
+      spec.forEach(specName => {
+        const requestedSize: string = body[i][specName];
+        if (!quotaSizeNames.includes(requestedSize)) {
+          throw new Error;
+        }
+        const allowedSizes: string[] = quotaOptions[i][specName];
+        if (!allowedSizes.includes(requestedSize)) {
+          throw new Error;
+        }
+      });
+    }
+    return;
+  } catch (err) {
+    return errorWithCode('Invalid quota request body', 400);
+  }
+};
+
+// TODO:(yf) add error handling for fn params
+export const MergeQuotasIntoNamespaces = (quotaBody: any, namespacesForQuotaEdit: any): Error | object[] => {
+  const merged = [];
+  try {
+    for (let i = 0; i < namespacesForQuotaEdit.length; i++) {
+      const { namespaceId } = namespacesForQuotaEdit[i];
+
+      const obj = {
+        namespaceId,
+        name: namespacesForQuotaEdit[i].name,
+        clusters: [{
+          clusterId: namespacesForQuotaEdit[i].clusters.clusterId,
+          name: namespacesForQuotaEdit[i].clusters.name,
+          provisioned: namespacesForQuotaEdit[i].clusters.provisioned,
+          quotas: {
+            cpu: quotaBody[i].quotaCpu,
+            memory: quotaBody[i].quotaMemory,
+            storage: quotaBody[i].quotaStorage
+          }
+        }]
+      };
+
+      // @ts-ignore
+      merged.push(obj);
+    }
+    return merged;
+  } catch (err) {
+    return errorWithCode('Unable to merge quotas into namespaces', 500);
+  }
+};
