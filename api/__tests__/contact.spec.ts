0d00ba6c71ff83eae1efd5e6a2f83f4e244f34ca
Shaun Bathgate <shaun.bathgate@gov.bc.ca>
2020-12-22 13:38:10 -0800

    Add contact edit processing test

--- api/__tests__/contact.spec.ts
+++ api/__tests__/contact.spec.ts
@@ -17,17 +17,20 @@
 import fs from 'fs';
 import path from 'path';
 import { Pool } from 'pg';
-import { requestContactEdit, updateContact } from '../src/controllers/contact';
+import { processContactEdit, requestContactEdit, updateContact } from '../src/controllers/contact';
 import FauxExpress from './src/fauxexpress';
 
 const p0 = path.join(__dirname, 'fixtures/select-profile-contacts.json');
 const selectContact = JSON.parse(fs.readFileSync(p0, 'utf8'));
 
+const p2 = path.join(__dirname, 'fixtures/select-contact-edit-request.json');
+const selectContactEditRequest = JSON.parse(fs.readFileSync(p2, 'utf8'));
+
 const client = new Pool().connect();
 
 jest.mock('../src/libs/fulfillment', () => {
-  const p3 = path.join(__dirname, 'fixtures/provisioning-context.json');
-  const natsContext = JSON.parse(fs.readFileSync(p3, 'utf8'));
+  const p1 = path.join(__dirname, 'fixtures/provisioning-context.json');
+  const natsContext = JSON.parse(fs.readFileSync(p1, 'utf8'));
   const natsSubject = 'edit';
   return {
     fulfillNamespaceEdit: jest.fn().mockResolvedValue({natsContext, natsSubject}),
@@ -181,5 +184,47 @@ describe('Contact event handlers', () => {
 
     expect(ex.res.status).not.toBeCalled();
     expect(ex.res.end).not.toBeCalled();
+  });
+
+  it('A contact edit request is processed', async () => {
+    const req = selectContactEditRequest
+
+    const editedPO = {
+      productOwner: {
+        roleId: 1,
+        id: 233,
+        firstName: 'Jane',
+        lastName: 'Doe',
+        email: 'jane.doe@example.com',
+        githubId: 'janedoe',
+      },
+    }
+
+    const editedTC = {
+      technicalContact: {
+        roleId: 2,
+        id: 234,
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john.doe@example.com',
+        githubId: 'john1100',
+      },
+    }
+
+    client.query.mockResolvedValue({ ...editedPO, ...editedTC});
+
+    client.query.mockResolvedValueOnce({ rows: [{ editedPO }] });
+    client.query.mockResolvedValueOnce({ rows: [{ editedPO }] });
+    client.query.mockResolvedValueOnce({ rows: [{ editedTC }] });
+    client.query.mockResolvedValueOnce({ rows: [{ editedTC }] });
+
+    JSON.parse = jest.fn().mockImplementationOnce(() => {
+      return selectContactEditRequest.editObject
+    });
+
+    await processContactEdit(req);
+
+    expect(client.query.mock.calls).toMatchSnapshot();
+    expect(JSON.parse).toHaveBeenCalledTimes(1);
   });
 });
