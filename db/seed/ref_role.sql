28e2b67a607160dfe927dcbf76590941d741ea6f
Freeman <yfreeman@nem020062.hitronhub.home>
2021-01-29 10:34:53 -0800

    remove obsolete manual sql script

--- db/seed/ref_role.sql
+++
@@ -1,8 +0,0 @@
-
-BEGIN TRANSACTION;
-
-INSERT INTO ref_role (name, description) VALUES
-  ('Product Owner', 'The business owner of this product'),
-  ('Technical Contact', 'Technical contact or DevOps specialist');
-
-END TRANSACTION;
