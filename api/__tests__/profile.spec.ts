22871ec6ee53ac7da2f7a328cc9048304b431b4a
Freeman <yfreeman@nem020062.hitronhub.home>
2021-02-02 18:17:54 -0800

    update unit tests

--- api/__tests__/profile.spec.ts
+++ api/__tests__/profile.spec.ts
@@ -34,6 +34,9 @@ const userRequest = JSON.parse(fs.readFileSync(p2, 'utf8'));
 const p3 = path.join(__dirname, 'fixtures/select-profile-contacts.json');
 const selectProfilesContacts = JSON.parse(fs.readFileSync(p3, 'utf8'));
 
+const p4 = path.join(__dirname, 'fixtures/select-default-cluster.json');
+const selectDefaultCluster = JSON.parse(fs.readFileSync(p4, 'utf8'));
+
 const client = new Pool().connect();
 
 jest.mock('../src/db/utils', () => ({
@@ -84,6 +87,7 @@ describe('Profile event handlers', () => {
     client.query.mockReturnValueOnce({ rows: [{ count: '0' }] });
     client.query.mockReturnValueOnce({ rows: [{ count: '0' }] });
 
+    client.query.mockReturnValueOnce({ rows: selectDefaultCluster });
     client.query.mockReturnValueOnce({ rows: [{ ...insertProfile, ...addon }] });
 
     // @ts-ignore
