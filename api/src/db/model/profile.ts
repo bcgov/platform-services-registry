65207db845e05f71a70a05eb9e54f235a9e09285
Yuhan Freeman <yhfreeman12@gmail.com>
2021-02-09 23:27:20 -0800

    Fix the current breaking changes to web portal csv extract (#301)
    * fix
    
    * update snapshot

--- api/src/db/model/profile.ts
+++ api/src/db/model/profile.ts
@@ -284,8 +284,7 @@ export default class ProfileModel extends Model {
   async findProfilesByUserIdOrEmail(id: number, email: string): Promise<any> {
     const query = {
       text: `
-      SELECT DISTINCT(profile.id), profile.name, profile.description, profile.bus_org_id,
-      profile.user_id, profile.namespace_prefix FROM ${this.table}
+      SELECT DISTINCT(profile.id), profile.* FROM ${this.table}
       JOIN profile_contact ON profile_contact.profile_id = profile.id
       JOIN contact ON contact.id = profile_contact.contact_id
       WHERE (profile.user_id = $1 OR contact.email = $2) AND profile.archived = false;`,
