462018a71d35f4644bef3a6f9fe2470f211ba9f0
yhfreeman <yhfreeman12@gmail.com>
2021-04-21 02:20:25 -0700

    update unit test

--- web/src/__tests__/transformDataHelper.test.tsx
+++ web/src/__tests__/transformDataHelper.test.tsx
@@ -19,7 +19,7 @@ import {
   getProfileMinistry,
   isProfileProvisioned,
   sortProfileByDatetime,
-  transformJsonToCsv,
+  transformJsonToCsv
 } from '../utils/transformDataHelper';
 import profileContacts from './fixtures/profile-contacts.json';
 import profileMinistry from './fixtures/profile-ministry.json';
@@ -29,8 +29,7 @@ import sortedProfile from './fixtures/sorted-profiles.json';
 
 describe('test the helper function sortProfileByDatetime', () => {
   test('should return sorted profile data based so the order goes from the latest updated profile', () => {
-    const stubProfileData = profiles;
-    expect(sortProfileByDatetime(stubProfileData)).toMatchObject(sortedProfile);
+    expect(sortProfileByDatetime(profiles)).toMatchObject(sortedProfile);
   });
 
   test('should return original profile data upon errors', () => {
@@ -52,16 +51,14 @@ describe('test the helper function sortProfileByDatetime', () => {
 });
 
 describe('test the helper function isProfileProvisioned', () => {
-  test('should return true if all namespaces under a profile in silver cluster are provisioned', () => {
-    const stubProfileNamespaces = profileNamespaces;
-    expect(isProfileProvisioned(stubProfileNamespaces)).toEqual(true);
+  test('should return true if all namespaces under a profile primary cluster are provisioned', () => {
+    expect(isProfileProvisioned(profiles[0], profileNamespaces)).toEqual(true);
   });
 });
 
 describe('test the helper function getProfileContacts', () => {
   test('should return an object with key-values pairs for PO email and TC email', () => {
-    const stubProfileContacts = profileContacts;
-    expect(getProfileContacts(stubProfileContacts)).toEqual({
+    expect(getProfileContacts(profileContacts)).toEqual({
       POEmail: 'jane@example.com',
       POFirstName: 'Jane',
       POGithubId: 'jane1100',
@@ -80,9 +77,8 @@ describe('test the helper function getProfileContacts', () => {
 
 describe('test the helper function getProfileMinistry', () => {
   test('should return an object with key-values pairs for PO email and TC email', () => {
-    const stubProfileMinistry = profileMinistry;
     const ministry = { busOrgId: 'ALC' };
-    expect(getProfileMinistry(stubProfileMinistry, ministry)).toEqual({
+    expect(getProfileMinistry(profileMinistry, ministry)).toEqual({
       ministryName: 'Agriculture Land Commission',
     });
   });
@@ -96,7 +92,7 @@ describe('test the helper function transformJsonToCsv', () => {
     });
 
     const result =
-      'id,name,description,prioritySystem,criticalSystem,createdAt,updatedAt,userId,quotaSize\r\n"2","EPIC","Hello World","false","true","28-10-2020 03:00","28-10-2020 03:00","4","small"\r\n"1","Health Gateway","null","false","false","28-04-2020 00:00","28-04-2020 00:00","4","small"\r\n"3","Mines Digital Services","This is some description","false","false","18-04-2020 10:10","18-04-2020 10:10","small"\r\n';
+      'id,name,description,prioritySystem,criticalSystem,primaryClusterName,createdAt,updatedAt,userId,quotaSize\r\n"2","EPIC","Hello World","false","true","silver","28-10-2020 03:00","28-10-2020 03:00","4","small"\r\n"1","Health Gateway","null","false","false","silver","28-04-2020 00:00","28-04-2020 00:00","4","small"\r\n"3","Mines Digital Services","This is some description","false","false","silver","18-04-2020 10:10","18-04-2020 10:10","4","small"\r\n';
     expect(transformJsonToCsv(stubJson)).toEqual(result);
   });
 });
