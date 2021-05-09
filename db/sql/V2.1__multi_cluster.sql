4c1d39d6b71e9ea1f96901af299d543ae4ed32dc
yhfreeman <yhfreeman12@gmail.com>
2021-05-08 18:56:26 -0700

    refactor sql, archive golddr and add additional field display_name to ref_cluster table

--- db/sql/V2.1__multi_cluster.sql
+++ db/sql/V2.1__multi_cluster.sql
@@ -10,9 +10,16 @@ DROP COLUMN IF EXISTS quota_cpu,
 DROP COLUMN IF EXISTS quota_memory,
 DROP COLUMN IF EXISTS quota_storage;
 
-UPDATE ref_cluster SET archived = true WHERE name = 'aro';
+ALTER TABLE ref_cluster
+ADD COLUMN IF NOT EXISTS is_prod BOOLEAN NOT NULL DEFAULT true,
+ADD COLUMN IF NOT EXISTS display_name VARCHAR(64);
 
-ALTER TABLE ref_cluster ADD COLUMN IF NOT EXISTS is_prod BOOLEAN NOT NULL DEFAULT true;
+UPDATE ref_cluster SET archived = true WHERE name = 'aro' OR name = 'golddr';
 UPDATE ref_cluster SET is_prod = false WHERE name = 'clab' OR name = 'klab' OR name = 'gold' OR name = 'golddr';
+UPDATE ref_cluster SET display_name = 'Silver Kamloops' WHERE name = 'silver';
+UPDATE ref_cluster SET display_name = 'Gold Kamloops' WHERE name = 'gold';
+UPDATE ref_cluster SET display_name = 'Gold (DR) Calgary' WHERE name = 'golddr';
+UPDATE ref_cluster SET display_name = 'KLAB Kamloops' WHERE name = 'klab';
+UPDATE ref_cluster SET display_name = 'CLAB Calgary' WHERE name = 'clab';
 
 END TRANSACTION;
