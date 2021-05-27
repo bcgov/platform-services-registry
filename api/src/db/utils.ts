c18deb901cd07ef2ed0e099fe0b95e7d8aec046f
sbathgate <30994898+sbathgate@users.noreply.github.com>
2021-05-27 13:32:03 -0700

    Implement an optional prefix config for project licenseplates (#409)
    * Implement an optional prefix config for project licenseplates
    
    * Add a comment describing license plate prefix

--- api/src/db/utils.ts
+++ api/src/db/utils.ts
@@ -18,6 +18,7 @@
 
 import crypto from 'crypto';
 import { camelCase } from 'lodash';
+import config from '../config';
 
 /**
  * Convert the keys of an object from snake notation to camel case
@@ -35,7 +36,9 @@ export const transformKeysToCamelCase = data => {
 };
 
 export const generateNamespacePrefix = (len: number = 6): string => {
-  return crypto
+  // For DEV/TEST environments, a license plate prefix is necessary to avoid duplication
+  // within ArgoCD. Utilizing the prefix "T" for TEST, and "D" for DEV will avoid this.
+  return config.get('api:prefix') + crypto
     .randomBytes(Math.ceil(len / 2))
     .toString('hex')
     .slice(0, len);
