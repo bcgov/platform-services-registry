e86b3c745f4b7c903f6e41b9027ab7e74cbb62f4
w8896699 <billy.li@gov.bc.ca>
2021-08-09 13:25:39 -0700

    test build

--- api/src/config/githubConfig.ts
+++ api/src/config/githubConfig.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 const getBasePathWithFile = (file) => {
   let filePath
-
+  console.log('hiahia what is this dev test', process.env.PRIVATE_KEY_PATH )
   if (
     (process.env.NODE_ENV === 'production'|| process.env.NODE_ENV === 'development') &&
     process.env.PRIVATE_KEY_PATH &&
