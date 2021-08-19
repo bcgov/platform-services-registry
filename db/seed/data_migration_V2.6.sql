986bd3647dcdc73aeeac92bdc6975412c7d37f3d
Shaun Bathgate <shaun.bathgate@gov.bc.ca>
2021-08-19 16:29:52 -0700

    Add profile status migration

---
+++ db/seed/data_migration_V2.6.sql
@@ -0,0 +1,9 @@
+UPDATE profile
+SET profile_status = 
+  CASE 
+    WHEN r.requires_human_action = true THEN 'pending_approval'::status_type
+    WHEN r.is_active = true THEN 'approved'::status_type
+    ELSE 'provisioned'::status_type
+  END
+FROM request r
+WHERE profile.id = r.profile_id AND r.is_active = true;
