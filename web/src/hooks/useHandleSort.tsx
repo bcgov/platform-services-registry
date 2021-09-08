a8cd4a10e87eebd2f240e8fa4e16b8d40e4412cc
w8896699 <billy.li@gov.bc.ca>
2021-09-08 12:48:51 -0700

    smells good now

--- web/src/hooks/useHandleSort.tsx
+++ web/src/hooks/useHandleSort.tsx
@@ -1,6 +1,14 @@
 import { useCallback } from 'react';
 
 export const useHandleSort = (setData: any, profileDetails: any) => {
+  const sortHelper = (a: any, b: any, array: any, index: any) => {
+    if (a > b) {
+      return array[index].desc ? -1 : 1;
+    }
+    if (a < b) {
+      return array[index].desc ? 1 : -1;
+    }
+  };
   const ourHandleSort = useCallback(
     (sortBy) => {
       // Doing multisort
@@ -9,24 +17,13 @@ export const useHandleSort = (setData: any, profileDetails: any) => {
         for (let i = 0; i < sortBy.length; ++i) {
           if (typeof a[sortBy[i].id] === 'object') {
             if (sortBy[i].id === 'clusters') {
-              if (a[sortBy[i].id][0] > b[sortBy[i].id][0]) return sortBy[i].desc ? -1 : 1;
-              if (a[sortBy[i].id][0] < b[sortBy[i].id][0]) return sortBy[i].desc ? 1 : -1;
+              return sortHelper(a[sortBy[i].id][0], b[sortBy[i].id][0], sortBy, i);
             }
-            if (sortBy[i].id === 'productOwners') {
-              if (a[sortBy[i].id][0].email > b[sortBy[i].id][0].email)
-                return sortBy[i].desc ? -1 : 1;
-              if (a[sortBy[i].id][0].email < b[sortBy[i].id][0].email)
-                return sortBy[i].desc ? 1 : -1;
-            }
-            if (sortBy[i].id === 'technicalLeads') {
-              if (a[sortBy[i].id][0].email > b[sortBy[i].id][0].email)
-                return sortBy[i].desc ? -1 : 1;
-              if (a[sortBy[i].id][0].email < b[sortBy[i].id][0].email)
-                return sortBy[i].desc ? 1 : -1;
+            if (sortBy[i].id === 'technicalLeads' || sortBy[i].id === 'productOwners') {
+              return sortHelper(a[sortBy[i].id][0].email, b[sortBy[i].id][0].email, sortBy, i);
             }
           } else {
-            if (a[sortBy[i].id] > b[sortBy[i].id]) return sortBy[i].desc ? -1 : 1;
-            if (a[sortBy[i].id] < b[sortBy[i].id]) return sortBy[i].desc ? 1 : -1;
+            return sortHelper(a[sortBy[i].id], b[sortBy[i].id], sortBy, i);
           }
         }
         return 0;
