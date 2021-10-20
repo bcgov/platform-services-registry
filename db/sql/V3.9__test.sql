721dcedc3d567c3f8a904f018510e8eaa17e53fd
w8896699 <billy.li@gov.bc.ca>
2021-10-20 16:25:08 -0700

    add migration script

---
+++ db/sql/V3.9__test.sql
@@ -0,0 +1,11 @@
+BEGIN TRANSACTION;
+
+ALTER TABLE ref_quota
+ADD COLUMN IF NOT EXISTS snapshot_volume smallint NOT NULL DEFAULT 5;
+
+
+ALTER TABLE cluster_namespace
+ADD COLUMN IF NOT EXISTS quota_snapshot varchar(16) REFERENCES ref_quota(id) DEFAULT 'small';
+
+
+END TRANSACTION;
