049e451ee052257e899ac0517a9bfb5d2b443a70
w8896699 <billy.li@gov.bc.ca>
2022-01-11 22:48:43 -0800

    test using webpack

---
+++ web/config-overrides.js
@@ -0,0 +1,30 @@
+const { override } = require('customize-cra');
+const cspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
+
+const cspConfigPolicy = {
+  'default-src': [
+    "'self'",
+    ' https://dev.oidc.gov.bc.ca/',
+    'http://localhost:8100/api/',
+    'https://registry-web-platform-registry-dev.apps.silver.devops.gov.bc.ca/api/',
+  ],
+  'base-uri': "'self'",
+  'script-src': ["'self'", "'unsafe-inline'"],
+  'manifest-src': "'self'",
+  'font-src': "'self'",
+  'style-src': ["'self'", "'unsafe-inline'"],
+  'img-src': ["'self'", 'https://avatars.githubusercontent.com/'],
+};
+
+function addCspHtmlWebpackPlugin(config) {
+  console.log('haihaihaiha', process.env);
+  if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy));
+  }
+
+  return config;
+}
+
+module.exports = {
+  webpack: override(addCspHtmlWebpackPlugin),
+};
