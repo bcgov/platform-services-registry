907ffa137675304f576ab737f8fcd6197a6c0e95
w8896699 <billy.li@gov.bc.ca>
2022-01-12 21:45:05 -0800

    csp test

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -2,29 +2,27 @@ const { override } = require('customize-cra');
 const cspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
 
 const cspConfigPolicy = {
-  'default-src': [
-    "'self'",
-    ' https://dev.oidc.gov.bc.ca/',
-    'http://localhost:8100/api/',
-    'https://registry-web-platform-registry-dev.apps.silver.devops.gov.bc.ca/api/',
-  ],
+  'default-src': ["'self'", '*.gov.bc.ca/', '*.gov.bc.ca/api/'],
   'script-src': ["'self'"],
   'base-uri': "'self'",
   'manifest-src': "'self'",
   'font-src': "'self'",
   'style-src': ["'self'", "'unsafe-inline'"],
   'img-src': ["'self'", 'https://avatars.githubusercontent.com/'],
+  'object-src': "'self'",
+  'frame-src': ["'self'", '*.oidc.gov.bc.ca/'],
+  'frame-ancestors': "'self'",
 };
 
 const additionalOps = {
   enabled: true,
   hashingMethod: 'sha256',
-  nonceEnabled: { 'script-src': false, 'style-src': false },
+  nonceEnabled: { 'script-src': true, 'style-src': false },
 };
 function addCspHtmlWebpackPlugin(config) {
-  if (process.env.NODE_ENV === 'production') {
-    config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy, additionalOps));
-  }
+  //   if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy, additionalOps));
+  //   }
 
   return config;
 }
