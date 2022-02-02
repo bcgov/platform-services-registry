85fed6cb14c329a8dafad1071bb63f07333011fd
Oamar Kanji <30703259+okanji@users.noreply.github.com>
2022-02-02 11:53:32 -0800

    Read only admin - fixed eslint issue (#609)
    * created READ_ONLY_ADMINISTRATOR role and created ReadAll and ReadEditAll authorization flags
    
    * added READ_ONLY_ADMINISTRATOR test
    
    * created read all flag
    
    * added authorization check in updateRequestHumanAction function to throw error if user on read-only-admin tries to submit a  review for a project
    
    * added api/.vscode/ to git ignore
    
    * added test for updateRequestHumanAction
    
    * web ui does not show option to review project for read only admin
    
    * added test to make sure that updateRequestHumanAction does not authorize a regular user
    
    * corrected spelling mistake
    
    * removed unnecessary code
    
    * removed console log
    
    * fixed linting issue

--- web/src/views/Dashboard.tsx
+++ web/src/views/Dashboard.tsx
@@ -77,7 +77,12 @@ const Dashboard: React.FC = () => {
 
   return (
     <>
-      { (userRoles.includes('administrator') || userRoles.includes('read_only_administrator')) && <ProjectRequests profileDetails={profileDetails} isAdminUser = {userRoles.includes('administrator')}/>}
+      {(userRoles.includes('administrator') || userRoles.includes('read_only_administrator')) && (
+        <ProjectRequests
+          profileDetails={profileDetails}
+          isAdminUser={userRoles.includes('administrator')}
+        />
+      )}
 
       <ProjectDetails profileDetails={profileDetails} linkedRows={true} />
     </>
