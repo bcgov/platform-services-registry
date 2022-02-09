979ae399a596da3932ca298ae6c9bdc71b4b620b
Oamar Kanji <oamarkanji@gmail.com>
2022-02-09 03:10:58 -0800

    moved communications selections array to constants.js

--- api/src/controllers/communications/helpers.ts
+++ api/src/controllers/communications/helpers.ts
@@ -1,44 +1,10 @@
 import { URLSearchParams } from "url";
 import axios from "axios";
+import { selectedCommunications } from "./constants";
 
 const suscribeData = (contactId) => ({
   ContactId: contactId,
-  SegmentsAndIds: [
-    {
-      segmentID: "5",
-      segmentName: "Pathfinder SSO Segment",
-      description:
-        "This mailing list includes updates about major upcoming changes for the BC Gov's Pathfinder Single Sign On (SSO) Service (KeyCloak), important information for the product teams, and updates about SSO Service outages",
-      isChecked: true,
-    },
-    {
-      segmentID: "7",
-      segmentName: "Platform Services team - test",
-      description: "This is for testing your email before sending it!",
-      isChecked: true,
-    },
-    {
-      segmentID: "1",
-      segmentName: "Platform Services Updates",
-      description:
-        "This mailing list includes updates about major upcoming changes on the Platform, important information for the product teams, and updates about Platform outages",
-      isChecked: true,
-    },
-    {
-      segmentID: "4",
-      segmentName: "SSO Updates",
-      description:
-        "This mailing list includes updates about major upcoming changes for the BC Gov's Pathfinder Single Sign On (SSO) Service (KeyCloak), important information for the product teams, and updates about SSO Service outages",
-      isChecked: true,
-    },
-    {
-      segmentID: "6",
-      segmentName: "SSO Updates Core Team",
-      description:
-        "This mailing list goes to the Pathfinder SSO core team&nbsp;",
-      isChecked: true,
-    },
-  ],
+  SegmentsAndIds: selectedCommunications,
 });
 
 export const getToken = async () => {
