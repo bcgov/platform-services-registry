ca441fffafa51b6f7d10fc20a3ade5f2ce4509a6
Alex Carmichael <carmichael.alexander@gmail.com>
2022-03-14 16:39:19 -0700

    Update PSRDocumentationLink.tsx

--- web/src/components/dashboard/PSRDocumentationLink.tsx
+++ web/src/components/dashboard/PSRDocumentationLink.tsx
@@ -6,8 +6,11 @@ const PSRDocumentationLink: React.FC = () => {
   return (
     <Box>
       <Text>
-        {/* eslint-disable-next-line */}
-        Git documentation for Platform Services Registry and a link to report issues can be found <Link to="https://github.com/bcgov/platform-services-registry">here</Link>
+        Git documentation for Platform Services Registry and a link to report issues can be found
+        &nbsp;
+        <Link to="https://github.com/bcgov/platform-services-registry" target="_blank">
+          here.
+        </Link>
       </Text>
     </Box>
   );
