158291b16aa11185c63458e76897ca31a27d9780
Alex Carmichael <carmichael.alexander@gmail.com>
2022-03-14 13:52:10 -0700

    fixed formatting and eslint issue

--- web/src/components/dashboard/PSRDocumentationLink.tsx
+++ web/src/components/dashboard/PSRDocumentationLink.tsx
@@ -6,8 +6,8 @@ const PSRDocumentationLink: React.FC = () => {
   return (
     <Box>
       <Text>
-        Git documentation for Platform Services Registry and a link to report issues can be found
-        <Link to="https://github.com/bcgov/platform-services-registry">here</Link>
+        {/* eslint-disable-next-line */}
+        Git documentation for Platform Services Registry and a link to report issues can be found <Link to="https://github.com/bcgov/platform-services-registry">here</Link>
       </Text>
     </Box>
   );
