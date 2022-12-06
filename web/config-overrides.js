ab5af51989c6012f6e0087ceb73cbd65ec66c2dc
Alex Carmichael <carmichael.alexander@gmail.com>
2022-12-06 13:00:53 -0800

    Update config-overrides.js

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -8,7 +8,7 @@ const cspConfigPolicy = {
   'manifest-src': "'self'",
   'font-src': "'self'",
   'style-src': ["'self'", "'unsafe-inline'"],
-  'img-src': ["'self'", 'https://graph.microsoft.com/v1.0/'],
+  'img-src': ["'self'", '*.gov.bc.ca'],
   'object-src': "'self'",
   'frame-src': ["'self'", 'https://dev.oidc.gov.bc.ca/', 'https://oidc.gov.bc.ca'],
   'frame-ancestors': "'self'",
