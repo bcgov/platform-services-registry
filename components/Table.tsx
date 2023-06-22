4480eb9aca29e7bfdb567f3f404ba459970b52ee
okanji <oamarkanji@gmail.com>
2023-06-22 10:05:24 -0700

    made table scrollable

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Edit from "@/components/assets/edit.svg";
-import { Suspense } from "react";
 
 interface TableProps {
   headers: Record<string, string>[];
@@ -12,58 +11,61 @@ export default function Table({ headers, rows }: TableProps) {
 
   return (
     <>
-      <div className="mt-8 flow-root overflow-hidden">
-        <div className="mx-auto w-full px-4 sm:px-6 lg:px-8">
-          <table className="w-full text-left">
-            <thead className="bg-white">
-              <tr>
-                <th
-                  scope="col"
-                  className="font-bcsans relative isolate py-3.5 pr-3 text-left text-sm font-normal text-mediumgrey w-full md:w-auto"
-                >
-                  {headers[0]["headerName"]}
-                  <div className="absolute inset-y-0 right-full -z-10 w-screen border-b border-b-gray-200" />
-                  <div className="absolute inset-y-0 left-0 -z-10 w-screen border-b border-b-gray-200" />
-                </th>
-                {headers.slice(1, headers.length).map(({ headerName }) => (
+      <div className="flow-root overflow-hidden">
+        <div
+          className="mx-auto w-full px-4 sm:px-6 lg:px-8 overflow-auto"
+          // style={{ maxHeight: "600px" }}
+        >
+          <div className="inline-block min-w-full align-middle">
+            <table className="w-full text-left">
+              <thead className="bg-white">
+                <tr>
                   <th
                     scope="col"
-                    className="font-bcsans hidden px-3 py-3.5 text-left text-sm font-normal md:table-cell  text-mediumgrey"
+                    className="font-bcsans relative isolate py-3.5 pr-3 text-left text-sm font-normal text-mediumgrey md:w-auto"
                   >
-                    {headerName}
+                    {headers[0]["headerName"]}
                   </th>
-                ))}
-                <th scope="col" className="relative py-3.5 pl-3">
-                  <span className="sr-only">Edit</span>
-                </th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {rows.map((row, i) => (
-                <tr key={row.licencePlate + i}>
-                  <td className="relative py-4 pr-3 text-sm  text-mediumgrey">
-                    {row[headers[0]["field"]]}
-                    <div className="absolute bottom-0 right-full h-px w-screen bg-gray-100" />
-                    <div className="absolute bottom-0 left-0 h-px w-screen bg-gray-100" />
-                  </td>
-                  {headers.slice(1, headers.length).map((value) => (
-                    <td className="hidden px-3 py-4 text-sm text-mediumgrey md:table-cell">
-                      {row[value["field"]]}
-                    </td>
+                  {headers.slice(1, headers.length).map(({ headerName }) => (
+                    <th
+                      scope="col"
+                      className="font-bcsans hidden px-3 py-3.5 text-left text-sm font-normal md:table-cell text-mediumgrey"
+                    >
+                      {headerName}
+                    </th>
                   ))}
-                  <td>
-                    <Image
-                      alt="Vercel logo"
-                      src={Edit}
-                      width={16}
-                      height={12.5}
-                    />
-                  </td>
+                  <th scope="col" className="relative py-3.5 pl-3">
+                    <span className="sr-only">Edit</span>
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+
+              <tbody>
+                {rows.map((row, i) => (
+                  <tr key={row.licencePlate + i}>
+                    <td className="relative py-4 pr-3 text-sm text-mediumgrey">
+                      {row[headers[0]["field"]]}
+                    </td>
+                    {headers.slice(1, headers.length).map((value) => (
+                      <td className="hidden px-3 py-4 text-sm text-mediumgrey md:table-cell">
+                        {row[value["field"]]}
+                      </td>
+                    ))}
+                    <td>
+                      <div style={{ width: "16px", height: "12.5px" }}>
+                        <Image
+                          alt="Vercel logo"
+                          src={Edit}
+                          width={16}
+                          height={12.5}
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
