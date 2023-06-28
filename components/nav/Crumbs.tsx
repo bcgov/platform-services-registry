42ba2f8e78e9a6d157c87bb5c2d4001a35672fdc
okanji <oamarkanji@gmail.com>
2023-06-28 12:58:18 +0300

    added crumbs

---
+++ components/nav/Crumbs.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import Arrow from "@/components/assets/arrow.svg";
+import Link from "next/link";
+
+const pages = [
+  { name: "Projects", href: "#", current: false },
+  { name: "Project Nero", href: "#", current: true },
+];
+
+export default function Crumbs({
+  backUrl,
+  previousLabel,
+  currentLabel,
+}: {
+  backUrl: string;
+  previousLabel?: string;
+  currentLabel?: string;
+}) {
+  return (
+    <nav className="flex" aria-label="Breadcrumb">
+      <ol role="list" className="flex items-center ">
+        <li>
+          <div>
+            <Link href="/private-cloud/products">
+              <Image
+                alt="Arrow"
+                src={Arrow}
+                width={25}
+                height={25}
+                style={{
+                  maxWidth: "100%",
+                  height: "auto",
+                }}
+              />
+            </Link>
+          </div>
+        </li>
+        <li>
+          <div className="flex items-center">
+            <a className="ml-4 font-bcsans text-xl font-medium text-gray-500">
+              {previousLabel}
+            </a>
+            {previousLabel && (
+              <svg
+                className="h-5 w-5 flex-shrink-0 text-gray-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                aria-hidden="true"
+              >
+                <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+              </svg>
+            )}
+          </div>
+        </li>
+        <li>
+          <div className="flex items-center font-bcsans text-xl">
+            {currentLabel}
+          </div>
+        </li>
+      </ol>
+    </nav>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,14 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}"
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       height: {
         18: "4.35rem",
         17: "4.3rem",
-        "screen-60": "80vh"
+        "screen-60": "80vh",
       },
       colors: {
         bcblue: "#003366",
@@ -21,24 +21,26 @@ module.exports = {
         mediumgrey: "#475467",
         tableheadergrey: "rgba(214, 216, 213, 0.15)",
         tablebordergrey: "#EAECF0",
-        disabledborder: "D0D5DD"
+        disabledborder: "D0D5DD",
+        cloudgrey: "#667085",
+        divider: "#0000001f",
       },
       borderWidth: {
         1: "1px",
-        3: "3px"
+        3: "3px",
       },
       textColor: {
-        deselected: "rgba(102, 112, 133, 0.44)"
+        deselected: "rgba(102, 112, 133, 0.44)",
       },
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
         roboto: ["Roboto", ...defaultTheme.fontFamily.sans],
-        bcsans: ["BCSans", "sans-serif"]
+        bcsans: ["BCSans", "sans-serif"],
       },
       maxWidth: {
-        test: "40%"
-      }
-    }
+        test: "40%",
+      },
+    },
   },
-  plugins: [require("@tailwindcss/forms")]
+  plugins: [require("@tailwindcss/forms")],
 };
