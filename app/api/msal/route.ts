3a4afb5179a626ba4c48c871d851c47821a755dd
okanji <oamarkanji@gmail.com>
2023-07-02 10:28:10 +0300

    contact form

---
+++ app/api/msal/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { callMsGraph, getAccessToken } from "@/lib//msal";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const email = searchParams.get("email");
+
+  const url = `https://graph.microsoft.com/v1.0/users?$filter=startswith(mail,'${email}')&$orderby=userPrincipalName&$count=true&$top=25`;
+
+  try {
+    const accessToken = await getAccessToken();
+
+    if (!accessToken) {
+      return new NextResponse("No access token", { status: 400 });
+    }
+
+    const response = await callMsGraph(url, accessToken);
+    const data = await response.json();
+
+    return NextResponse.json(data.value);
+  } catch (error) {
+    console.error("ERROR");
+    console.error(error);
+    return NextResponse.error();
+  }
+}
