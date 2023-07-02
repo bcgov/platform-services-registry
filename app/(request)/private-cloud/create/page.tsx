ba824a1cfe440ebc1f72b9dcfd9fee4bc2f4548a
okanji <oamarkanji@gmail.com>
2023-07-02 16:36:01 +0300

    fixed build issues

--- app/(request)/private-cloud/create/page.tsx
+++ app/(request)/private-cloud/create/page.tsx
@@ -133,7 +133,7 @@ export default function Page() {
             Notifications
           </h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
-            We'll always let you know about important changes, but you pick what
+            We&apos;sll always let you know about important changes, but you pick what
             else you want to hear about
           </p>
 
