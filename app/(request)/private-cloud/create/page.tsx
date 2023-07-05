a0a323bd92c5c071bbf1d6d1a5840b096eaece6d
okanji <oamarkanji@gmail.com>
2023-07-05 13:40:25 +0300

    fixed validation

--- app/(request)/private-cloud/create/page.tsx
+++ app/(request)/private-cloud/create/page.tsx
@@ -27,6 +27,8 @@ export default function Page() {
   });
 
   const onSubmit = async (data: any) => {
+    console.log("SUBMIT")
+    console.log(data);
     try {
       const response = await fetch("/api/requests/create", {
         method: "POST",
@@ -55,18 +57,23 @@ export default function Page() {
     }
   };
 
-  console.log("VALUES");
-  console.log(watch());
-  console.log("ERRORS");
   console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
+          <h1 className="text-base font-semibold leading-7 text-gray-900 mb-8">
+            Private Cloud OpenShift Platform - Project Set Provisioning Request
+          </h1>
           <h2 className="text-base font-semibold leading-7 text-gray-900">
             1. Product Description
           </h2>
+          <p className="mt-2 text-sm leading-6 ">
+            If this is your first time on the OpenShift platform you need to
+            book an alignment meeting with the Platform Services team. Reach out
+            to Olena Mitovska to get started.
+          </p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="col-span-full">
@@ -79,6 +86,7 @@ export default function Page() {
               <div className="mt-2">
                 <input
                   type="text"
+                  placeholder="Enter product name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   {...register("name")}
                 />
@@ -103,6 +111,7 @@ export default function Page() {
               <div className="mt-2">
                 <textarea
                   id="about"
+                  placeholder="Enter a description..."
                   {...register("description")}
                   rows={3}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -130,6 +139,7 @@ export default function Page() {
                   // name="ministry"
                   // id="first-name"
                   // autoComplete="given-name"
+
                   {...register("ministry")}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 >
@@ -299,8 +309,6 @@ export default function Page() {
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        {/* <input type="submit" /> */}
-
         <button
           type="submit"
           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
