efb513316c70a9c59a703dc43137f0d4c7a1ca34
okanji <oamarkanji@gmail.com>
2023-07-31 17:59:25 +0300

    added filter

---
+++ components/table/FilterPanel.tsx
@@ -0,0 +1,72 @@
+import {
+  usePathname,
+  useRouter,
+  useSearchParams,
+  useParams,
+} from "next/navigation";
+
+const filters = {
+  cluster: [
+    { value: "KLAB", label: "KLAB", checked: false },
+    { value: "CLAB", label: "CLAB", checked: false },
+    { value: "SILVER", label: "SILVER", checked: false },
+    { value: "GOLD", label: "GOLD", checked: false },
+  ],
+};
+
+export default function FilterPanel() {
+  const { replace } = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams()!;
+
+  const handleClusterFilterChange = (cluster: string | null) => {
+    const urlSearchParams = new URLSearchParams(searchParams?.toString());
+
+    if (cluster) {
+      urlSearchParams.set("cluster", cluster);
+    } else {
+      urlSearchParams.delete("cluster");
+    }
+    urlSearchParams.delete("page");
+
+    replace(`${pathname}?${urlSearchParams.toString()}`);
+  };
+
+  return (
+    <div className="mx-auto grid max-w-7xl grid-cols-2 gap-x-4 px-4 text-sm sm:px-6 md:gap-x-6 lg:px-8">
+      <div className="grid auto-rows-min grid-cols-1 gap-y-10 md:grid-cols-2 md:gap-x-6">
+        <fieldset>
+          <legend className="block font-medium">Cluster</legend>
+          <div className="space-y-6 pt-6 sm:space-y-4 sm:pt-4">
+            {filters.cluster.map((option, optionIdx) => (
+              <div
+                key={option.value}
+                className="flex items-center text-base sm:text-sm"
+              >
+                <input
+                  id={`price-${optionIdx}`}
+                  name="price[]"
+                  defaultValue={option.value}
+                  type="checkbox"
+                  className="h-4 w-4 flex-shrink-0 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                  defaultChecked={option.checked}
+                  onChange={(e) =>
+                    handleClusterFilterChange(
+                      e.target.checked ? e.target.value : null
+                    )
+                  }
+                />
+                <label
+                  htmlFor={`price-${optionIdx}`}
+                  className="ml-3 min-w-0 flex-1 text-gray-600"
+                >
+                  {option.label}
+                </label>
+              </div>
+            ))}
+          </div>
+        </fieldset>
+      </div>
+    </div>
+  );
+}
--- components/table/SearchFilterSort.tsx
+++ components/table/SearchFilterSort.tsx
@@ -12,61 +12,19 @@ import {
   useParams,
 } from "next/navigation";
 import { useDebounce } from "@/components/utils/useDebounce";
+import FilterPanel from "./FilterPanel";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
 
 export default function SearchFilterSort() {
   const [focused, setFocused] = useState(false);
   const { replace } = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams()!;
-  const params = useParams();
-
+ 
   const [isPending, startTransition] = useTransition();
 
   const [searchTerm, setSearchTerm] = useState<string>("");
   const debouncedValue = useDebounce<string>(searchTerm, 450);
-
-  const handleMinistryFilterChange = (ministry: string) => {
-    const urlParams = { ...params };
-
-    if (ministry) {
-      urlParams.ministry = ministry;
-    } else {
-      delete urlParams.filter;
-    }
-
-    startTransition(() => {
-      replace(`${pathname}?${urlParams.toString()}`);
-    });
-  };
-
-  const handleClusterFilterChange = (cluster: string) => {
-    const urlParams = { ...params };
-
-    if (cluster) {
-      urlParams.ministry = cluster;
-    } else {
-      delete urlParams.filter;
-    }
-
-    startTransition(() => {
-      replace(`${pathname}?${urlParams.toString()}`);
-    });
-  };
-
-  // handle search with debounce
-
-  // const handleSearch = (term: string) => {
-  //   const params = new URLSearchParams(searchParams?.toString());
-
-  //   if (term) {
-  //     params.set("search", term);
-  //   } else {
-  //     params.delete("search");
-  //   }
-  //   params.delete("page");
-
-  //   replace(`${pathname}?${params.toString()}`);
-  // };
 
   const handleSearch = useCallback(
     (term: string) => {
@@ -79,7 +37,9 @@ export default function SearchFilterSort() {
       }
       params.delete("page");
 
-      replace(`${pathname}?${params.toString()}`);
+      startTransition(() => {
+        replace(`${pathname}?${params.toString()}`);
+      });
     },
     [searchParams, replace, pathname]
   );
@@ -96,73 +56,79 @@ export default function SearchFilterSort() {
   }, [debouncedValue, handleSearch]);
 
   return (
-    <div className="flex justify-end space-x-2.5 ">
-      <form className="flex-grow flex-shrink max-w-sm">
-        <label htmlFor="simple-search" className="sr-only">
-          Search
-        </label>
-        <div className="relative w-full">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <Image
-              alt="Search"
-              src={Search}
-              width={15}
-              height={15}
-              style={{
-                maxWidth: "100%",
-                height: "auto",
-              }}
-            />
-          </div>
-          <input
-            type="text"
-            id="simple-search"
-            className="w-full border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-slate-300-500 focus:border-slate-300-500 block pl-9 p-1.5 dark:border-gray-300 dark:placeholder-gray-400 dark:text-darkergrey dark:focus:ring-slate-300 dark:focus:border-slate-300"
-            placeholder="Search"
-            onFocus={() => setFocused(true)}
-            onBlur={() => setFocused(false)}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            spellCheck={false}
-          />
-          {isPending && (
-            <div className="absolute inset-y-0 right-0 mr-4 flex items-center pl-3 pointer-events-none">
-              <span className=" border-gray-300 h-5 w-5 animate-spin rounded-full border-2 border-t-slate-400" />
+    <Disclosure>
+      <div className="flex justify-end space-x-2.5">
+        <form className="flex-grow flex-shrink max-w-sm">
+          <label htmlFor="simple-search" className="sr-only">
+            Search
+          </label>
+          <div className="relative w-full">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <Image
+                alt="Search"
+                src={Search}
+                width={15}
+                height={15}
+                style={{
+                  maxWidth: "100%",
+                  height: "auto",
+                }}
+              />
             </div>
-          )}
-        </div>
-      </form>
-      <button
-        type="button"
-        className="inline-flex items-center gap-x-2 rounded-md bg-white px-3 py-1.5 text-sm font-semibold text-darkergrey shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-      >
-        <Image
-          alt="Filter"
-          src={Filter}
-          width={16}
-          height={10}
-          style={{
-            maxWidth: "100%",
-            height: "auto",
-          }}
-        />
-        <span className="md:inline hidden">Filters</span>
-      </button>
-      <button
-        type="button"
-        className="inline-flex items-center gap-x-2 rounded-md bg-white px-3 py-1.5 text-sm font-semibold text-darkergrey shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-      >
-        <Image
-          alt="Export"
-          src={Export}
-          width={16}
-          height={12.5}
-          style={{
-            maxWidth: "100%",
-            height: "auto",
-          }}
-        />
-        <span className="md:inline hidden">Export</span>
-      </button>
-    </div>
+            <input
+              type="text"
+              id="simple-search"
+              className="w-full border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-slate-300-500 focus:border-slate-300-500 block pl-9 p-1.5 dark:border-gray-300 dark:placeholder-gray-400 dark:text-darkergrey dark:focus:ring-slate-300 dark:focus:border-slate-300"
+              placeholder="Search"
+              onFocus={() => setFocused(true)}
+              onBlur={() => setFocused(false)}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              spellCheck={false}
+            />
+            {isPending && (
+              <div className="absolute inset-y-0 right-0 mr-4 flex items-center pl-3 pointer-events-none">
+                <span className=" border-gray-300 h-5 w-5 animate-spin rounded-full border-2 border-t-slate-400" />
+              </div>
+            )}
+          </div>
+        </form>
+        <Disclosure.Button
+          type="button"
+          className="inline-flex items-center gap-x-2 rounded-md bg-white px-3 py-1.5 text-sm font-semibold text-darkergrey shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        >
+          <Image
+            alt="Filter"
+            src={Filter}
+            width={16}
+            height={10}
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
+          />
+
+          <span className="md:inline hidden">Filters</span>
+        </Disclosure.Button>
+        <button
+          type="button"
+          className="inline-flex items-center gap-x-2 rounded-md bg-white px-3 py-1.5 text-sm font-semibold text-darkergrey shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        >
+          <Image
+            alt="Export"
+            src={Export}
+            width={16}
+            height={12.5}
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
+          />
+          <span className="md:inline hidden">Export</span>
+        </button>
+      </div>
+      <Disclosure.Panel className="py-10">
+        <FilterPanel />
+      </Disclosure.Panel>
+    </Disclosure>
   );
 }
