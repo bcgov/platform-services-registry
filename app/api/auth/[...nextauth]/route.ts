9f205727ab32c32f13ffb8ac97e408dcf49ced15
okanji <oamarkanji@gmail.com>
2023-09-13 07:34:41 -0700

    fixed auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,33 @@
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { PrismaClient } from "@prisma/client";
+// import KeycloakProvider from "next-auth/providers/keycloak";
+
+// const prisma = new PrismaClient();
+
+// const handler = NextAuth({
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     KeycloakProvider({
+//       clientId: process.env.AUTH_RESOURCE!,
+//       clientSecret: process.env.AUTH_SECRET!,
+//       issuer: `${process.env.AUTH_SERVER_URL}/realms/${process.env.AUTH_RELM}`,
+//       profile(profile) {
+//         return {
+//           id: profile.sub,
+//           name: profile.name,
+//           email: profile.email,
+//           image: null,
+//         };
+//       },
+//     }),
+//   ],
+// });
+
+// export { handler as GET, handler as POST };
+
+
 import NextAuth, { User, Account } from "next-auth";
 import { JWT } from "next-auth/jwt";
 import { Session } from "next-auth";
@@ -42,6 +72,10 @@ export const authOptions = {
   //   error: "/auth/error", // Error code passed in query string as ?error=
   //   verifyRequest: "/auth/verify-request", // (used for check email message)
   //   newUser: "/auth/new-user" // New users will be directed here on first sign in (leave the property out if not of interest)
+  // },
+  // pages: {
+  //   signIn: '/api/auth/signin',
+  //   error: '/api/auth/error',
   // },
   callbacks: {
     async jwt({ token, account }: { token: JWT; account: Account | null }) {
