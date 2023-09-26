8a8c9fffbc72087416be4b89f760b5397788f208
okanji <oamarkanji@gmail.com>
2023-09-26 13:36:52 -0700

    added clear db in jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,27 @@
-import 'isomorphic-fetch'
+import "isomorphic-fetch";
 import "@testing-library/jest-dom/extend-expect";
 import { PrismaClient } from "@prisma/client";
 
+jest.setTimeout(30000);
+
 export const prisma = new PrismaClient();
+
+async function cleanUp() {
+  const deleteRequests = prisma.privateCloudRequest.deleteMany();
+  const deleteProjects = prisma.privateCloudProject.deleteMany();
+  const deleteRequestedProjects =
+    prisma.privateCloudRequestedProject.deleteMany();
+  const deleteUsers = prisma.user.deleteMany();
+
+  await prisma.$transaction([
+    deleteRequests,
+    deleteProjects,
+    deleteRequestedProjects,
+    deleteUsers,
+  ]);
+}
+
+afterAll(async () => {
+  await cleanUp();
+  await prisma.$disconnect();
+});
