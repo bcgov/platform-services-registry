8d63727acc747f3e5fffb177a3c48a28f77e78fe
okanji <oamarkanji@gmail.com>
2023-10-16 11:29:28 -0700

    removed settings and profile from drop down

--- components/nav/ProfileDropdown.tsx
+++ components/nav/ProfileDropdown.tsx
@@ -56,7 +56,7 @@ export default function ProfileDropdown() {
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <Menu.Item>
+          {/* <Menu.Item>
             {({ active }) => (
               <Link
                 href="#"
@@ -81,7 +81,7 @@ export default function ProfileDropdown() {
                 Settings
               </Link>
             )}
-          </Menu.Item>
+          </Menu.Item> */}
           <Menu.Item>
             {({ active }) => (
               <div>
