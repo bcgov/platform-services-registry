b6170d5393a6ce8f3e4363a2efb118ba8bff6035
okanji <oamarkanji@gmail.com>
2023-10-17 10:30:38 -0700

    fixed create button null pathname

--- components/buttons/CreateButton.tsx
+++ components/buttons/CreateButton.tsx
@@ -8,6 +8,8 @@ import Plus from "@/components/assets/plus.svg";
 export default function Tabs() {
   const pathname = usePathname();
 
+  if (!pathname) return null;
+
   return (
     <Link
       className="flex justify-center items-center mr-20 rounded-md bg-bcorange px-4 py-2 h-10 font-bcsans text-bcblue text-base font-light tracking-[.2em] shadow-sm hover:brightness-110 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm sm:px-3 sm:tracking-[.1em]"
