02e2edbd695278416608ec260a66145b09074786
calbertan <02c.albert@gmail.com>
2023-10-27 16:51:47 -0700

    image added to static folder in root dir

--- emails/components/Header.tsx
+++ emails/components/Header.tsx
@@ -1,11 +1,15 @@
-import Image from 'next/image';
-import Logo from '../assets/logo.png';
 import { Img, Text } from '@react-email/components';
 
 export default function Header() {
   return (
     <div className="flex flex-row border-b-3 border-bcorange bg-bcblue shadow">
-      <Img src="./logo.png" alt="BC Platform Services Product Registry" width={56} height={56} className="ml-2 mr-2" />
+      <Img
+        src="/static/logo.png"
+        alt="BC Platform Services Product Registry"
+        width={56}
+        height={50}
+        className="ml-2 mr-2"
+      />
       <div className="flex flex-row text-white">
         <Text className="text-lg mr-2 font-roboto font-thin">BC Platform Services</Text>
         <Text className="text-lg font-roboto font-normal">Product Registry</Text>
