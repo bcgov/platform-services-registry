b7a3788b34ba7b3dc8640a0a45b183abf8033495
Nijjar <Amritpal.Nijjar@gov.bc.ca>
2023-11-10 14:04:33 -0800

    modified api route for csv download, first attempt

--- app/api/private-cloud/allprojects/route.ts
+++ app/api/private-cloud/allprojects/route.ts
@@ -3,6 +3,7 @@ import { PrivateCloudProject } from '@prisma/client';
 import prisma from '@/lib/prisma';
 import { string, z } from 'zod';
 import { privateCloudProjectsPaginated } from '@/queries/paginated/private-cloud';
+import { parse, stringify } from 'csv-stringify/sync';
 // import { sendProvisionedEmails } from "../ches/emailHandlers.js";
 
 // See this for pagination: https://github.com/Puppo/it-s-prisma-time/blob/10-pagination/src/index.ts
@@ -12,7 +13,7 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
 
   const defaultPageSize = searchParams.get('defaultPageSize');
   const currentPage = searchParams.get('currentPage');
-  const search = searchParams.get('search');
+  const search = searchParams.get('search') || '';
   const ministry = searchParams.get('ministry');
   const cluster = searchParams.get('cluster');
   const userEmail = searchParams.get('email');
@@ -32,10 +33,24 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
     );
 
     if (!data) {
-      return new NextResponse('No data found.', {
+      return new NextResponse('No data found for the provided query', {
         status: 404,
       });
     }
+
+    // Convert the data to CSV
+    const csv = stringify(data, {
+      header: true,
+      columns: [], //insert fields here
+    });
+    // Response for csv
+    const response = new NextResponse(csv, {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/csv',
+        'Content-Disposition': 'attachment; filename="projects.csv"',
+      },
+    });
 
     return NextResponse.json(data);
   } catch (error: any) {
