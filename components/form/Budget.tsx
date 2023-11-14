80c9c6a0af50b041914bb825cc7fafc3a158edf5
Zhanna <65827870+Kolezhanchik@users.noreply.github.com>
2023-11-14 10:25:51 -0800

    Decimal budget/1254 (#1327)
    * checkout commit
    
    * feat(1254): decimal numbers for public cloud product budget input

--- components/form/Budget.tsx
+++ components/form/Budget.tsx
@@ -60,7 +60,6 @@ export default function Budget({ disabled }: { disabled?: boolean }) {
           <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900 mb-1">
             Total Estimated average monthly spend
           </label>
-
           <input
             type="number"
             className="bg-neutral-200 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
