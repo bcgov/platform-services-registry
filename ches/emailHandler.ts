ae35c58f4b4cfb66ebc2cbd39c110785d04aac04
Oamar Kanji <30703259+okanji@users.noreply.github.com>
2023-11-16 12:15:56 -0800

    Refactored Ches to not use class and hopefully work in dev branch (#1306)
    * refactored ches
    
    * changed variable names
    
    * re formatted
    
    * Cleaned up imports
    
    * removed filter boolean

--- ches/emailHandler.ts
+++ ches/emailHandler.ts
@@ -1,18 +1,17 @@
 import { render } from '@react-email/render';
-import { PrivateCloudCreateRequestBodySchema, PrivateCloudCreateRequestBody } from '@/schema';
 import { PrivateCloudRequestWithRequestedProject } from '@/requestActions/private-cloud/decisionRequest';
 import { NewRequestTemplate } from '@/emails/templates/NewRequestTemplate';
 import { RequestApprovalTemplate } from '@/emails/templates/RequestApprovalTemplate';
 import { RequestRejectionTemplate } from '@/emails/templates/RequestRejectionTemplate';
-import { adminEmails } from './emailConstant';
-import chesService from './index';
+import { adminEmails } from '@/ches/emailConstant';
+import { sendEmail } from '@/ches';
 
 export const sendNewRequestEmails = async (request: PrivateCloudRequestWithRequestedProject) => {
   const email = render(NewRequestTemplate({ request }), { pretty: true });
+
   try {
-    const send1 = chesService.send({
+    const contacts = await sendEmail({
       body: email,
-      // For all project contacts. Sent when the project set deletion request is successfully submitted
       to: [
         request.requestedProject.projectOwner.email,
         request.requestedProject.primaryTechnicalLead.email,
@@ -21,14 +20,14 @@ export const sendNewRequestEmails = async (request: PrivateCloudRequestWithReque
       subject: `${request.requestedProject.name} provisioning request received`,
     });
 
-    const send2 = chesService.send({
+    const admins = await sendEmail({
       bodyType: 'html',
       body: email,
       to: adminEmails,
       subject: `New Provisioning request in Registry waiting for your approval`,
     });
 
-    await Promise.all([send1, send2]);
+    await Promise.all([contacts, admins]);
   } catch (error) {
     console.log('ERROR SENDING NEW REQUEST EMAIL EMAIL');
   }
@@ -36,10 +35,10 @@ export const sendNewRequestEmails = async (request: PrivateCloudRequestWithReque
 
 export const sendRequestApprovalEmails = async (request: PrivateCloudRequestWithRequestedProject) => {
   const email = render(RequestApprovalTemplate({ request }), { pretty: true });
+
   try {
-    await chesService.send({
+    await sendEmail({
       body: email,
-      // For all project contacts. Sent when the project set deletion request is successfully submitted
       to: [
         request.requestedProject.projectOwner.email,
         request.requestedProject.primaryTechnicalLead.email,
@@ -54,10 +53,10 @@ export const sendRequestApprovalEmails = async (request: PrivateCloudRequestWith
 
 export const sendRequestRejectionEmails = async (request: PrivateCloudRequestWithRequestedProject, message: String) => {
   const email = render(RequestRejectionTemplate({ request }), { pretty: true });
+
   try {
-    await chesService.send({
+    await sendEmail({
       body: email,
-      // For all project contacts. Sent when the project set deletion request is successfully submitted
       to: [
         request.requestedProject.projectOwner.email,
         request.requestedProject.primaryTechnicalLead.email,
