ac0ba1c1aa194d19dd46d6a9cc786662d49f52ce
calbertan <02c.albert@gmail.com>
2023-11-21 13:19:03 -0800

    fix: update react-emails/tailwind

--- emails/components/Header.tsx
+++ emails/components/Header.tsx
@@ -1,55 +1,31 @@
-import { Img, Text } from '@react-email/components';
+import Image from 'next/image';
+import Logo from '../assets/logo.png';
+import { Button, Heading, Img, Link, Tailwind, Text } from '@react-email/components';
+import { PrivateCloudCreateRequestBody } from '@/schema';
 import { TailwindConfig } from './TailwindConfig';
 
-interface Styles {
-  [key: string]: React.CSSProperties;
-}
-
-const containerStyles: React.CSSProperties = {
-  display: 'flex',
-  flexDirection: 'row',
-  borderBottom: '3px solid #FFA500',
-  backgroundColor: '#003366',
-  boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-};
-
-const imgStyles: React.CSSProperties = {
-  margin: 'auto 8px',
-};
-
-const textContainerStyles: React.CSSProperties = {
-  display: 'flex',
-  flexDirection: 'row',
-  color: '#ffffff',
-};
-
-const textStyles: React.CSSProperties = {
-  fontSize: '1.25rem',
-  fontFamily: 'Roboto',
-  fontWeight: 300,
-};
-
-const boldTextStyles: React.CSSProperties = {
-  ...textStyles,
-  fontWeight: 'normal',
-};
-
-export default function Header() {
+export default function NamespaceDetails({ cluster, licencePlate }: { cluster: string; licencePlate?: string }) {
   return (
-    <div>
-      <div style={containerStyles}>
-        <Img
-          src={`${process.env.BASE_URL}/logo.png`}
-          alt="BC Platform Services Product Registry"
-          width={58}
-          height={41}
-          style={imgStyles}
-        />
-        <div style={textContainerStyles}>
-          <Text style={textStyles}>BC Platform Services</Text>
-          <Text style={boldTextStyles}>Product Registry</Text>
+    <Tailwind config={TailwindConfig}>
+      <div>
+        <Heading className="text-lg">Namespace Details</Heading>
+        <div>
+          <Text className="mb-0 font-semibold h-4">OpenShift Cluster: </Text>
+          <Text className="mt-0 h-4">{cluster}</Text>
         </div>
+        {licencePlate && (
+          <>
+            <Text className="mb-0 font-semibold h-4">Development Namespace: </Text>
+            <Text className="mt-0 h-4">{licencePlate}-dev</Text>
+            <Text className="mb-0 font-semibold h-4">Test Namespace: </Text>
+            <Text className="mt-0 h-4">{licencePlate}-test</Text>
+            <Text className="mb-0 font-semibold h-4">Production Namespace: </Text>
+            <Text className="mt-0 h-4">{licencePlate}-prod</Text>
+            <Text className="mb-0 font-semibold h-4">Tools Namespace: </Text>
+            <Text className="mt-0 h-4">{licencePlate}-tools</Text>
+          </>
+        )}
       </div>
-    </div>
+    </Tailwind>
   );
 }
