08c68b79b87a513bd1da6419b80c9d1ab8be2ded
calbertan <02c.albert@gmail.com>
2023-11-21 17:06:27 -0800

    fix: remove redundant tailwind imports

--- emails/components/Edit/ContactChanges.tsx
+++ emails/components/Edit/ContactChanges.tsx
@@ -1,8 +1,4 @@
-import Image from 'next/image';
-import Logo from '../assets/logo.png';
-import { Button, Heading, Img, Link, Tailwind, Text } from '@react-email/components';
-import { PrivateCloudCreateRequestBody } from '@/schema';
-import { TailwindConfig } from '../TailwindConfig';
+import { Heading, Link, Text } from '@react-email/components';
 import { User } from '@prisma/client';
 
 export default function ContactChanges({
@@ -21,76 +17,74 @@ export default function ContactChanges({
   tl2Requested: User | null;
 }) {
   return (
-    <Tailwind config={TailwindConfig}>
-      <div>
-        <Heading className="text-lg text-black">Contact Changes</Heading>
-        {poCurrent.id !== poRequested.id && (
-          <div>
-            <Text className="mb-0 font-semibold h-4">Current Product Owner: </Text>
-            <Text className="mt-0 mb-0 h-4">
-              {poCurrent.firstName} {poCurrent.lastName}
-            </Text>
-            <Link className="mt-0 h-4" href={`mailto:${poCurrent.email}`}>
-              {poCurrent.email}
-            </Link>
-            <Text className="mb-0 font-semibold h-4">Requested Product Owner: </Text>
-            <Text className="mt-0 mb-0 h-4">
-              {poRequested.firstName} {poRequested.lastName}
-            </Text>
-            <Link className="mt-0 h-4" href={`mailto:${poRequested.email}`}>
-              {poRequested.email}
-            </Link>
-          </div>
-        )}
-        {tl1Current.id !== tl1Requested.id && (
-          <div>
-            <Text className="mb-0 font-semibold h-4">Current Primary Technical Lead: </Text>
-            <Text className="mt-0 mb-0 h-4">
-              {tl1Current.firstName} {tl1Current.lastName}
-            </Text>
-            <Link className="mt-0 h-4" href={`mailto:${tl1Current.email}`}>
-              {tl1Current.email}
-            </Link>
-            <Text className="mb-0 font-semibold h-4">Requested Primary Technical Lead: </Text>
-            <Text className="mt-0 mb-0 h-4">
-              {tl1Requested.firstName} {tl1Requested.lastName}
-            </Text>
-            <Link className="mt-0 h-4" href={`mailto:${tl1Requested.email}`}>
-              {tl1Requested.email}
-            </Link>
-          </div>
-        )}
-        {tl2Current?.id !== tl2Requested?.id && (
-          <div>
-            <Text className="mb-0 font-semibold h-4">Current Secondary Technical Lead: </Text>
-            {tl2Current ? (
-              <div>
-                <Text className="mt-0 h-4">
-                  {tl2Current.firstName} {tl2Current.lastName}
-                </Text>
-                <Link className="mt-0 h-4" href={`mailto:${tl2Current?.email}`}>
-                  {tl2Current?.email}
-                </Link>
-              </div>
-            ) : (
-              <Text className="mt-0 h-4"> - </Text>
-            )}
-            <Text className="mb-0 font-semibold h-4">Requested Secondary Technical Lead: </Text>
-            {tl2Requested ? (
-              <div>
-                <Text className="mt-0 h-4">
-                  {tl2Requested.firstName} {tl2Requested.lastName}
-                </Text>
-                <Link className="mt-0 h-4" href={`mailto:${tl2Requested?.email}`}>
-                  {tl2Requested?.email}
-                </Link>
-              </div>
-            ) : (
-              <Text className="mt-0 h-4"> Removed </Text>
-            )}
-          </div>
-        )}
-      </div>
-    </Tailwind>
+    <div>
+      <Heading className="text-lg text-black">Contact Changes</Heading>
+      {poCurrent.id !== poRequested.id && (
+        <div>
+          <Text className="mb-0 font-semibold h-4">Current Product Owner: </Text>
+          <Text className="mt-0 mb-0 h-4">
+            {poCurrent.firstName} {poCurrent.lastName}
+          </Text>
+          <Link className="mt-0 h-4" href={`mailto:${poCurrent.email}`}>
+            {poCurrent.email}
+          </Link>
+          <Text className="mb-0 font-semibold h-4">Requested Product Owner: </Text>
+          <Text className="mt-0 mb-0 h-4">
+            {poRequested.firstName} {poRequested.lastName}
+          </Text>
+          <Link className="mt-0 h-4" href={`mailto:${poRequested.email}`}>
+            {poRequested.email}
+          </Link>
+        </div>
+      )}
+      {tl1Current.id !== tl1Requested.id && (
+        <div>
+          <Text className="mb-0 font-semibold h-4">Current Primary Technical Lead: </Text>
+          <Text className="mt-0 mb-0 h-4">
+            {tl1Current.firstName} {tl1Current.lastName}
+          </Text>
+          <Link className="mt-0 h-4" href={`mailto:${tl1Current.email}`}>
+            {tl1Current.email}
+          </Link>
+          <Text className="mb-0 font-semibold h-4">Requested Primary Technical Lead: </Text>
+          <Text className="mt-0 mb-0 h-4">
+            {tl1Requested.firstName} {tl1Requested.lastName}
+          </Text>
+          <Link className="mt-0 h-4" href={`mailto:${tl1Requested.email}`}>
+            {tl1Requested.email}
+          </Link>
+        </div>
+      )}
+      {tl2Current?.id !== tl2Requested?.id && (
+        <div>
+          <Text className="mb-0 font-semibold h-4">Current Secondary Technical Lead: </Text>
+          {tl2Current ? (
+            <div>
+              <Text className="mt-0 h-4">
+                {tl2Current.firstName} {tl2Current.lastName}
+              </Text>
+              <Link className="mt-0 h-4" href={`mailto:${tl2Current?.email}`}>
+                {tl2Current?.email}
+              </Link>
+            </div>
+          ) : (
+            <Text className="mt-0 h-4"> - </Text>
+          )}
+          <Text className="mb-0 font-semibold h-4">Requested Secondary Technical Lead: </Text>
+          {tl2Requested ? (
+            <div>
+              <Text className="mt-0 h-4">
+                {tl2Requested.firstName} {tl2Requested.lastName}
+              </Text>
+              <Link className="mt-0 h-4" href={`mailto:${tl2Requested?.email}`}>
+                {tl2Requested?.email}
+              </Link>
+            </div>
+          ) : (
+            <Text className="mt-0 h-4"> Removed </Text>
+          )}
+        </div>
+      )}
+    </div>
   );
 }
--- emails/components/Header.tsx
+++ emails/components/Header.tsx
@@ -1,23 +1,19 @@
-import { Img, Tailwind, Text } from '@react-email/components';
-import { TailwindConfig } from './TailwindConfig';
+import { Img, Text } from '@react-email/components';
 
 export default function Header() {
   return (
-    <Tailwind config={TailwindConfig}>
-      <div className="flex flex-row border-b-3 border-bcorange bg-bcblue shadow">
-        {/* <Img src="/static/logo.png" alt="BC Platform Services Product Registry" width={58} height={41} className="m-auto ml-2 mr-2" /> */}
-        <Img
-          src={`${process.env.BASE_URL}/logo.png`}
-          alt="BC Platform Services Product Registry"
-          width={58}
-          height={41}
-          className="m-auto ml-2 mr-2"
-        />
-        <div className="flex flex-row text-white">
-          <Text className="text-lg mr-2 font-roboto font-thin">BC Platform Services</Text>
-          <Text className="text-lg font-roboto font-normal">Product Registry</Text>
-        </div>
+    <div className="flex flex-row border-b-3 border-bcorange bg-bcblue shadow">
+      <Img
+        src={`${process.env.BASE_URL}/logo.png`}
+        alt="BC Platform Services Product Registry"
+        width={58}
+        height={41}
+        className="m-auto ml-2 mr-2"
+      />
+      <div className="flex flex-row text-white">
+        <Text className="text-lg mr-2 font-roboto font-thin">BC Platform Services</Text>
+        <Text className="text-lg font-roboto font-normal">Product Registry</Text>
       </div>
-    </Tailwind>
+    </div>
   );
 }
