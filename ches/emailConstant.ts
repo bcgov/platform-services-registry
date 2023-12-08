e4a94d9e3741d4c7d8135582ef229660f5211685
calbertan <02c.albert@gmail.com>
2023-12-08 13:56:46 -0800

    chore: revert some changes

--- ches/emailConstant.ts
+++ ches/emailConstant.ts
@@ -147,11 +147,6 @@ export const ministriesNames = [
     name: 'TRAN',
     humanFriendlyName: 'Transportation and Infrastructure',
   },
-  {
-    id: 30,
-    name: 'HOUS',
-    humanFriendlyName: 'Housing',
-  },
 ];
 
 // const ministriesNamesUpdated = [
@@ -210,7 +205,7 @@ export const ministriesNames = [
 //     name: "HLTH",
 //     humanFriendlyName: "Health"
 //   },
-//  {
+//   {
 //     id: 12,
 //     name: "HOUS",
 //     humanFriendlyName: "Housing"
