71d72327693eeaf966596fa22a1f078fd3040ca1
calbertan <02c.albert@gmail.com>
2023-12-08 15:47:06 -0800

    remove changes to emailConstant

--- ches/emailConstant.ts
+++ ches/emailConstant.ts
@@ -1,7 +1,7 @@
 export const adminEmails = `${process.env.PRIVATE_ADMIN_EMAILS}`.split(',');
 export const adminPublicEmails = `${process.env.PUBLIC_ADMIN_EMAILS}`.split(',');
 
-export const oldMinistriesNames = [
+export const ministriesNames = [
   {
     id: 1,
     name: 'AEST',
@@ -149,123 +149,123 @@ export const oldMinistriesNames = [
   },
 ];
 
-const ministriesNames = [
-  {
-    id: 1,
-    name: 'AF',
-    humanFriendlyName: 'Agriculture and Food',
-  },
-  {
-    id: 2,
-    name: 'AG',
-    humanFriendlyName: 'Attorney General',
-  },
-  {
-    id: 3,
-    name: 'MSFD',
-    humanFriendlyName: 'Children and Family Development',
-  },
-  {
-    id: 4,
-    name: 'CITZ',
-    humanFriendlyName: "Citizens' Services",
-  },
-  {
-    id: 5,
-    name: 'ECC',
-    humanFriendlyName: 'Education and Child Care',
-  },
-  {
-    id: 6,
-    name: 'EMCR',
-    humanFriendlyName: 'Emergency Management and Climate Readiness',
-  },
-  {
-    id: 7,
-    name: 'EMLI',
-    humanFriendlyName: 'Energy, Mines and Low Carbon Innovation',
-  },
-  {
-    id: 8,
-    name: 'ENV',
-    humanFriendlyName: 'Environment and Climate Change Strategy',
-  },
-  {
-    id: 9,
-    name: 'FIN',
-    humanFriendlyName: 'Finance',
-  },
-  {
-    id: 10,
-    name: 'FOR',
-    humanFriendlyName: 'Forests',
-  },
-  {
-    id: 11,
-    name: 'HLTH',
-    humanFriendlyName: 'Health',
-  },
-  {
-    id: 12,
-    name: 'HOUS',
-    humanFriendlyName: 'Housing',
-  },
-  {
-    id: 13,
-    name: 'IRR',
-    humanFriendlyName: 'Indigenous Relations & Reconciliation',
-  },
-  {
-    id: 14,
-    name: 'JEDI',
-    humanFriendlyName: 'Jobs, Economic Development and Innovation',
-  },
-  {
-    id: 15,
-    name: 'LBR',
-    humanFriendlyName: 'Labour',
-  },
-  {
-    id: 16,
-    name: 'MMHA',
-    humanFriendlyName: 'Mental Health and Addictions',
-  },
-  {
-    id: 17,
-    name: 'MUNI',
-    humanFriendlyName: 'Municipal Affairs',
-  },
-  {
-    id: 18,
-    name: 'PSFS',
-    humanFriendlyName: 'Post-Secondary Education and Future Skills',
-  },
-  {
-    id: 19,
-    name: 'PSSG',
-    humanFriendlyName: 'Public Safety and Solicitor General',
-  },
-  {
-    id: 20,
-    name: 'SDPR',
-    humanFriendlyName: 'Social Development and Poverty Reduction',
-  },
-  {
-    id: 21,
-    name: 'TACS',
-    humanFriendlyName: 'Tourism, Arts, Culture and Sport',
-  },
-  {
-    id: 22,
-    name: 'MOTI',
-    humanFriendlyName: 'Transportation and Infrastructure',
-  },
-  {
-    id: 23,
-    name: 'WLRS',
-    humanFriendlyName: 'Water, Land and Resource Stewardship',
-  },
-];
+// const ministriesNamesUpdated = [
+//   {
+//     id: 1,
+//     name: "AF",
+//     humanFriendlyName: "Agriculture and Food"
+//   },
+//   {
+//     id: 2,
+//     name: "AG",
+//     humanFriendlyName: "Attorney General"
+//   },
+//   {
+//     id: 3,
+//     name: "MSFD",
+//     humanFriendlyName: "Children and Family Development"
+//   },
+//   {
+//     id: 4,
+//     name: "CITZ",
+//     humanFriendlyName: "Citizens' Services"
+//   },
+//   {
+//     id: 5,
+//     name: "ECC",
+//     humanFriendlyName: "Education and Child Care"
+//   },
+//   {
+//     id: 6,
+//     name: "EMCR",
+//     humanFriendlyName: "Emergency Management and Climate Readiness"
+//   },
+//   {
+//     id: 7,
+//     name: "EMLI",
+//     humanFriendlyName: "Energy, Mines and Low Carbon Innovation"
+//   },
+//   {
+//     id: 8,
+//     name: "ENV",
+//     humanFriendlyName: "Environment and Climate Change Strategy"
+//   },
+//   {
+//     id: 9,
+//     name: "FIN",
+//     humanFriendlyName: "Finance"
+//   },
+//   {
+//     id: 10,
+//     name: "FOR",
+//     humanFriendlyName: "Forests"
+//   },
+//   {
+//     id: 11,
+//     name: "HLTH",
+//     humanFriendlyName: "Health"
+//   },
+//   {
+//     id: 12,
+//     name: "HOUS",
+//     humanFriendlyName: "Housing"
+//   },
+//   {
+//     id: 13,
+//     name: "IRR",
+//     humanFriendlyName: "Indigenous Relations & Reconciliation"
+//   },
+//   {
+//     id: 14,
+//     name: "JEDI",
+//     humanFriendlyName: "Jobs, Economic Development and Innovation"
+//   },
+//   {
+//     id: 15,
+//     name: "LBR",
+//     humanFriendlyName: "Labour"
+//   },
+//   {
+//     id: 16,
+//     name: "MMHA",
+//     humanFriendlyName: "Mental Health and Addictions"
+//   },
+//   {
+//     id: 17,
+//     name: "MUNI",
+//     humanFriendlyName: "Municipal Affairs"
+//   },
+//   {
+//     id: 18,
+//     name: "PSFS",
+//     humanFriendlyName: "Post-Secondary Education and Future Skills"
+//   },
+//   {
+//     id: 19,
+//     name: "PSSG",
+//     humanFriendlyName: "Public Safety and Solicitor General"
+//   },
+//   {
+//     id: 20,
+//     name: "SDPR",
+//     humanFriendlyName: "Social Development and Poverty Reduction"
+//   },
+//   {
+//     id: 21,
+//     name: "TACS",
+//     humanFriendlyName: "Tourism, Arts, Culture and Sport"
+//   },
+//   {
+//     id: 22,
+//     name: "MOTI",
+//     humanFriendlyName: "Transportation and Infrastructure"
+//   },
+//   {
+//     id: 23,
+//     name: "WLRS",
+//     humanFriendlyName: "Water, Land and Resource Stewardship"
+//   },
+// ];
 
 export const clusterNames = [
   {
