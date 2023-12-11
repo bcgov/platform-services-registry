4e2b7bc028254f5b9e9f10f6cdec3a4e0b9ef37d
Kolezanchik <zhanna.kolesnyk@gov.bc.ca>
2023-12-11 12:37:02 -0800

    pr review fixies

--- app/api/public-cloud/aws-roles/routes.ts
+++ app/api/public-cloud/aws-roles/routes.ts
@@ -18,7 +18,12 @@ interface User {
   email: string;
 }
 
-async function getToken(): Promise<string | undefined> {
+const parseError = (error: unknown): void => {
+  if (error instanceof Error) {
+    console.log(error.message);
+  } else console.log(String(error));
+};
+const getToken = async (): Promise<string | undefined> => {
   try {
     const apiUrl = `${process.env.AWS_ROLES_BASE_URL}/realms/${process.env.AWS_ROLES_REALM_NAME}/protocol/openid-connect/token`;
     const requestBody = {
@@ -33,148 +38,103 @@ async function getToken(): Promise<string | undefined> {
     });
     return response.data.access_token;
   } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    } else console.log(String(error));
+    parseError(error);
   }
-}
+};
 
-async function getGroups(): Promise<Group[] | undefined> {
-  const accessToken = await getToken();
-  try {
-    const apiUrl = `${process.env.AWS_ROLES_BASE_URL}/admin/realms/${process.env.AWS_ROLES_REALM_NAME}/groups`;
-    const response = await axios.get(apiUrl, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+const awsRolesApiInstance = axios.create({
+  baseURL: `${process.env.AWS_ROLES_BASE_URL}/admin/realms/${process.env.AWS_ROLES_REALM_NAME}`,
+});
+
+awsRolesApiInstance.interceptors.request.use(
+  async (config) => {
+    const token = await getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+const getGroups: Promise<Group[] | undefined> = awsRolesApiInstance
+  .get('/groups')
+  .then((response) => {
     return response.data;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    } else console.log(String(error));
-  }
-}
+  })
+  .catch((error: unknown) => {
+    parseError(error);
+  });
+
 //search by substring, returns all of groups, which names includes searchParam
-export async function getGroupByName(groupName: string): Promise<Group[] | undefined> {
-  const accessToken = await getToken();
-  try {
-    const apiUrl = `${process.env.AWS_ROLES_BASE_URL}/admin/realms/${process.env.AWS_ROLES_REALM_NAME}/groups`;
-    const response = await axios.get(apiUrl, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
+export const getGroupByName = (groupName: string = 'Project Team Groups'): Promise<Group[] | undefined> =>
+  awsRolesApiInstance
+    .get('/groups', {
       params: {
         search: groupName,
       },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error: unknown) => {
+      parseError(error);
     });
-    return response.data;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    } else console.log(String(error));
-  }
-}
 
-export async function getUsers(): Promise<User[] | undefined> {
-  const accessToken = await getToken();
-  try {
-    const apiUrl = `${process.env.AWS_ROLES_BASE_URL}/admin/realms/${process.env.AWS_ROLES_REALM_NAME}/users`;
-    const response = await axios.get(apiUrl, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
+export const getUsers: Promise<Group[] | undefined> = awsRolesApiInstance
+  .get('/users')
+  .then((response) => {
+    return response.data;
+  })
+  .catch((error: unknown) => {
+    parseError(error);
+  });
+
+export const getMembersByGroupId = (groupId: string): Promise<Group[] | undefined> =>
+  awsRolesApiInstance
+    .get(`/groups/${groupId}/members`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error: unknown) => {
+      parseError(error);
     });
-    return response.data;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    } else console.log(String(error));
-  }
-}
 
-export async function getMembersByGroupId(groupId: string): Promise<User[] | undefined> {
-  const accessToken = await getToken();
-  try {
-    const apiUrl = `${process.env.AWS_ROLES_BASE_URL}/admin/realms/${process.env.AWS_ROLES_REALM_NAME}/groups/${groupId}/members`;
-    const response = await axios.get(apiUrl, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    console.log(response.data);
-    return response.data;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    } else console.log(String(error));
-  }
-}
-// getMembersByGroupId("734122b2-1b80-4fc4-a614-21d94773e3cb")
-
-export async function getUserByEmail(email: string): Promise<User[] | undefined> {
-  const accessToken = await getToken();
-  try {
-    const apiUrl = `${process.env.AWS_ROLES_BASE_URL}/admin/realms/${process.env.AWS_ROLES_REALM_NAME}/users`;
-    const response = await axios.get(apiUrl, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
+export const getUserByEmail = (email: string): Promise<User[] | undefined> =>
+  awsRolesApiInstance
+    .get('/users', {
       params: {
-        email: email,
+        search: email,
       },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error: unknown) => {
+      parseError(error);
     });
-    return response.data;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    } else console.log(String(error));
-  }
-}
 
-export async function addUserToGroup(userId: string, groupId: string) {
-  const accessToken = await getToken();
-  try {
-    const apiUrl = `${process.env.AWS_ROLES_BASE_URL}/admin/realms/${process.env.AWS_ROLES_REALM_NAME}/users/${userId}/groups/${groupId}/`;
-
-    const response = await fetch(apiUrl, {
-      method: 'PUT',
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
+export const addUserToGroup = (userId: string, groupId: string) =>
+  awsRolesApiInstance
+    .put(`/users/${userId}/groups/${groupId}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error: unknown) => {
+      parseError(error);
     });
-    if (!response.ok) {
-      console.error('Failed to fetch data.');
-    }
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    } else console.log(String(error));
-  }
-}
 
-export async function removeUserFromGroup(userId: string, groupId: string) {
-  const accessToken = await getToken();
-  try {
-    const apiUrl = `${process.env.AWS_ROLES_BASE_URL}/admin/realms/${process.env.AWS_ROLES_REALM_NAME}/users/${userId}/groups/${groupId}/`;
-
-    const response = await fetch(apiUrl, {
-      method: 'DELETE',
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
+export const removeUserFromGroup = (userId: string, groupId: string) =>
+  awsRolesApiInstance
+    .delete(`/users/${userId}/groups/${groupId}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error: unknown) => {
+      parseError(error);
     });
-    if (!response.ok) {
-      console.error('Failed to fetch data.');
-    }
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    } else console.log(String(error));
-  }
-}
-
-// removeUserFromGroup("31b8fdbf-56fa-48f6-a02d-6fb44e1d9933", "734122b2-1b80-4fc4-a614-21d94773e3cb")
 
 const findObjectByValue = (array: Group[], key: keyof Group, value: any): Group[] | undefined => {
   return array.filter((obj) => obj[key] === value);
@@ -185,11 +145,10 @@ const findObjectByValueSubstring = (array: Group[], key: keyof Group, value: any
 };
 
 export async function getProductAWSRoles(licencePlate: string = 'eu9cfk'): Promise<Group[] | undefined> {
-  const keyClockGroups = await getGroups();
+  const keyClockGroups = await getGroups;
   if (keyClockGroups) {
     const projectTeamGroups = findObjectByValue(keyClockGroups, 'name', 'Project Team Groups');
     if (projectTeamGroups) {
-      // console.log(findObjectByValueSubstring(projectTeamGroups[0].subGroups, "name", licencePlate)![0].subGroups[0].id)
       return findObjectByValueSubstring(projectTeamGroups[0].subGroups, 'name', licencePlate);
     }
   }
