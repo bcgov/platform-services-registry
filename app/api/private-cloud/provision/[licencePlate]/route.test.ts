6b1131cc262d917cc11b4310f9cf5056411e3546
Kolezanchik <zhanna.kolesnyk@gov.bc.ca>
2023-12-13 14:10:55 -0800

    branch problems

--- app/api/private-cloud/provision/[licencePlate]/route.test.ts
+++ app/api/private-cloud/provision/[licencePlate]/route.test.ts
@@ -14,18 +14,18 @@ const createRequestBody = {
   name: 'Sample Project',
   description: 'This is a sample project description.',
   cluster: 'SILVER', // Assuming CLUSTER_A is a valid enum value for Cluster
-  ministry: 'AGRI', // Assuming AGRI is a valid enum value for Ministry
+  ministry: 'AG', // Assuming AG is a valid enum value for Ministry
   projectOwner: {
     firstName: 'John',
     lastName: 'Doe',
     email: 'oamar.kanji@gov.bc.ca',
-    ministry: 'AGRI', // Assuming AGRI is a valid enum value for Ministry
+    ministry: 'AG', // Assuming AG is a valid enum value for Ministry
   },
   primaryTechnicalLead: {
     firstName: 'Jane',
     lastName: 'Smith',
     email: 'jane.smith@example.com',
-    ministry: 'AGRI', // Assuming AGRI is a valid enum value for Ministry
+    ministry: 'AG', // Assuming AG is a valid enum value for Ministry
   },
   commonComponents: {
     addressAndGeolocation: {
@@ -82,7 +82,7 @@ const adminChanges = {
     firstName: 'James',
     lastName: 'Tim',
     email: 'jamestim@gov.bc.ca',
-    ministry: 'AGRI',
+    ministry: 'AG',
   },
   testQuota: {
     cpu: 'CPU_REQUEST_8_LIMIT_16',
