34b7e69759f6ae726734b0f533807ec3bb6ac85f
Oamar Kanji <oamarkanji@gmail.com>
2023-12-12 22:59:54 -0800

    Removed commented out code

--- components/tabs/AllActiveTabs.tsx
+++ components/tabs/AllActiveTabs.tsx
@@ -43,33 +43,3 @@ export default function Tabs() {
   );
 }
 
-// export default function Tabs() {
-//   const pathname = usePathname();
-
-//   return (
-//     <span className="isolate inline-flex rounded-md h-10">
-//       <Link
-//         style={{ width: 97 }}
-//         type="button"
-//         href={`/${pathname.split('/')[1]}/products`}
-//         className={`pl-4 relative inline-flex items-center rounded-l-lg px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-bcblue focus:z-10
-//           ${
-//             pathname.split('/')[2] === 'products' ? 'bg-bcblue text-white' : 'bg-white text-gray-900 hover:bg-gray-100'
-//           }`}
-//       >
-//         Active
-//       </Link>
-//       <Link
-//         style={{ width: 97 }}
-//         type="button"
-//         href={`/${pathname.split('/')[1]}/requests`}
-//         className={`pl-5 relative -ml-px inline-flex items-center rounded-r-lg px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-bcblue focus:z-10
-//           ${
-//             pathname.split('/')[2] === 'requests' ? 'bg-bcblue text-white' : 'bg-white text-gray-900 hover:bg-gray-100'
-//           }`}
-//       >
-//         All
-//       </Link>
-//     </span>
-//   );
-// }
