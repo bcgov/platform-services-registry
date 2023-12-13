cde247cc8dfec7586d14c19de3806ac4bb4f87ff
Zhanna <65827870+Kolezhanchik@users.noreply.github.com>
2023-12-13 15:55:04 -0800

    Feat/1078 (#1579)
    * added aws roles
    
    * fix(deps): update dependency @tanstack/react-query to v5.13.4
    
    * fix(deps): update dependency @types/react to v18.2.43
    
    * chore(deps): update dependency pre-commit to v3.6.0
    
    * fix(deps): update dependency @auth/mongodb-adapter to v2.0.8
    
    * chore(deps): update dependency @typescript-eslint/eslint-plugin to v6.14.0
    
    * fix(deps): update dependency react-hook-form to v7.49.0
    
    * fix(deps): update dependency @auth/prisma-adapter to v1.0.10
    
    * chore(deps): update dependency python to v3.12.1
    
    * feat=add env prefix for email subject and heading if its not 'Prod' for privatecloud emails
    
    * fix: border not showing in email header
    
    * fix: remove commented out lines
    
    * chore: revert some changes
    
    * feat: create config.ts for common variables
    
    * update config.ts
    
    * add default value to config.ts
    
    * remove changes to emailConstant
    
    * fix: updated emailHandler to grab value from config.ts
    
    * fix: prefix applies to sendEmail level
    
    * add newline
    
    * feat: new modal for approve/deny an edit request.
    
    * add useForm for public cloud
    
    * cleanup pr
    
    * PR review fixies
    
    * fix(deps): update dependency react-hook-form to v7.49.1
    
    * filter for public clo9ud provider (#1557)
    
    * filter for public clo9ud provider
    
    * review changes
    
    * remove unecessary bool in conditional
    
    * chore(deps): update dependency kubectl to v1.29.0
    
    * fix(deps): update dependency react-hook-form to v7.49.2
    
    * fix(deps): update dependency @auth/prisma-adapter to v1.0.11
    
    * fix(deps): update dependency @types/react to v18.2.45
    
    * fix(deps): update dependency @auth/mongodb-adapter to v2.0.9
    
    * fix: update .gitignore for react-email
    
    * add react-email in docs
    
    * fix: .react-email in .gitignore
    
    * fix dockerfile name
    
    * fix: react-email misspell
    
    * remove .react-email from eslint
    
    * revert ministry rename from AGRI
    
    * clean pr
    
    * feat: update react-email doc
    
    * chore: add next.config_.react-email for successful react-email
    
    * chore: add BASE_URL in email Dockerfile
    
    * PR review fixies
    
    * branch problems
    
    * confilcts
    
    * confilcts
    
    * deleted .react-email folder
    
    * Update .pre-commit-config.yaml
    
    * Update route.test.ts
    
    * Update route.test.ts
    
    * Update route.test.ts
    
    * Update route.test.ts
    
    * Update Params.ts
    
    * Update constants.ts
    
    * Update index.test.ts
    
    * Update sampleNatsMessage.json
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: calbertan <02c.albert@gmail.com>
    Co-authored-by: j.dev <junmin.dev@outlook.com>
    Co-authored-by: Oamar Kanji <oamarkanji@gmail.com>
    Co-authored-by: Oamar Kanji <30703259+okanji@users.noreply.github.com>

--- app/(request)/public-cloud/edit/layout.tsx
+++ app/(request)/public-cloud/edit/layout.tsx
@@ -1,10 +1,23 @@
-import PublicProductTabs from '@/components/tabs/PublicProductTabs';
+import CloudTabs from '@/components/tabs/CloudTabs';
+
+const tabsData = [
+  {
+    name: 'PUBLIC CLOUD EDIT',
+    href: '',
+    subHref: 'product',
+  },
+  {
+    name: 'PUBLIC CLOUD USER ROLES',
+    href: '',
+    subHref: 'roles',
+  },
+];
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <PublicProductTabs />
-      <div className="mt-8 mb-20 h-full">{children}</div>
+      <CloudTabs tabs={tabsData} />
+      <div className="mt-8 mb-20 h-full mx-4 lg:mx-20">{children}</div>
     </div>
   );
 }
