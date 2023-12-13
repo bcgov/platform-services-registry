298eb2f58587feae6f4f8a5ad7e5bfa01f4ecd0c
j.dev <junmin.dev@outlook.com>
2023-12-13 13:57:49 -0800

    ci: add oc deployer for tools namespace

--- terraform/openshift-deployer/101ed4-tools/config.tf
+++ terraform/openshift-deployer/101ed4-tools/config.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.7"
 
   backend "kubernetes" {
-    namespace     = "101ed4-test"
+    namespace     = "101ed4-tools"
     secret_suffix = "state" # pragma: allowlist secret
     config_path   = "~/.kube/config"
   }
