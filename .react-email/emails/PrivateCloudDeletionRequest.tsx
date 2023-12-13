235e621252879cfc70fd13479001e6fe30a594cb
j.dev <junmin.dev@outlook.com>
2023-12-13 11:32:24 -0800

    chore: add next.config_.react-email for successful react-email

--- .react-email/emails/PrivateCloudDeletionRequest.tsx
+++
@@ -1,2 +0,0 @@
-import Mail from '../../emails/PrivateCloudDeletionRequest.tsx';
-export default Mail;
--- .react-email/emails/PrivateCloudEditRequest.tsx
+++
@@ -1,2 +0,0 @@
-import Mail from '../../emails/PrivateCloudEditRequest.tsx';
-export default Mail;
