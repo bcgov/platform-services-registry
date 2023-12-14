27492f4b65511562a8457ffcac4733710669914a
Zhanna <65827870+Kolezhanchik@users.noreply.github.com>
2023-12-14 09:58:10 -0800

    added HOUS ministry (#1583)

--- components/form/Comment.tsx
+++ components/form/Comment.tsx
@@ -1,5 +1,6 @@
 import { useFormContext } from 'react-hook-form';
 import classNames from '@/components/utils/classnames';
+import { providers, ministries } from '@/constants';
 
 export default function AdminComment() {
   const {
@@ -73,9 +74,11 @@ export default function AdminComment() {
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             >
               <option value="">Select Ministry</option>
-              <option>CITZ</option>
-              <option>PSA</option>
-              <option>HLTH</option>
+              {ministries.map((ministry) => (
+                <option key={ministry} value={ministry}>
+                  {ministry}
+                </option>
+              ))}
             </select>
 
             <p className={classNames(errors.ministry ? 'text-red-400' : '', 'mt-3 text-sm leading-6 text-gray-600')}>
