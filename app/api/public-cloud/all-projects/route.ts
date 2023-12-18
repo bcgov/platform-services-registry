c01cfde03b1d9d94c3f7f3fa13f056420bd119a7
Oamar Kanji <oamarkanji@gmail.com>
2023-12-18 14:54:34 -0800

    added public cloud csv download

---
+++ app/api/public-cloud/all-projects/route.ts
@@ -0,0 +1,99 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { stringify } from 'csv-stringify/sync';
+import { PublicProject } from '@/queries/types';
+import { publicCloudProjectsPaginated } from '@/queries/paginated/public-cloud';
+import formatDate from '@/components/utils/formatdates';
+import { formatFullName } from '@/components/utils/formatFullName';
+import { z } from 'zod';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/app/api/auth/options';
+import { userInfo } from '@/queries/user';
+
+const searchParamsSchema = z.object({
+  search: z.string().nullable(),
+  ministry: z.string().nullable(),
+  provider: z.string().nullable(),
+  active: z.boolean().nullable(),
+});
+
+export async function GET(req: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      console.log('No session, sending 401');
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const { searchParams } = req.nextUrl;
+
+    const parsedSearchParams = searchParamsSchema.parse({
+      search: searchParams.get('search'),
+      ministry: searchParams.get('ministry'),
+      provider: searchParams.get('provider'),
+      active: searchParams.get('active'),
+    });
+
+    const { userEmail, ministryRoles } = userInfo(session.user.email, session.user.roles);
+
+    const { data } = await publicCloudProjectsPaginated(
+      0,
+      0,
+      parsedSearchParams.search,
+      parsedSearchParams.ministry,
+      parsedSearchParams.provider,
+      userEmail,
+      ministryRoles,
+      parsedSearchParams.active,
+    );
+
+    // Map the data to the correct format for CSV conversion
+    const formattedData = data.map((project: PublicProject) => ({
+      name: project.name,
+      description: project.description,
+      ministry: project.ministry,
+      provider: project.provider,
+      projectOwnerEmail: project.projectOwner.email,
+      projectOwnerName: formatFullName(project.projectOwner),
+      primaryTechnicalLeadEmail: project.primaryTechnicalLead.email,
+      primaryTechnicalLeadName: formatFullName(project.primaryTechnicalLead),
+      secondaryTechnicalLeadEmail: project.secondaryTechnicalLead ? project.secondaryTechnicalLead.email : '',
+      secondaryTechnicalLeadName: formatFullName(project.secondaryTechnicalLead),
+      created: formatDate(project.created.$date),
+      licencePlate: project.licencePlate,
+    }));
+
+    // Convert the data to CSV
+    const csv = stringify(formattedData, {
+      header: true,
+      columns: [
+        'name',
+        'description',
+        'ministry',
+        'provider',
+        'projectOwnerEmail',
+        'projectOwnerName',
+        'primaryTechnicalLeadEmail',
+        'primaryTechnicalLeadName',
+        'secondaryTechnicalLeadEmail',
+        'secondaryTechnicalLeadName',
+        'created',
+        'licencePlate',
+      ],
+    });
+
+    // Response for csv
+    const response = new NextResponse(csv, {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/csv',
+        'Content-Disposition': 'attachment; filename="projects.csv"',
+      },
+    });
+
+    return response;
+  } catch (error: any) {
+    console.error('Error in handler:', error);
+    return new NextResponse(error.message, { status: 500 });
+  }
+}
