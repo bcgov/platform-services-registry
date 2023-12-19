983722317eb366c1efaa47e7046d1ff2d9335b96
j.dev <junmin.dev@outlook.com>
2023-12-18 16:14:16 -0800

    chore: encapsulate ministry roles in keycloak using tf

--- terraform/keycloak/dev/roles.tf
+++ terraform/keycloak/dev/roles.tf
@@ -13,35 +13,3 @@ resource "keycloak_role" "pltsvc_reader" {
   name        = "reader"
   description = "Registry Read-Only"
 }
-
-resource "keycloak_role" "pltsvc_ministry_citz_admin" {
-  realm_id  = data.keycloak_realm.pltsvc.id
-  client_id = keycloak_openid_client.pltsvc.id
-
-  name        = "ministry-citz-admin"
-  description = "Ministry CITZ Administrator"
-}
-
-resource "keycloak_role" "pltsvc_ministry_citz_reader" {
-  realm_id  = data.keycloak_realm.pltsvc.id
-  client_id = keycloak_openid_client.pltsvc.id
-
-  name        = "ministry-citz-reader"
-  description = "Ministry CITZ Read-Only"
-}
-
-resource "keycloak_role" "pltsvc_ministry_hlth_admin" {
-  realm_id  = data.keycloak_realm.pltsvc.id
-  client_id = keycloak_openid_client.pltsvc.id
-
-  name        = "ministry-hlth-admin"
-  description = "Ministry HLTH Administrator"
-}
-
-resource "keycloak_role" "pltsvc_ministry_hlth_reader" {
-  realm_id  = data.keycloak_realm.pltsvc.id
-  client_id = keycloak_openid_client.pltsvc.id
-
-  name        = "ministry-hlth-reader"
-  description = "Ministry HLTH Read-Only"
-}
