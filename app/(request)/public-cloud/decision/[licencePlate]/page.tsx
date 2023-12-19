fdf4889f5187546b35dda628c19ecca518bb7db7
Oamar Kanji <30703259+okanji@users.noreply.github.com>
2023-12-19 11:02:45 -0800

    Misc bug fixes (#1608)
    * fixed disable submit on dirty and autocomplete not showing names
    
    * fixed margin
    
    * removed question tool tip and fixed link
    
    * changed cad to usd
    
    * fixed budget and account coding
    
    * fixed redirect uri's
    
    * show technical lead if it exists

--- app/(request)/public-cloud/decision/[licencePlate]/page.tsx
+++ app/(request)/public-cloud/decision/[licencePlate]/page.tsx
@@ -98,10 +98,6 @@ export default function RequestDecision({ params }: { params: { licencePlate: st
     }
   };
 
-  const setComment = (comment: string) => {
-    onSubmit({ ...methods.getValues(), comment });
-  };
-
   useEffect(() => {
     if (data?.requestedProject.secondaryTechnicalLead) {
       setSecondTechLead(true);
@@ -156,14 +152,6 @@ export default function RequestDecision({ params }: { params: { licencePlate: st
         setOpen={setOpenCreate}
         handleSubmit={methods.handleSubmit(onSubmit)}
         isLoading={isLoading}
-      />
-      <Comment
-        open={openComment}
-        setOpen={setOpenComment}
-        onSubmit={setComment}
-        isLoading={isLoading}
-        type={data?.type}
-        action={currentAction}
       />
       <ReturnModal open={openReturn} setOpen={setOpenReturn} redirectUrl="/public-cloud/products/active-requests" />
     </div>
