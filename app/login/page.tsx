38b9de55dd8fafd3628c249df9967974e780c044
Amritpal Nijjar <amritpal.nijjar@gov.bc.ca>
2023-12-19 10:14:18 -0800

    fixed error handling, created new test

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -150,7 +150,7 @@ export default function SignInPage() {
           >
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
           </svg>
-          <span>Share contact details and IDR information for the product owner and up to 2 technical leads</span>
+          <span>Share contact details and IDIR information for the product owner and up to 2 technical leads</span>
         </li>
         <li className="flex flex-row">
           <svg
