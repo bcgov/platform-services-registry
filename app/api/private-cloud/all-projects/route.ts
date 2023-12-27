8ff6029d15d04613101aac574a44dbc882244882
Kolezanchik <zhanna.kolesnyk@gov.bc.ca>
2023-12-27 11:41:42 -0800

    merge commit

--- app/api/private-cloud/all-projects/route.ts
+++ app/api/private-cloud/all-projects/route.ts
@@ -33,6 +33,8 @@ export async function GET(req: NextRequest) {
       active: searchParams.get('active') === 'true', //Converts 'true' string to true boolean
     });
 
+    console.log(`Parsed Search Params: ${JSON.stringify(parsedSearchParams)}`); // Log the parsed search parameters
+
     const { userEmail, ministryRoles } = userInfo(session.user.email, session.user.roles);
 
     const { data } = await privateCloudProjectsPaginated(
