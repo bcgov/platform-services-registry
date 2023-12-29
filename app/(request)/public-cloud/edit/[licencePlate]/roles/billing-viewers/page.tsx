977daeafb34a6effca4df1c64e4f4eb7635cb5be
Kolezanchik <zhanna.kolesnyk@gov.bc.ca>
2023-12-29 13:32:58 -0800

    added dashboards for other roles users and pagination

--- app/(request)/public-cloud/edit/[licencePlate]/roles/billing-viewers/page.tsx
+++ app/(request)/public-cloud/edit/[licencePlate]/roles/billing-viewers/page.tsx
@@ -1,12 +1,12 @@
 import TableAWSRoles from '@/components/table/TableAWSRoles';
-import { getSubGroupMembersByLicencePlateAndName, User, paramsURL } from '@/app/api/public-cloud/aws-roles/routes';
+import { getSubGroupMembersByLicencePlateAndName, paramsURL, User } from '@/app/api/public-cloud/aws-roles/routes';
 import TableBodyAWSRoles from '@/components/table/TableBodyAWSRoles';
 
 export default async function ProductAWSRoles(req: paramsURL) {
   const licencePlate = req.params.licencePlate;
   const currentPage: number = +req.searchParams.page || 1;
   const pageSize: number = +req.searchParams.pageSize || 5;
-  const users = await getSubGroupMembersByLicencePlateAndName(licencePlate, 'Developer', {
+  const users = await getSubGroupMembersByLicencePlateAndName(licencePlate, 'Billing Viewer', {
     page: currentPage,
     pageSize: pageSize,
   });
