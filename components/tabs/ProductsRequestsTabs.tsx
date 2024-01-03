1275ebaa96062df09cafa6d19d76c222568657d2
Oamar Kanji <30703259+okanji@users.noreply.github.com>
2024-01-03 15:05:23 -0800

    Added history tab (#1662)
    * added footer and fixed table height
    
    * changed table font
    
    * added avatars for projects
    
    * formatted table column spacing
    
    * deleted footer large
    
    * removed hard coded email
    
    * added clear filter button
    
    * added padding to button
    
    * removed unused footer component
    
    * removed description
    
    * updated variable name and constants
    
    * formatted table
    
    * re aligned button
    
    * added autoComplete=off
    
    * Added autoComplete field
    
    * tabs
    
    * history
    
    * added private cloud
    
    * changed id to licence plate
    
    * changed id to licence plate
    
    * removed request page
    
    * fixed public cloud history
    
    * fixed typo
    
    * Changed lanuage
    
    * re formatted spacing
    
    * shifted spacing to layout
    
    * changed client component to server side
    
    * removed duplicate folder
    
    * removed unused page
    
    * changed crumbs
    
    * Review changes
    
    * chagned cluster to provider

--- components/tabs/ProductsRequestsTabs.tsx
+++ components/tabs/ProductsRequestsTabs.tsx
@@ -9,26 +9,24 @@ export default function Tabs() {
   return (
     <span className="isolate inline-flex rounded-md h-10 mb-8">
       <Link
-        style={{ width: 97 }}
         type="button"
         href={`/${pathname.split('/')[1]}/products/all`}
-        className={`pl-4 relative inline-flex items-center rounded-l-lg px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-bcblue focus:z-10
-          ${
-            pathname.split('/')[2] === 'products' ? 'bg-bcblue text-white' : 'bg-white text-gray-900 hover:bg-gray-100'
-          }`}
+        className={`w-24 pl-4 relative inline-flex items-center rounded-l-lg px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-bcblue focus:z-10
+          ${pathname.split('/')[3] === 'all' ? 'bg-bcblue text-white' : 'bg-white text-gray-900 hover:bg-gray-100'}`}
       >
         Products
       </Link>
       <Link
-        style={{ width: 97 }}
         type="button"
-        href={`/${pathname.split('/')[1]}/history`}
-        className={`pl-5 relative -ml-px inline-flex items-center rounded-r-lg px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-bcblue focus:z-10
+        href={`/${pathname.split('/')[1]}/products/active-requests`}
+        className={`w-28 pl-5 relative -ml-px inline-flex items-center rounded-r-lg px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-bcblue focus:z-10
           ${
-            pathname.split('/')[2] === 'history' ? 'bg-bcblue text-white' : 'bg-white text-gray-900 hover:bg-gray-100'
+            pathname.split('/')[3] === 'active-requests'
+              ? 'bg-bcblue text-white'
+              : 'bg-white text-gray-900 hover:bg-gray-100'
           }`}
       >
-        History
+        In Progress
       </Link>
     </span>
   );
