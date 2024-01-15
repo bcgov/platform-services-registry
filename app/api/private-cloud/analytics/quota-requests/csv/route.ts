92c2dd728666fe3e2199a447117a90f32ea5536e
Oamar Kanji <oamarkanji@gmail.com>
2024-01-15 15:15:59 -0800

    fixed csv download

--- app/api/private-cloud/analytics/quota-requests/csv/route.ts
+++ app/api/private-cloud/analytics/quota-requests/csv/route.ts
@@ -1,19 +1,41 @@
 import { NextResponse } from 'next/server';
 import { stringify } from 'csv-stringify/sync';
 import createApiHandler from '@/core/apiHandler';
-import { quotaEditRequests, type DataPoint } from '@/analytics/private-cloud/quotaChanges';
+import { quotaEditRequests } from '@/analytics/private-cloud/quotaChanges';
+import { DecisionStatus } from '@prisma/client';
+
+type DataPoint = {
+  date: string;
+  'All quota requests': number;
+  'Approved quota requests': number;
+  'Rejected quota requests': number;
+};
 
 const apiHandler = createApiHandler({
   roles: ['admin'],
 });
 
 export const GET = apiHandler(async () => {
-  const data: DataPoint[] = await quotaEditRequests();
+  const allData = await quotaEditRequests();
+  const approvedData = await quotaEditRequests(DecisionStatus.APPROVED);
+  const rejectedData = await quotaEditRequests(DecisionStatus.REJECTED);
 
+  const allDates = Array.from(
+    new Set([...allData.map((d) => d.date), ...approvedData.map((d) => d.date), ...rejectedData.map((d) => d.date)]),
+  );
+
+  const data: DataPoint[] = allDates.map((date) => {
+    return {
+      date,
+      'All quota requests': allData.find((d) => d.date === date)?.['Quota requests'] || 0,
+      'Approved quota requests': approvedData.find((d) => d.date === date)?.['Quota requests'] || 0,
+      'Rejected quota requests': rejectedData.find((d) => d.date === date)?.['Quota requests'] || 0,
+    };
+  });
   // Convert the data to CSV
   const csv = stringify(data, {
     header: true,
-    columns: ['date', 'Quota requests'],
+    columns: ['date', 'All quota requests', 'Approved quota requests', 'Rejected quota requests'],
   });
 
   // Response for csv
@@ -21,7 +43,7 @@ export const GET = apiHandler(async () => {
     status: 200,
     headers: {
       'Content-Type': 'text/csv',
-      'Content-Disposition': 'attachment; filename=private-cloud-products.csv',
+      'Content-Disposition': 'attachment; filename=quota-requests.csv',
     },
   });
 
