e9dc830e2255cf7ff2a2354f4dc1dfb29fabb8f3
Amritpal-Nijjar <amritpal.nijjar@gov.bc.ca>
2024-01-15 16:27:57 -0800

    fix: wording

--- emails/templates/private-cloud/RequestApproval.tsx
+++ emails/templates/private-cloud/RequestApproval.tsx
@@ -36,7 +36,7 @@ const RequestApprovalTemplate = ({ request }: EmailProp) => {
                 <Text className="">
                   The Product Owner and the Technical Lead have been provisioned with admin access to the namespaces
                   below and can add other users as necessary. Please note that if a Product Owner or a Technical Lead is
-                  removed as a project contact in the Platform Registry, they will lost their access to the projcect set
+                  removed as a project contact in the Platform Registry, they will lose their access to the project set
                   namespaces in Openshift. The new Product or Technical Lead provided on the product details page will
                   gain the administrative access to the namespaces.
                 </Text>
