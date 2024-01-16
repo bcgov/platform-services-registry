a3942608f050ece23c9d45037bca5989b7c93372
Oamar Kanji <30703259+okanji@users.noreply.github.com>
2024-01-15 16:11:58 -0800

    Analytics (#1767)
    * fixed disable submit on dirty and autocomplete not showing names
    
    * fixed margin
    
    * removed question tool tip and fixed link
    
    * changed cad to usd
    
    * fixed budget and account coding
    
    * fixed redirect uri's
    
    * show technical lead if it exists
    
    * added crumbs and provisioning icon
    
    * shortened names
    
    * fixed error modal working
    
    * added dot between ministry and date
    
    * added analytics
    
    * Added decision status to analyitics
    
    * added all three lines
    
    * fixed csv download
    
    * review changes

---
+++ app/api/private-cloud/analytics/quota-requests/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from 'next/server';
+import createApiHandler from '@/core/apiHandler';
+import { quotaEditRequests } from '@/analytics/private-cloud/quotaChanges';
+import { DecisionStatus } from '@prisma/client';
+
+type DataPoint = {
+  date: string;
+  'All quota requests': number;
+  'Approved quota requests': number;
+  'Rejected quota requests': number;
+};
+
+const apiHandler = createApiHandler({
+  roles: ['admin'],
+});
+
+export const GET = apiHandler(async ({ queryParams }) => {
+  const getAllQuotaEditRequests = quotaEditRequests();
+  const getApprovedQuotaEditRequests = quotaEditRequests(DecisionStatus.APPROVED);
+  const getRejectedQuotaEditRequests = quotaEditRequests(DecisionStatus.REJECTED);
+
+  const [allData, approvedData, rejectedData] = await Promise.all([
+    getAllQuotaEditRequests,
+    getApprovedQuotaEditRequests,
+    getRejectedQuotaEditRequests,
+  ]);
+
+  const allDates = Array.from(
+    new Set([...allData.map((d) => d.date), ...approvedData.map((d) => d.date), ...rejectedData.map((d) => d.date)]),
+  );
+
+  const data: DataPoint[] = allDates.map((date) => {
+    return {
+      date,
+      'All quota requests': allData.find((d) => d.date === date)?.['Quota requests'] || 0,
+      'Approved quota requests': approvedData.find((d) => d.date === date)?.['Quota requests'] || 0,
+      'Rejected quota requests': rejectedData.find((d) => d.date === date)?.['Quota requests'] || 0,
+    };
+  });
+
+  return NextResponse.json(data);
+});
