3a37ddc938b93dd5456f8b287889f563ae6e4f6d
j.dev <junmin.dev@outlook.com>
2024-01-17 21:00:22 -0800

    chore: support array type in custom query parser

--- lib/query-string.ts
+++ lib/query-string.ts
@@ -1,5 +1,18 @@
 export function parseQueryString(queryString: string) {
   const params = new URLSearchParams(queryString);
-  const parsedParams = Object.fromEntries(params);
+  const parsedParams: Record<string, string | string[]> = {};
+
+  params.forEach((value, key) => {
+    if (parsedParams[key]) {
+      // If the key already exists in the parsedParams, convert it to an array
+      if (!Array.isArray(parsedParams[key])) {
+        parsedParams[key] = [parsedParams[key] as string];
+      }
+      (parsedParams[key] as string[]).push(value);
+    } else {
+      parsedParams[key] = value;
+    }
+  });
+
   return parsedParams;
 }
