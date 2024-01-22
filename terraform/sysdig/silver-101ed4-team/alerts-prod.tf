7cb61cb89a7c1273f2e0aa01feaaeba3b97f2222
j.dev <junmin.dev@outlook.com>
2024-01-22 10:49:06 -0800

    feat(sysdig): add sysdig alerts for upper environments

---
+++ terraform/sysdig/silver-101ed4-team/alerts-prod.tf
@@ -0,0 +1,71 @@
+resource "sysdig_monitor_alert_metric" "prod_pltsvc_db_pod_memory" {
+  name        = "[Prod] Registry MongoDB - Memory"
+  description = "Prod: Registry MongoDB pod is using 90% of the memory limit"
+  severity    = 2
+  enabled     = false
+
+  metric                = "avg(max(sysdig_container_memory_limit_used_percent)) > 90"
+  trigger_after_minutes = 60
+
+  scope                 = "kubernetes.cluster.name in (\"silver\") and kube_namespace_name in (\"101ed4-prod\") and kube_workload_name in (\"pltsvc-mongodb\")"
+  multiple_alerts_by    = []
+  notification_channels = [148742]
+
+  custom_notification {
+    title = "{{__alert_name__}} is {{__alert_status__}}"
+  }
+}
+
+resource "sysdig_monitor_alert_metric" "prod_pltsvc_db_pod_cpu" {
+  name        = "[Prod] Registry MongoDB - CPU"
+  description = "Prod: Any of Registry MongoDB pod is using more than 80% of limited CPU"
+  severity    = 1
+  enabled     = false
+
+  metric                = "max(max(sysdig_container_cpu_cores_used_percent)) > 80"
+  trigger_after_minutes = 60
+
+  scope                 = "kubernetes.cluster.name in (\"silver\") and kube_namespace_name in (\"101ed4-prod\") and kube_workload_name in (\"pltsvc-mongodb\")"
+  multiple_alerts_by    = []
+  notification_channels = [148742]
+
+  custom_notification {
+    title = "{{__alert_name__}} is {{__alert_status__}}"
+  }
+}
+
+resource "sysdig_monitor_alert_metric" "prod_pltsvc_app_pod_memory" {
+  name        = "[Prod] Registry App - Memory"
+  description = "Prod: Registry app pod is using 90% of the memory limit"
+  severity    = 2
+  enabled     = false
+
+  metric                = "avg(max(sysdig_container_memory_limit_used_percent)) > 90"
+  trigger_after_minutes = 60
+
+  scope                 = "kubernetes.cluster.name in (\"silver\") and kube_namespace_name in (\"101ed4-prod\") and kube_workload_name in (\"pltsvc-app\")"
+  multiple_alerts_by    = []
+  notification_channels = [148742]
+
+  custom_notification {
+    title = "{{__alert_name__}} is {{__alert_status__}}"
+  }
+}
+
+resource "sysdig_monitor_alert_metric" "prod_pltsvc_app_pod_cpu" {
+  name        = "[Prod] Registry App - CPU"
+  description = "Prod: Any of Registry App pod is using more than 80% of limited CPU"
+  severity    = 1
+  enabled     = false
+
+  metric                = "max(max(sysdig_container_cpu_cores_used_percent)) > 80"
+  trigger_after_minutes = 60
+
+  scope                 = "kubernetes.cluster.name in (\"silver\") and kube_namespace_name in (\"101ed4-prod\") and kube_workload_name in (\"pltsvc-app\")"
+  multiple_alerts_by    = []
+  notification_channels = [148742]
+
+  custom_notification {
+    title = "{{__alert_name__}} is {{__alert_status__}}"
+  }
+}
