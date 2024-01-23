f247bedba4885a4abad5c38647c1f392d0aa3025
Oamar Kanji <oamarkanji@gmail.com>
2024-01-23 10:31:08 -0800

    Added comment to request approval and rejection templates

--- emails/_components/NamespaceDetails.tsx
+++ emails/_components/NamespaceDetails.tsx
@@ -6,38 +6,38 @@ export default function NamespaceDetails({ cluster, licencePlate }: { cluster: s
       <Heading className="text-lg">Namespace Details</Heading>
       <div>
         <Text className="mb-1 font-semibold h-4">OpenShift Cluster: </Text>
-        <Text className="mt-0 h-4">{cluster}</Text>
+        <Text className="mt-1 h-4">{cluster}</Text>
       </div>
       {licencePlate && (
         <>
           <Text className="mb-0 font-semibold h-4">Development Namespace: </Text>
           <Link
-            className="mt-0 h-4"
+            className="mt-1 h-4"
             href={`https://console.apps.${cluster}.devops.gov.bc.ca/k8s/cluster/projects/${licencePlate}-dev`}
           >
-            <Text className="mt-0 h-4">{licencePlate}-dev</Text>
+            <Text className="mt-1 h-4">{licencePlate}-dev</Text>
           </Link>
           <Text className="mb-0 font-semibold h-4">Test Namespace: </Text>
           <Link
-            className="mt-0 h-4"
+            className="mt-1 h-4"
             href={`https://console.apps.${cluster}.devops.gov.bc.ca/k8s/cluster/projects/${licencePlate}-test`}
           >
-            <Text className="mt-0 h-4">{licencePlate}-test</Text>
+            <Text className="mt-1 h-4">{licencePlate}-test</Text>
           </Link>
-          <Text className="mb-0 font-semibold h-4">Production Namespace: </Text>
+          <Text className="mb-1 font-semibold h-4">Production Namespace: </Text>
           <Link
-            className="mt-0 h-4"
+            className="mt-1 h-4"
             href={`https://console.apps.${cluster}.devops.gov.bc.ca/k8s/cluster/projects/${licencePlate}-prod`}
           >
             <Text className="mt-0 h-4">{licencePlate}-prod</Text>
           </Link>
 
-          <Text className="mb-0 font-semibold h-4">Tools Namespace: </Text>
+          <Text className="mb-1 font-semibold h-4">Tools Namespace: </Text>
           <Link
-            className="mt-0 h-4"
+            className="mt-1 h-4"
             href={`https://console.apps.${cluster}.devops.gov.bc.ca/k8s/cluster/projects/${licencePlate}-tools`}
           >
-            <Text className="mt-0 h-4">{licencePlate}-tools</Text>
+            <Text className="mt-1 h-4">{licencePlate}-tools</Text>
           </Link>
         </>
       )}
--- emails/_components/ProductDetails.tsx
+++ emails/_components/ProductDetails.tsx
@@ -26,21 +26,21 @@ export default function ProductDetails({
     <div>
       <Heading className="text-lg">Product Details</Heading>
       <div>
-        <Text className="mb-0 font-semibold h-4">Product Name: </Text>
-        <Text className="mt-0 h-4">{name}</Text>
-        <Text className="mb-0 font-semibold h-4">Product Description: </Text>
-        <Text className="mt-0 h-4">{description}</Text>
-        <Text className="mb-0 font-semibold h-4">Ministry: </Text>
-        <Text className="mt-0 h-4">{ministry}</Text>
-        <Text className="mb-0.5 font-semibold h-4">Product Owner: </Text>
-        <Text className="mt-0 mb-1.5 h-4">
+        <Text className="mb-2 font-semibold h-4">Product Name: </Text>
+        <Text className="mt-1 h-4">{name}</Text>
+        <Text className="mb-2 font-semibold h-4">Product Description: </Text>
+        <Text className="mt-1 h-4">{description}</Text>
+        <Text className="mb-2 font-semibold h-4">Ministry: </Text>
+        <Text className="mt-1 h-4">{ministry}</Text>
+        <Text className="mb-2 font-semibold h-4">Product Owner: </Text>
+        <Text className="mt-1 mb-2 h-4">
           {po.firstName} {po.lastName}
         </Text>
         <Link className="mt-0 h-4" href={`mailto:${po.email}`}>
           {po.email}
         </Link>
-        <Text className="mb-0.5 font-semibold h-4">Technical Lead: </Text>
-        <Text className="mt-0 mb-1.5 h-4">
+        <Text className="mb-2 font-semibold h-4">Technical Lead: </Text>
+        <Text className="mt-0 mb-2 h-4">
           {tl1.firstName} {tl1.lastName}
         </Text>
         <Link className="mt-0 h-4" href={`mailto:${tl1.email}`}>
@@ -48,8 +48,8 @@ export default function ProductDetails({
         </Link>
         {tl2 && (
           <div>
-            <Text className="mb-0.5 font-semibold h-4">Secondary Technical Lead: </Text>
-            <Text className="mt-0 mb-1.5 h-4">
+            <Text className="mb-2 font-semibold h-4">Secondary Technical Lead: </Text>
+            <Text className="mt-0 mb-2 h-4">
               {tl2.firstName} {tl2.lastName}
             </Text>
             <Link className="mt-0 h-4" href={`mailto:${tl2.email}`}>
