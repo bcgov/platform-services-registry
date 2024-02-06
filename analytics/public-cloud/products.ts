587ce5d31a785ba5d36662d1ea19dccf00ac17c8
Oamar Kanji <30703259+okanji@users.noreply.github.com>
2024-02-06 11:26:50 -0800

    Public cloud analytics (#1955)
    * added footer and fixed table height
    
    * changed table font
    
    * added avatars for projects
    
    * formatted table column spacing
    
    * deleted footer large
    
    * removed hard coded email
    
    * added clear filter button
    
    * added padding to button
    
    * removed unused footer component
    
    * removed description
    
    * updated variable name and constants
    
    * formatted table
    
    * re aligned button
    
    * added autoComplete=off
    
    * Added autoComplete field
    
    * added public cloud analytics
    
    * added promise.all

---
+++ analytics/public-cloud/products.ts
@@ -0,0 +1,60 @@
+import prisma from '@/lib/prisma';
+import _isEqual from 'lodash-es/isEqual';
+
+export type DataPoint = {
+  date: string;
+  Products: number;
+};
+
+const formatter = new Intl.DateTimeFormat('en', { month: 'short', year: 'numeric' });
+
+function createMonthKey(date: Date) {
+  return formatter.format(date);
+}
+
+export async function productsCreatedPerMonth() {
+  const projects = await prisma.publicCloudProject.findMany({
+    select: {
+      created: true,
+    },
+  });
+
+  const projectsOverTime = projects.reduce(
+    (acc, project) => {
+      const key = createMonthKey(project.created);
+
+      if (acc[key]) {
+        acc[key] += 1;
+      } else {
+        acc[key] = 1;
+      }
+
+      return acc;
+    },
+    {} as { [key: string]: number },
+  );
+
+  return projectsOverTime;
+}
+
+export async function numberOfProductsOverTime() {
+  const projectsOverTime = await productsCreatedPerMonth();
+
+  // Calculate cumulative products over time
+  let cumulativeProducts = 0;
+  const cumulativeProductsOverTime = Object.entries(projectsOverTime).reduce(
+    (acc, [key, value]) => {
+      cumulativeProducts += value;
+      acc[key] = cumulativeProducts;
+      return acc;
+    },
+    {} as { [key: string]: number },
+  );
+
+  const result: DataPoint[] = Object.entries(cumulativeProductsOverTime).map((item) => ({
+    date: item[0],
+    Products: item[1],
+  }));
+
+  return result;
+}
---
+++ analytics/public-cloud/requests.ts
@@ -0,0 +1,94 @@
+import { RequestType } from '@prisma/client';
+import prisma from '@/lib/prisma';
+import _isEqual from 'lodash-es/isEqual';
+
+export type CombinedDataPoint = {
+  date: string;
+  'All requests': number;
+  'Edit requests': number;
+  'Create requests': number;
+  'Delete requests': number;
+};
+
+const formatter = new Intl.DateTimeFormat('en', { month: 'short', year: 'numeric' });
+
+function createMonthKey(date: Date) {
+  return formatter.format(date);
+}
+
+export async function combinedRequests() {
+  const [requestsData, editRequestsData, createRequestsData, deleteRequestsData] = await Promise.all([
+    prisma.publicCloudRequest.findMany({
+      select: {
+        created: true,
+      },
+      orderBy: {
+        created: 'asc',
+      },
+    }),
+    prisma.publicCloudRequest.findMany({
+      where: {
+        type: RequestType.EDIT,
+      },
+      select: {
+        created: true,
+      },
+      orderBy: {
+        created: 'asc',
+      },
+    }),
+    prisma.publicCloudRequest.findMany({
+      where: {
+        type: RequestType.CREATE,
+      },
+      select: {
+        created: true,
+      },
+      orderBy: {
+        created: 'asc',
+      },
+    }),
+    prisma.publicCloudRequest.findMany({
+      where: {
+        type: RequestType.DELETE,
+      },
+      select: {
+        created: true,
+      },
+      orderBy: {
+        created: 'asc',
+      },
+    }),
+  ]);
+
+  const combinedRequestsData: CombinedDataPoint[] = [];
+
+  const dateMap = new Map<string, number>();
+
+  const combinedDates = [...requestsData, ...editRequestsData, ...createRequestsData, ...deleteRequestsData]
+    .map((request) => request.created)
+    .sort((a, b) => (a > b ? 1 : -1));
+
+  combinedDates.forEach((d) => {
+    const date = createMonthKey(d);
+    const count = dateMap.get(date) || 0;
+    dateMap.set(date, count + 1);
+  });
+
+  dateMap.forEach((count, date) => {
+    const allCount = requestsData.filter((request) => createMonthKey(request.created) === date).length;
+    const editCount = editRequestsData.filter((request) => createMonthKey(request.created) === date).length;
+    const createCount = createRequestsData.filter((request) => createMonthKey(request.created) === date).length;
+    const deleteCount = deleteRequestsData.filter((request) => createMonthKey(request.created) === date).length;
+
+    combinedRequestsData.push({
+      date,
+      'All requests': allCount,
+      'Edit requests': editCount,
+      'Create requests': createCount,
+      'Delete requests': deleteCount,
+    });
+  });
+
+  return combinedRequestsData;
+}
