7e595b7bc011b63cfce114f5c3ccac347f9c563e
j.dev <junmin.dev@outlook.com>
2024-03-14 16:04:49 -0700

    feat: add private & public roles in keycloak

--- terraform/keycloak/test/roles.tf
+++ terraform/keycloak/test/roles.tf
@@ -14,6 +14,22 @@ resource "keycloak_role" "pltsvc_reader" {
   description = "Registry Read-Only"
 }
 
+resource "keycloak_role" "pltsvc_private_admin" {
+  realm_id  = data.keycloak_realm.pltsvc.id
+  client_id = keycloak_openid_client.pltsvc.id
+
+  name        = "private-admin"
+  description = "Registry Public Cloud Administrator"
+}
+
+resource "keycloak_role" "pltsvc_private_reader" {
+  realm_id  = data.keycloak_realm.pltsvc.id
+  client_id = keycloak_openid_client.pltsvc.id
+
+  name        = "private-reader"
+  description = "Registry Public Cloud Read-Only"
+}
+
 resource "keycloak_role" "pltsvc_approver" {
   realm_id  = data.keycloak_realm.pltsvc.id
   client_id = keycloak_openid_client.pltsvc.id
