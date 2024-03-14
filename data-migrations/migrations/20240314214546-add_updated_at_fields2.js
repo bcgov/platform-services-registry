f07f32dea3b88630e7cfa40a43cbc5287243c3bf
j.dev <junmin.dev@outlook.com>
2024-03-14 14:48:21 -0700

    fix: update missing updated_at

---
+++ data-migrations/migrations/20240314214546-add_updated_at_fields2.js
@@ -0,0 +1,23 @@
+export const up = async (db, client) => {
+  const privateProjectProm = db
+    .collection('PrivateCloudProject')
+    .updateMany({ updatedAt: { $exists: false } }, [{ $set: { updatedAt: '$created' } }]);
+
+  const privateRequestProm = db
+    .collection('PrivateCloudRequest')
+    .updateMany({ updatedAt: { $exists: false } }, [{ $set: { updatedAt: '$created' } }]);
+
+  const publicProjectProm = db
+    .collection('PublicCloudProject')
+    .updateMany({ updatedAt: { $exists: false } }, [{ $set: { updatedAt: '$created' } }]);
+
+  const publicRequestProm = db
+    .collection('PublicCloudRequest')
+    .updateMany({ updatedAt: { $exists: false } }, [{ $set: { updatedAt: '$created' } }]);
+
+  const result = await Promise.all([privateProjectProm, privateRequestProm, publicProjectProm, publicRequestProm]);
+
+  console.log('add_updated_at_fields2:', result);
+};
+
+export const down = async (db, client) => {};
