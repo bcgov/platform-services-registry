be32408fe69d37370e2429ab96a912552300e9fd
Amritpal-Nijjar <amritpal.nijjar@gov.bc.ca>
2024-03-19 09:22:41 -0700

    feat: create comment api

--- app/api/private-cloud/products/[licencePlate]/comments/_operations/create.ts
+++ app/api/private-cloud/products/[licencePlate]/comments/_operations/create.ts
@@ -1,3 +1,30 @@
-export default async function createOp() {
-  return { message: 'Create comment successful' };
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+interface CreateCommentParams {
+  text: string;
+  projectId: string;
+  userId: string;
+}
+
+export async function createOp({ text, projectId, userId }: CreateCommentParams) {
+  // Create the comment with a link to the user
+  const comment = await prisma.privateCloudComment.create({
+    data: {
+      text,
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+      project: {
+        connect: {
+          id: projectId,
+        },
+      },
+    },
+  });
+
+  return comment;
 }
--- app/api/private-cloud/products/[licencePlate]/comments/route.ts
+++ app/api/private-cloud/products/[licencePlate]/comments/route.ts
@@ -1,11 +1,30 @@
 import { NextResponse } from 'next/server';
-import createOp from './_operations/create';
+import createApiHandler from '@/core/api-handler';
+import { createOp } from './_operations/create';
 import listOp from './_operations/list';
+import { z } from 'zod';
 
-export async function POST() {
-  const data = await createOp();
-  return NextResponse.json(data);
-}
+const CreateCommentBodySchema = z.object({
+  text: z.string().min(1, 'The comment text must not be empty'),
+  projectId: z.string(),
+});
+
+export const POST = createApiHandler({
+  roles: ['admin'],
+  validations: {
+    body: CreateCommentBodySchema,
+  },
+})(async ({ session, body }) => {
+  console.log(session);
+  if (!session) {
+    return new NextResponse(JSON.stringify({ message: 'Unauthorized' }), { status: 401 });
+  }
+
+  const userId = session!.userId!;
+
+  const comment = await createOp({ text: body.text, projectId: body.projectId, userId });
+  return new NextResponse(JSON.stringify(comment), { status: 201 });
+});
 
 export async function GET() {
   const data = await listOp();
