82de4855edb031d2aca7d40b6dd4d9908b39acb1
j.dev <junmin.dev@outlook.com>
2024-03-20 11:42:15 -0700

    feat: add total quotas in csv exports

--- app/api/private-cloud/all-projects/route.ts
+++ app/api/private-cloud/all-projects/route.ts
@@ -1,11 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { $Enums } from '@prisma/client';
+import forEach from 'lodash-es/forEach';
 import formatDate from '@/utils/date';
 import { formatFullName } from '@/helpers/user';
 import { z } from 'zod';
 import createApiHandler from '@/core/api-handler';
 import { CsvResponse } from '@/core/responses';
 import { searchPrivateCloudProducts } from '@/queries/private-cloud-products';
+import { extractNumbers } from '@/utils/string';
+
+function getTotalQuota(...quotaValues: string[]) {
+  let total = 0;
+  forEach(quotaValues, (val) => {
+    const nums = extractNumbers(val);
+    if (nums.length > 0) total += nums[0];
+  });
+
+  return total;
+}
 
 const queryParamSchema = z.object({
   search: z.string().optional(),
@@ -54,6 +66,24 @@ export const GET = apiHandler(async ({ queryParams, session }) => {
     created: formatDate(project.created),
     updatedAt: formatDate(project.updatedAt),
     licencePlate: project.licencePlate,
+    'Total Compute Quota (Cores)': getTotalQuota(
+      project.developmentQuota.cpu,
+      project.testQuota.cpu,
+      project.productionQuota.cpu,
+      project.toolsQuota.cpu,
+    ),
+    'Total Memory Quota (Gb)': getTotalQuota(
+      project.developmentQuota.memory,
+      project.testQuota.memory,
+      project.productionQuota.memory,
+      project.toolsQuota.memory,
+    ),
+    'Total Storage Quota (Gb)': getTotalQuota(
+      project.developmentQuota.storage,
+      project.testQuota.storage,
+      project.productionQuota.storage,
+      project.toolsQuota.storage,
+    ),
     status: project.status,
   }));
 
