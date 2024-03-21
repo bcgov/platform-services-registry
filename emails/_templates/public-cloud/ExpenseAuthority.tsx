555c3fc854874e044d74c63caac5337e02081421
Kolezanchik <zhanna.kolesnyk@gov.bc.ca>
2024-03-21 15:36:23 -0700

    chore: expense authority email template wording

--- emails/_templates/public-cloud/ExpenseAuthority.tsx
+++ emails/_templates/public-cloud/ExpenseAuthority.tsx
@@ -34,10 +34,10 @@ const ExpenseAuthorityTemplate = ({ product }: EmailProp) => {
           <Body className="bg-white my-auto mx-auto font-sans text-xs text-darkergrey">
             <div className="m-12">
               <div className="pb-6 mt-4 mb-4 border-solid border-0 border-b-1 border-slate-300">
-                <Heading className="text-lg">You are requested as the Expense Authority</Heading>
+                <Heading className="text-lg">You are requested as the Expense Authority person</Heading>
                 <Text>Hi, {expenseAuthority?.firstName},</Text>
                 <Text className="">
-                  You are added as an Expense Authority person for the product {name} in Platform Service Registry.
+                  You are requested as the Expense Authority person for the product {name} in Platform Service Registry.
                 </Text>
               </div>
               <div className="pb-6 mt-4 mb-4 border-solid border-0 border-b-1 border-slate-300">
