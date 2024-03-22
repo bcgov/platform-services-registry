4167255c9a11e966085028090ad6c08ca2319090
Amritpal-Nijjar <amritpal.nijjar@gov.bc.ca>
2024-03-21 23:49:12 -0700

    feat: logic for read comment by id api logic

--- app/api/private-cloud/products/[licencePlate]/comments/[commentId]/route.ts
+++ app/api/private-cloud/products/[licencePlate]/comments/[commentId]/route.ts
@@ -1,12 +1,34 @@
 import { NextResponse } from 'next/server';
+import createApiHandler from '@/core/api-handler';
+import { z } from 'zod';
+import { PermissionsEnum } from '@/types/permissions';
+import { UnauthorizedResponse, OkResponse, NotFoundResponse } from '@/core/responses';
 import deleteOp from '../_operations/delete';
-import readOp from '../_operations/read';
+import { readOp } from '../_operations/read';
 import updateOp from '../_operations/update';
 
-export async function GET() {
-  const data = await readOp();
-  return NextResponse.json(data);
-}
+const licencePlateSchema = z.object({
+  licencePlate: z.string(),
+  commentId: z.string(),
+});
+
+export const GET = createApiHandler({
+  roles: ['user'],
+  permissions: [PermissionsEnum.ViewAllPrivateProductComments],
+  validations: {
+    pathParams: licencePlateSchema,
+  },
+})(async ({ session, pathParams }) => {
+  if (!session) {
+    return UnauthorizedResponse('Session not found');
+  }
+  const { licencePlate, commentId } = pathParams;
+  const comment = await readOp(licencePlate, commentId);
+  if (!comment) {
+    return NotFoundResponse('Comment not found');
+  }
+  return OkResponse(comment);
+});
 
 export async function PUT() {
   const data = await updateOp();
