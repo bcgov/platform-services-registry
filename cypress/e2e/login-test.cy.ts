bd817db8016e1468ebbb76443e163d4395cbd68b
Artem Kravchenko <43115082+funtigr@users.noreply.github.com>
2024-03-28 16:34:31 -0700

    Update login-test.cy.ts

--- cypress/e2e/login-test.cy.ts
+++ cypress/e2e/login-test.cy.ts
@@ -1,5 +1,5 @@
 describe('Registry App Login', () => {
-  it('Verifies that error message after auth call to backend and user db is shown after login attempt', () => {
+ it('Logs in', () => {
     cy.loginToRegistry(Cypress.env('username'), Cypress.env('password'));
   });
 });
