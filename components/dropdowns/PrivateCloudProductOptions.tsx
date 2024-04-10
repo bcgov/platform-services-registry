4c26fba6fb466d507f9a29eca2eb9469d2f28d72
Kolezanchik <zhanna.kolesnyk@gov.bc.ca>
2024-04-10 11:50:13 -0700

    chore: delete button for regular user and public cloud, and options for admin and public-admin

--- components/dropdowns/PrivateCloudProductOptions.tsx
+++ components/dropdowns/PrivateCloudProductOptions.tsx
@@ -13,6 +13,7 @@ import {
   reprovisionPriviateCloudRequest,
   resendPriviateCloudRequest,
 } from '@/services/backend/private-cloud';
+import DeleteButton from '@/components/buttons/DeleteButton';
 
 export default function Dropdown({
   licensePlace = '',
@@ -94,93 +95,84 @@ export default function Dropdown({
         errorMessage={errorMessage}
         redirectUrl="/private-cloud/products/all"
       />
-      <Menu as="div" className="relative inline-block text-left">
-        <div>
-          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-            Options
-            <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-          </Menu.Button>
-        </div>
+      {!canReprovision ? (
+        <DeleteButton canDelete={canDelete} setShowModal={setShowModal} active={true} />
+      ) : (
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+              Options
+              <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+            </Menu.Button>
+          </div>
 
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="py-1">
-              {canResend && (
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      disabled={!canResend}
-                      type="button"
-                      onClick={async () => {
-                        await resend();
-                      }}
-                      className={classNames(
-                        'group flex items-center px-4 py-2 text-sm w-full',
-                        active && canResend ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      )}
-                    >
-                      <PlayCircleIcon
-                        className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                        aria-hidden="true"
-                      />
-                      Resend
-                    </button>
-                  )}
-                </Menu.Item>
-              )}
-              {canReprovision && (
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      disabled={!canReprovision}
-                      type="button"
-                      onClick={async () => {
-                        await reprovision();
-                      }}
-                      className={classNames(
-                        'group flex items-center px-4 py-2 text-sm w-full',
-                        active && canReprovision ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      )}
-                    >
-                      <PlayCircleIcon
-                        className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                        aria-hidden="true"
-                      />
-                      Reprovision
-                    </button>
-                  )}
-                </Menu.Item>
-              )}
-              {canDelete && (
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      disabled={!canDelete}
-                      type="button"
-                      onClick={() => setShowModal(true)}
-                      className={classNames(
-                        'group flex items-center px-4 py-2 text-sm w-full',
-                        active && canDelete ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      )}
-                    >
-                      <TrashIcon className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
-                      Delete
-                    </button>
-                  )}
-                </Menu.Item>
-              )}
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="py-1">
+                {canResend && (
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        disabled={!canResend}
+                        type="button"
+                        onClick={async () => {
+                          await resend();
+                        }}
+                        className={classNames(
+                          'group flex items-center px-4 py-2 text-sm w-full',
+                          active && canResend ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        )}
+                      >
+                        <PlayCircleIcon
+                          className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                          aria-hidden="true"
+                        />
+                        Resend
+                      </button>
+                    )}
+                  </Menu.Item>
+                )}
+                {canReprovision && (
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        disabled={!canReprovision}
+                        type="button"
+                        onClick={async () => {
+                          await reprovision();
+                        }}
+                        className={classNames(
+                          'group flex items-center px-4 py-2 text-sm w-full',
+                          active && canReprovision ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        )}
+                      >
+                        <PlayCircleIcon
+                          className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                          aria-hidden="true"
+                        />
+                        Reprovision
+                      </button>
+                    )}
+                  </Menu.Item>
+                )}
+                {canDelete && (
+                  <Menu.Item>
+                    {({ active }) => <DeleteButton canDelete={canDelete} setShowModal={setShowModal} active={active} />}
+                  </Menu.Item>
+                )}
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      )}
     </>
   );
 }
