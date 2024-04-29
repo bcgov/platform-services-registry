33bd1de439fc47ffe5c55ea8121efea55822dbfb
Amritpal-Nijjar <amritpal.nijjar@gov.bc.ca>
2024-04-29 13:01:28 -0700

    feat: created stylized comments bubbles

--- components/comments/CommentBubble.tsx
+++ components/comments/CommentBubble.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { EllipsisHorizontalIcon, TrashIcon, PencilSquareIcon } from '@heroicons/react/20/solid';
 import { formatDistanceToNow } from 'date-fns';
 
 interface ChatBubbleProps {
@@ -10,32 +11,58 @@ interface ChatBubbleProps {
 }
 
 const CommentBubble: React.FC<ChatBubbleProps> = ({ firstName, lastName, timestamp, text, isUser }) => {
-  const bubbleStyles = 'relative max-w-xl mx-auto my-2 bg-white rounded-lg border border-blue-200 ';
+  const [menuOpen, setMenuOpen] = useState(false);
+  const bubbleStyles =
+    'relative max-w-xl mx-auto my-2 bg-white rounded-lg border border-blue-200 justify-between items-center';
 
-  // Styles for the header
-  const headerStyles = 'px-4 py-1 bg-blue-100 text-gray-700 text-xs';
+  const headerStyles = 'flex justify-between items-center px-4 py-1 bg-blue-100 text-gray-700 text-xs';
 
-  // Styles for the body of the bubble
   const bodyStyles = 'p-4 text-gray-700';
 
-  const tailStyles = isUser
-    ? 'absolute top-3 right-0 w-0 h-0 border-l-[10px] border-l-transparent border-b-[10px] border-b-transparent border-t-[10px] border-t-blue-300 transform translate-x-1/4 -translate-y-1/3 rotate-45'
-    : '';
+  const userTailStyles =
+    'absolute top-3 right-0 w-0 h-0 border-l-[26px] border-l-transparent border-b-[10px] border-b-transparent border-t-[26px] border-t-blue-100 transform translate-x-1/4 -translate-y-1/4 rotate-45';
+
+  const otherUserTailStyles =
+    'absolute top-3 left-0 w-0 h-0 border-r-[16px] border-r-transparent border-b-[10px] border-b-transparent border-t-[16px] border-t-blue-100 transform -translate-x-1/4 -translate-y-1/3 -rotate-45';
 
   const timeAgo = formatDistanceToNow(timestamp, { addSuffix: true });
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
     <div className={bubbleStyles}>
       <div className={headerStyles}>
-        <span>
-          {firstName} {lastName} <span className="commented-text">commented</span>{' '}
-        </span>
-        <span>{timeAgo}</span>
+        <div>
+          <strong>
+            {firstName} {lastName}
+          </strong>{' '}
+          <span className="commented-text">commented </span>
+          <span>{timeAgo}</span>
+        </div>
+        {isUser && (
+          <button onClick={toggleMenu} className="p-1 rounded-full hover:bg-gray-200">
+            <EllipsisHorizontalIcon className="w-5 h-5" />
+          </button>
+        )}
       </div>
+      {menuOpen && (
+        <div className="absolute right-0 mt-2 py-1 w-48 bg-white rounded-md shadow-lg z-50">
+          <ul>
+            <li className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+              <PencilSquareIcon className="w-5 h-5 mr-2" />
+              Edit
+            </li>
+            <li className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+              <TrashIcon className="w-5 h-5 mr-2" />
+              Delete
+            </li>
+          </ul>
+        </div>
+      )}
       <div className={bodyStyles}>
         <p>{text}</p>
       </div>
-      {isUser && <div className={tailStyles}></div>}
+      <div className={isUser ? userTailStyles : otherUserTailStyles}></div>
     </div>
   );
 };
--- components/comments/CommentForm.tsx
+++ components/comments/CommentForm.tsx
@@ -12,6 +12,7 @@ interface CommentFormProps {
 function CommentForm({ licencePlate, userId, projectId, onCommentAdded }: CommentFormProps) {
   const [text, setText] = useState('');
   const [isLoading, setLoading] = useState(false);
+  const [showCommentBox, setShowCommentBox] = useState(false); // State to toggle comment box visibility
 
   const mutation = useMutation({
     mutationFn: () => createPrivateCloudComment(licencePlate, text, projectId, userId),
@@ -22,6 +23,7 @@ function CommentForm({ licencePlate, userId, projectId, onCommentAdded }: Commen
       onCommentAdded();
       setText('');
       setLoading(false);
+      setShowCommentBox(false); // Hide the comment box after submitting
     },
     onError: (error: Error) => {
       console.error('Failed to add comment:', error);
@@ -35,43 +37,59 @@ function CommentForm({ licencePlate, userId, projectId, onCommentAdded }: Commen
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <form onSubmit={handleSubmit} style={{ width: '50%' }}>
-        <textarea
-          id="comment"
-          name="comment box"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Leave a comment"
-          style={{
-            width: '100%',
-            height: '150px',
-            padding: '10px',
-            marginBottom: '10px',
-            resize: 'none',
-            borderColor: 'blue',
-            borderWidth: '2px',
-            borderRadius: '4px',
-            boxSizing: 'border-box',
-          }}
-          required
-        />
-        <button
-          type="submit"
-          style={{
-            display: 'block',
-            width: '100%',
-            padding: '10px',
-            backgroundColor: 'gold',
-            border: 'none',
-            borderRadius: '4px',
-            cursor: 'pointer',
-          }}
-          disabled={isLoading}
-        >
-          Post comment
-        </button>
-      </form>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <button
+        onClick={() => setShowCommentBox(!showCommentBox)} // Toggle the visibility of the comment box
+        style={{
+          marginBottom: '10px',
+          padding: '10px 20px',
+          backgroundColor: 'gold',
+          border: 'none',
+          borderRadius: '4px',
+          cursor: 'pointer',
+        }}
+      >
+        {showCommentBox ? 'Cancel' : 'Add Comment'}
+      </button>
+
+      {showCommentBox && ( // Render the form only if showCommentBox is true
+        <form onSubmit={handleSubmit} style={{ width: '50%' }}>
+          <textarea
+            id="comment"
+            name="comment box"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Leave a comment"
+            style={{
+              width: '100%',
+              height: '150px',
+              padding: '10px',
+              marginBottom: '10px',
+              resize: 'none',
+              borderColor: 'blue',
+              borderWidth: '2px',
+              borderRadius: '4px',
+              boxSizing: 'border-box',
+            }}
+            required
+          />
+          <button
+            type="submit"
+            style={{
+              display: 'block',
+              width: '100%',
+              padding: '10px',
+              backgroundColor: 'gold',
+              border: 'none',
+              borderRadius: '4px',
+              cursor: 'pointer',
+            }}
+            disabled={isLoading}
+          >
+            Post comment
+          </button>
+        </form>
+      )}
     </div>
   );
 }
