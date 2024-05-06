4ffe1c7be527565e1ad295a83ff24e796b2024e3
j.dev <junmin.dev@outlook.com>
2024-05-06 15:50:59 -0700

    chore: build styles in advance

--- emails/_templates/private-cloud/AdminCreateRequest.tsx
+++ emails/_templates/private-cloud/AdminCreateRequest.tsx
@@ -10,27 +10,33 @@ interface EmailProp {
   request: PrivateCloudRequestWithRequestedProject;
 }
 
+const styles = [
+  twj('border border-solid border-[#eaeaea] rounded my-4 mx-auto p-4 max-w-xl'),
+  twj('bg-white my-auto mx-auto font-sans text-xs text-darkergrey'),
+  twj('m-12'),
+  twj('pb-6 mt-4 mb-4 border-solid border-0 border-b-1 border-slate-300'),
+  twj('text-lg'),
+  twj('bg-bcorange rounded-md px-4 py-2 text-white'),
+];
+
 const NewRequestTemplate = ({ request }: EmailProp) => {
   if (!request) return <></>;
 
   return (
     <Html>
-      <div style={twj('border border-solid border-[#eaeaea] rounded my-4 mx-auto p-4 max-w-xl')}>
+      <div style={styles[0]}>
         <Header />
-        <Body style={twj('bg-white my-auto mx-auto font-sans text-xs text-darkergrey')}>
-          <div style={twj('m-12')}>
-            <div style={twj('pb-6 mt-4 mb-4 border-solid border-0 border-b-1 border-slate-300')}>
-              <Heading style={twj('text-lg')}>New Request!</Heading>
+        <Body style={styles[1]}>
+          <div style={styles[2]}>
+            <div style={styles[3]}>
+              <Heading style={styles[4]}>New Request!</Heading>
               <Text>Hi Registry Team, </Text>
               <Text>
                 There is a new request that requires your review. Log in to the Registry to review the details. If you
                 have any questions about the request, the PO and TL contact details are included below and in the
                 Registry.
               </Text>
-              <Button
-                href="https://registry.developer.gov.bc.ca/"
-                style={twj('bg-bcorange rounded-md px-4 py-2 text-white')}
-              >
+              <Button href="https://registry.developer.gov.bc.ca/" style={styles[5]}>
                 Review Request
               </Button>
             </div>
