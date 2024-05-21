dddce07c553c531b7dd5239e78bcdb82ae9c0c00
j.dev <junmin.dev@outlook.com>
2024-05-21 10:57:53 -0700

    chore: show error titles only in the form validation notifications

--- components/generic/FormErrorNotification.tsx
+++ components/generic/FormErrorNotification.tsx
@@ -1,8 +1,11 @@
 import { notifications } from '@mantine/notifications';
 import { IconPoint } from '@tabler/icons-react';
-import _get from 'lodash-es/get';
-import React, { useEffect, ReactNode } from 'react';
+import _startCase from 'lodash-es/startCase';
+import React, { useEffect } from 'react';
 import { useFormContext } from 'react-hook-form';
+import { numberToWords } from '@/utils/number';
+
+const NUM_TO_DISPLAY = 5;
 
 export default function FormErrorNotification() {
   const { formState } = useFormContext();
@@ -10,23 +13,32 @@ export default function FormErrorNotification() {
   useEffect(() => {
     const keys = Object.keys(formState.errors);
     if (keys.length > 0) {
+      const keysToDisplay = keys.slice(0, NUM_TO_DISPLAY);
+      const remaining = keys.length - NUM_TO_DISPLAY;
+
       notifications.show({
         color: 'red',
         title: 'Please correct the following errors in the form:',
         autoClose: 5000,
         message: (
           <ul className="text-red-500 list-disc">
-            {keys.map((key) => (
+            {keysToDisplay.map((key) => (
               <li key={key}>
                 <IconPoint className="inline-block" />
-                {_get(formState.errors, `${key}.message`) as ReactNode}
+                {_startCase(key)}
               </li>
             ))}
+            {remaining > 0 && (
+              <li key="remaining">
+                <IconPoint className="inline-block" />
+                and {numberToWords(remaining)} other errors.
+              </li>
+            )}
           </ul>
         ),
       });
     }
-  }, [formState.submitCount]);
+  }, [formState.submitCount, formState.errors]);
 
   return null;
 }
--- utils/number.ts
+++ utils/number.ts
@@ -8,3 +8,20 @@ export const formatNumber = (number: number, options?: { prefix?: string; suffix
   const value = new Intl.NumberFormat('us').format(roundNumber(number, { decimals })).toString();
   return `${prefix}${value}${suffix}`;
 };
+
+export function numberToWords(number: number) {
+  const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  if (number >= 0 && number < 10) {
+    return ones[number];
+  } else if (number >= 11 && number <= 19) {
+    return teens[number - 11];
+  } else if (number >= 10 && number % 10 === 0) {
+    return tens[number / 10 - 1];
+  } else if (number >= 20 && number <= 99) {
+    return tens[Math.floor(number / 10) - 1] + '-' + ones[number % 10];
+  }
+  return 'Number out of range';
+}
