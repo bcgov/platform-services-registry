c5a281f0824714cc3df6d520ee41c69a218f2178
j.dev <junmin.dev@outlook.com>
2024-05-28 10:08:16 -0700

    feat(ui): update header menu design

---
+++ components/layouts/UserMenu.tsx
@@ -0,0 +1,139 @@
+'use client';
+
+import { Menu, UnstyledButton } from '@mantine/core';
+import {
+  IconUserCircle,
+  IconApi,
+  IconVirusSearch,
+  IconScan,
+  IconPresentationAnalytics,
+  IconLogout,
+  IconProps,
+  Icon,
+} from '@tabler/icons-react';
+import Link from 'next/link';
+import { Permissions } from 'next-auth';
+import { useSession } from 'next-auth/react';
+import { useState, ForwardRefExoticComponent, RefAttributes } from 'react';
+import UserProfile from '@/components/dropdowns/UserProfile';
+import { signOut } from '@/helpers/auth';
+import { useAppState } from '@/states/global';
+import ProfileImage from '../ProfileImage';
+
+type IconType = ForwardRefExoticComponent<IconProps & RefAttributes<Icon>>;
+
+interface DividerMenu {
+  divider: boolean;
+}
+
+interface LinkMenu {
+  text: string;
+  Icon: IconType;
+  href: string;
+  permission?: keyof Permissions;
+}
+
+interface ClickMenu {
+  text: string;
+  Icon: IconType;
+  onClick?: () => void;
+  permission?: keyof Permissions;
+}
+
+type MenuType = DividerMenu | LinkMenu | ClickMenu;
+
+const isDividerMenu = (menu: MenuType): menu is DividerMenu => 'divider' in menu;
+const isLinkMenu = (menu: MenuType): menu is LinkMenu => 'href' in menu;
+const isClickMenu = (menu: MenuType): menu is ClickMenu => 'onClick' in menu;
+
+export default function ProfileDropdown() {
+  const [appState, appSnapshot] = useAppState();
+  const { data: session } = useSession();
+  const [isProfileOpen, setProfileOpen] = useState(false);
+
+  if (!session) return null;
+  const { permissions } = session;
+
+  const menus: MenuType[] = [
+    {
+      text: 'Profile',
+      Icon: IconUserCircle,
+      onClick: () => setProfileOpen(true),
+    },
+    {
+      text: 'API Account',
+      Icon: IconApi,
+      href: '/api-account',
+    },
+    {
+      text: 'Private Cloud Analytics',
+      Icon: IconPresentationAnalytics,
+      href: '/private-cloud/analytics',
+      permission: 'viewPrivateAnalytics',
+    },
+    {
+      text: 'Public Cloud Analytics',
+      Icon: IconPresentationAnalytics,
+      href: '/public-cloud/analytics',
+      permission: 'viewPublicAnalytics',
+    },
+    {
+      text: 'Zap Scan Results',
+      Icon: IconVirusSearch,
+      href: '/zapscan/results',
+      permission: 'viewZapscanResults',
+    },
+    {
+      text: 'Sonar Scan Results',
+      Icon: IconScan,
+      href: '/sonarscan/results',
+      permission: 'viewSonarscanResults',
+    },
+    { divider: true },
+    {
+      text: 'Sign Out',
+      Icon: IconLogout,
+      onClick: async () => {
+        await signOut(appSnapshot.info.LOGOUT_URL, session.idToken);
+      },
+    },
+  ];
+
+  return (
+    <>
+      <Menu shadow="md">
+        <Menu.Target>
+          <UnstyledButton>
+            <ProfileImage email={session?.user.email ?? ''} image={session?.user.image ?? ''} />
+          </UnstyledButton>
+        </Menu.Target>
+
+        <Menu.Dropdown>
+          {menus.map((menu, index) => {
+            if (isDividerMenu(menu)) return <Menu.Divider key={index} />;
+            if (menu.permission && !permissions[menu.permission]) return null;
+
+            if (isLinkMenu(menu)) {
+              return (
+                <Menu.Item key={menu.text} component={Link} href={menu.href} leftSection={<menu.Icon size={20} />}>
+                  {menu.text}
+                </Menu.Item>
+              );
+            }
+
+            if (isClickMenu(menu)) {
+              return (
+                <Menu.Item key={menu.text} leftSection={<menu.Icon size={20} />} onClick={menu.onClick}>
+                  {menu.text}
+                </Menu.Item>
+              );
+            }
+
+            return null;
+          })}
+        </Menu.Dropdown>
+      </Menu>
+      {session && isProfileOpen && <UserProfile isOpen={isProfileOpen} onClose={() => setProfileOpen(false)} />}
+    </>
+  );
+}
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,6 +3,45 @@ import NextAuth, { DefaultSession, JWT } from 'next-auth/jwt';
 import { Permissions, PermissionKey } from './permissions';
 
 declare module 'next-auth' {
+  interface Permissions {
+    createPrivateCloudProducts: boolean;
+    viewAllPrivateCloudProducts: boolean;
+    viewAllPrivateCloudProductsHistory: boolean;
+    editAllPrivateCloudProducts: boolean;
+    deleteAllPrivateCloudProducts: boolean;
+    reviewAllPrivateCloudRequests: boolean;
+    createPrivateCloudProductsAsAssignee: boolean;
+    viewAssignedPrivateCloudProducts: boolean;
+    editAssignedPrivateCloudProducts: boolean;
+    deleteAssignedPrivateCloudProducts: boolean;
+
+    createPublicCloudProducts: boolean;
+    viewAllPublicCloudProducts: boolean;
+    viewAllPublicCloudProductsHistory: boolean;
+    editAllPublicCloudProducts: boolean;
+    deleteAllPublicCloudProducts: boolean;
+    reviewAllPublicCloudRequests: boolean;
+    createPublicCloudProductsAsAssignee: boolean;
+    viewAssignedPublicCloudProducts: boolean;
+    editAssignedPublicCloudProducts: boolean;
+    deleteAssignedPublicCloudProducts: boolean;
+
+    createPrivateProductComments: boolean;
+    viewAllPrivateProductComments: boolean;
+    editAllPrivateProductComments: boolean;
+    deleteAllPrivateProductComments: boolean;
+
+    createPublicProductComments: boolean;
+    viewAllPublicProductComments: boolean;
+    editAllPublicProductComments: boolean;
+    deleteAllPublicProductComments: boolean;
+
+    viewZapscanResults: boolean;
+    viewSonarscanResults: boolean;
+    viewPrivateAnalytics: boolean;
+    viewPublicAnalytics: boolean;
+  }
+
   interface Session extends DefaultSession {
     idToken: string;
     kcUserId: string;
@@ -34,44 +73,7 @@ declare module 'next-auth' {
       reader: string[];
       [key: string]: string[];
     };
-    permissions: {
-      createPrivateCloudProducts: boolean;
-      viewAllPrivateCloudProducts: boolean;
-      viewAllPrivateCloudProductsHistory: boolean;
-      editAllPrivateCloudProducts: boolean;
-      deleteAllPrivateCloudProducts: boolean;
-      reviewAllPrivateCloudRequests: boolean;
-      createPrivateCloudProductsAsAssignee: boolean;
-      viewAssignedPrivateCloudProducts: boolean;
-      editAssignedPrivateCloudProducts: boolean;
-      deleteAssignedPrivateCloudProducts: boolean;
-
-      createPublicCloudProducts: boolean;
-      viewAllPublicCloudProducts: boolean;
-      viewAllPublicCloudProductsHistory: boolean;
-      editAllPublicCloudProducts: boolean;
-      deleteAllPublicCloudProducts: boolean;
-      reviewAllPublicCloudRequests: boolean;
-      createPublicCloudProductsAsAssignee: boolean;
-      viewAssignedPublicCloudProducts: boolean;
-      editAssignedPublicCloudProducts: boolean;
-      deleteAssignedPublicCloudProducts: boolean;
-
-      createPrivateProductComments: boolean;
-      viewAllPrivateProductComments: boolean;
-      editAllPrivateProductComments: boolean;
-      deleteAllPrivateProductComments: boolean;
-
-      createPublicProductComments: boolean;
-      viewAllPublicProductComments: boolean;
-      editAllPublicProductComments: boolean;
-      deleteAllPublicProductComments: boolean;
-
-      viewZapscanResults: boolean;
-      viewSonarscanResults: boolean;
-      viewPrivateAnalytics: boolean;
-      viewPublicAnalytics: boolean;
-    };
+    permissions: Permissions;
     previews: {
       security: boolean;
       comments: boolean;
