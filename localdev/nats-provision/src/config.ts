9d16fa7652a779f2b1645b0eee8c869caf594935
j.dev <junmin.dev@outlook.com>
2024-06-05 10:48:23 -0700

    feat(2808): create Keycloak groups for AWS requests

--- localdev/nats-provision/src/config.ts
+++ localdev/nats-provision/src/config.ts
@@ -1,3 +1,7 @@
 export const NATS_HOST = process.env.NATS_HOST || '';
 export const NATS_PORT = process.env.NATS_PORT || '';
 export const APP_URL = process.env.APP_URL || '';
+export const KEYCLOAK_URL = process.env.KEYCLOAK_URL || 'http://localhost:8080';
+export const PUBLIC_CLOUD_REALM_NAME = process.env.PUBLIC_CLOUD_REALM_NAME || 'public-cloud';
+export const PUBLIC_CLOUD_CLIENT_ID = process.env.PUBLIC_CLOUD_CLIENT_ID || 'roles';
+export const PUBLIC_CLOUD_CLIENT_SECRET = process.env.PUBLIC_CLOUD_CLIENT_SECRET || 'testsecret';
