3ef456b6a800ee800f3a635300d898c0c0cb3e58
Kolezanchik <zhanna.kolesnyk@gov.bc.ca>
2024-06-05 10:10:21 -0700

    chore: add app.get for mocks file requests

--- localdev/m365mock/src/server.ts
+++ localdev/m365mock/src/server.ts
@@ -1,25 +1,38 @@
 import express, { Request, Response } from 'express';
 import { readFileSync } from 'fs';
 
-const mocks = JSON.parse(readFileSync('../m365proxy/mocks.json', 'utf-8'));
+let mocks;
+
+try {
+  const jsonData = readFileSync('../m365proxy/mocks.json', 'utf-8');
+  const jsonDataObject = JSON.parse(jsonData);
+
+  if (!Array.isArray(jsonDataObject.mocks)) {
+    throw new Error("Invalid JSON file. 'mocks' property is not an array.");
+  }
+
+  mocks = jsonDataObject.mocks;
+} catch (error) {
+  console.error('Error parsing JSON data:', error);
+  process.exit(1);
+}
 const app = express();
 const port = 4040;
 
-// app.get('/v1.0/$metadata', async (req: Request, res: Response) => {
+app.get('/v1.0/users*', async (req: Request, res: Response) => {
+  const requestedPath = req.path;
+  const requestedQuery = req.query;
+  const filteredMocks = mocks.filter((mock: any) => {
+    return mock.request.url.includes(requestedPath) && mock.request.url.includes(Object.keys(requestedQuery)[0]);
+  });
 
-//   const mockResponse = mocks.mocks.find((mock: any) => {
-//     return (
-//       mock.request.url === 'https://graph.microsoft.com/v1.0/users?$filter*' &&
-//       mock.request.method === 'GET'
-//     );
-//   });
-
-//   if (mockResponse) {
-//     res.json(mockResponse.response.body);
-//   } else {
-//     res.status(404).send('Mock data not found');
-//   }
-// });
+  if (filteredMocks.length > 0) {
+    const mockResponse = filteredMocks[0];
+    res.json(mockResponse.response.body);
+  } else {
+    res.status(404).send('Mock data not found');
+  }
+});
 
 app.get('/', async (req: Request, res: Response) => {
   res.send('Hello, M365 Mock Server!');
