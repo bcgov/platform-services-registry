d99de4535a7e4d0251106d1b1abd07dbf2c23d36
Kolezanchik <zhanna.kolesnyk@gov.bc.ca>
2024-06-05 08:20:47 -0700

    chore: unique mock.json file, tsconfig edit

--- localdev/m365mock/src/server.ts
+++ localdev/m365mock/src/server.ts
@@ -1,42 +1,24 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
 import { readFileSync } from 'fs';
 
+const mocks = JSON.parse(readFileSync('../m365proxy/mocks.json', 'utf-8'));
 const app = express();
 const port = 4040;
 
-interface Mock {
-  request: {
-    method: string;
-    url: string;
-  };
-  response: {
-    statusCode: number;
-    body?: any;
-  };
-}
+// app.get('/v1.0/$metadata', async (req: Request, res: Response) => {
 
-// const mocks = JSON.parse(readFileSync('./mocks.json', 'utf-8'));
-
-// (mocks as { mocks: Mock[] }).mocks.forEach((mock: Mock) => {
-//     console.log('Mock:', mock);
-//     app.get(mock.request.url, async (req: Request, res: Response, next: NextFunction) => {
-//         console.log('Received request:', req);
-//         next();
-//     });
-// });
-
-// app.get('graph.microsoft.com/v1.0/users', async (req: Request, res: Response) => {
-//   res.send({
-//     "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#users/$entity",
-//     "id": "a008b857-36c8-4a17-a79a-518e33effeb1",
-//     "onPremisesSamAccountName": "JOHNDOE",
-//     "userPrincipalName": "john.doe@gov.bc.ca",
-//     "mail": "john.doe@gov.bc.ca",
-//     "displayName": "Doe, John CITZ:EX",
-//     "givenName": "John",
-//     "surname": "Doe",
-//     "jobTitle": ""
+//   const mockResponse = mocks.mocks.find((mock: any) => {
+//     return (
+//       mock.request.url === 'https://graph.microsoft.com/v1.0/users?$filter*' &&
+//       mock.request.method === 'GET'
+//     );
 //   });
+
+//   if (mockResponse) {
+//     res.json(mockResponse.response.body);
+//   } else {
+//     res.status(404).send('Mock data not found');
+//   }
 // });
 
 app.get('/', async (req: Request, res: Response) => {
