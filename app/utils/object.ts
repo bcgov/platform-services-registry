61536027748da43483b6c1859854e4e74d8d3ac6
j.dev <junmin.dev@outlook.com>
2024-06-11 13:22:02 -0700

    test(3046): add api test for createPrivateCloudProject endpoint

---
+++ app/utils/object.ts
@@ -0,0 +1,11 @@
+export function pick<T extends object, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
+  return keys.reduce(
+    (result, key) => {
+      if (key in obj) {
+        result[key] = obj[key];
+      }
+      return result;
+    },
+    {} as Pick<T, K>,
+  );
+}
