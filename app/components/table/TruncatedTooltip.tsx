8f494633cca8d4d48217ccc0ebbdbceade2f4e81
Amritpal Nijjar <amritpal.nijjar@gov.bc.ca>
2024-06-20 16:52:50 -0700

    chore(3093): refactor TruncatedTooltip common component

--- app/components/table/TruncatedTooltip.tsx
+++ app/components/table/TruncatedTooltip.tsx
@@ -1,26 +1,41 @@
-import { Tooltip } from '@mantine/core';
+import { Tooltip, TooltipProps } from '@mantine/core';
 import _truncate from 'lodash-es/truncate';
 import React from 'react';
 
-interface TruncatedTooltipProps {
-  label: string;
+type TruncatedTooltipProps = TooltipProps & {
+  label: string | React.ReactNode;
   maxLength?: number;
-  children: React.ReactNode;
-}
+  maxWidth?: number;
+};
 
-const TruncatedTooltip: React.FC<TruncatedTooltipProps> = ({ label, maxLength = 300, children }) => {
+function TruncatedTooltip({
+  label,
+  maxLength = 300,
+  maxWidth = 400,
+  children,
+  ...tooltipProps
+}: TruncatedTooltipProps) {
+  const isStringLabel = typeof label === 'string';
+
   return (
     <Tooltip
       label={
-        <div className="whitespace-pre-wrap break-words max-w-[400px]">{_truncate(label, { length: maxLength })}</div>
+        isStringLabel ? (
+          <div className="tooltip-content max-w-[400px] whitespace-pre-wrap break-words">
+            {_truncate(label as string, { length: maxLength })}
+          </div>
+        ) : (
+          label
+        )
       }
       offset={10}
       position="top-start"
       withArrow
+      {...tooltipProps}
     >
       {children}
     </Tooltip>
   );
-};
+}
 
 export default TruncatedTooltip;
