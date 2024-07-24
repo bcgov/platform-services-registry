c9ff432e3d1415172ebb8d09c567c6c8a2536f90
j.dev <junmin.dev@outlook.com>
2024-07-24 15:32:26 -0700

    feat(2759): add mantine color theme

--- app/app/mantine-theme.ts
+++ app/app/mantine-theme.ts
@@ -7,16 +7,23 @@ import { createTheme } from '@mantine/core';
 // See https://mantine.dev/styles/variants-sizes/#adding-custom-variants
 export const theme = createTheme({
   colors: {
-    primary: generateColors('#2065a8'),
-
-    secondary: generateColors('#868E96'),
-
-    success: generateColors('#3CB371'),
-
+    primary: generateColors('#003366'),
+    secondary: generateColors('#868e96'),
+    success: generateColors('#3cb371'),
     danger: generateColors('#d42222'),
-
-    warning: generateColors('#FCBA19'),
-
+    warning: generateColors('#fcba19'),
     info: generateColors('#2bceff'),
+    dark: [
+      '#d5d7e0',
+      '#acaebf',
+      '#8c8fa3',
+      '#666980',
+      '#4d4f66',
+      '#34354a',
+      '#2b2c3d',
+      '#1d1e30',
+      '#0c0d21',
+      '#01010a',
+    ],
   },
 });
