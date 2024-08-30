ebed6673955e6efb9154457ae3d856a661da8954
Kolezhanchik <zhanna.kolesnyk@gov.bc.ca>
2024-08-30 11:41:41 -0700

    feat(3471): add usage metrics for namespaces pods

---
+++ app/app/api/private-cloud/analytics/cpu-usage/route.ts
@@ -0,0 +1,47 @@
+import { KubeConfig, CoreV1Api, Metrics } from '@kubernetes/client-node';
+const clusterName = 'silver';
+const systemNamespace = 'de0974-prod';
+
+async function getPodMetrics() {
+  const kc = new KubeConfig();
+  kc.loadFromOptions({
+    clusters: [
+      {
+        name: 'silver',
+        server: `https://api.${clusterName}.devops.gov.bc.ca:6443`,
+        skipTLSVerify: false,
+      },
+    ],
+    users: [
+      {
+        name: 'my-user',
+        token: 'xxxxx', // oc whoami -t  -- personal temporary token
+      },
+    ],
+    contexts: [
+      {
+        name: `${clusterName}-context`,
+        user: 'my-user',
+        cluster: clusterName,
+      },
+    ],
+    currentContext: `${clusterName}-context`,
+  });
+
+  // const k8sApi = kc.makeApiClient(CoreV1Api);
+  const metricsClient = new Metrics(kc);
+
+  try {
+    // const pods = await k8sApi.listNamespacedPod(systemNamespace);
+    // console.log('pods body', pods.body);
+
+    const metrics = await metricsClient.getPodMetrics(systemNamespace);
+    metrics.items.forEach((item) => {
+      console.log(item.metadata.name, item.containers);
+    });
+  } catch (err) {
+    console.error('Error fetching metrics:', err);
+  }
+}
+
+getPodMetrics();
