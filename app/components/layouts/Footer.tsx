6c4380c6411a68d8c47c06e44b81b2a41716ce61
j.dev <junmin.dev@outlook.com>
2024-09-03 14:12:07 -0700

    fix(3641): resolve a bug displaying multiple app versions in footer

--- app/components/layouts/Footer.tsx
+++ app/components/layouts/Footer.tsx
@@ -1,19 +1,19 @@
 import { useAppState } from '@/states/global';
 
-const navigation = {
-  main: [
-    // { name: 'Home', href: '#' }, a home page does not currently exist
-    { name: 'Disclaimer', href: 'https://www2.gov.bc.ca/gov/content/home/disclaimer' },
-    { name: 'Privacy', href: 'https://www2.gov.bc.ca/gov/content/home/privacy' },
-    { name: 'Accessibility', href: 'https://www2.gov.bc.ca/gov/content/home/accessible-government' },
-    { name: 'Copyright', href: 'https://www2.gov.bc.ca/gov/content/home/copyright' },
-    { name: 'Contact Us', href: 'https://chat.developer.gov.bc.ca/channel/devops-registry' },
-    { name: 'Report a bug/Request a feature', href: 'https://github.com/bcgov/platform-services-registry/issues' },
-  ],
-};
-
 export default function Footer() {
   const [, appSnapshot] = useAppState();
+
+  const navigation = {
+    main: [
+      // { name: 'Home', href: '#' }, a home page does not currently exist
+      { name: 'Disclaimer', href: 'https://www2.gov.bc.ca/gov/content/home/disclaimer' },
+      { name: 'Privacy', href: 'https://www2.gov.bc.ca/gov/content/home/privacy' },
+      { name: 'Accessibility', href: 'https://www2.gov.bc.ca/gov/content/home/accessible-government' },
+      { name: 'Copyright', href: 'https://www2.gov.bc.ca/gov/content/home/copyright' },
+      { name: 'Contact Us', href: 'https://chat.developer.gov.bc.ca/channel/devops-registry' },
+      { name: 'Report a bug/Request a feature', href: 'https://github.com/bcgov/platform-services-registry/issues' },
+    ],
+  };
 
   if (appSnapshot.info.DEPLOYMENT_TAG) {
     const ref = appSnapshot.info.DEPLOYMENT_TAG.includes('.')
