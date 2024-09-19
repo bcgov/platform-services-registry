1004aaf12c791c3f9763c3faa1ba14a1416989d2
j.dev <junmin.dev@outlook.com>
2024-09-19 09:59:13 -0700

    fix(3795): remove duplicate group names

--- app/app/api/public-cloud/aws-roles/helpers.ts
+++ app/app/api/public-cloud/aws-roles/helpers.ts
@@ -1,7 +1,9 @@
 import KcAdminClient from '@keycloak/keycloak-admin-client';
 import { Credentials } from '@keycloak/keycloak-admin-client/lib/utils/auth';
+import _compact from 'lodash-es/compact';
 import _kebabCase from 'lodash-es/kebabCase';
 import _startCase from 'lodash-es/startCase';
+import _uniq from 'lodash-es/uniq';
 import {
   AWS_ROLES_BASE_URL,
   AWS_ROLES_REALM_NAME,
@@ -172,11 +174,13 @@ async function findParentGroup(groupName = PROJECT_GROUP) {
 
 async function listSubGroups({ parentId, search }: { parentId: string; search?: string }) {
   const subGroups = [];
+
+  const max = 100;
   let first = 0;
 
   while (true) {
-    const groups = await kcAdminClient.groups.listSubGroups({ parentId, search, first, max: 100 });
-    if (groups.length === 0) break;
+    const groups = await kcAdminClient.groups.listSubGroups({ parentId, search, first, max });
+    if (groups.length < max) break;
     subGroups.push(...groups);
     first += 1;
   }
@@ -248,5 +252,6 @@ export async function addUserToGroupByEmail(userPrincipalName: string, userEmail
 
 export async function getGroupsNamesByLicencePlate(licencePlate: string) {
   const productRoleGroups = await getProductRoleGroups(licencePlate);
-  return productRoleGroups.map((group) => parseGroupNameToTab(group.name as string));
+  const groupNames = _compact(_uniq(productRoleGroups.map((group) => group.name)));
+  return groupNames.map(parseGroupNameToTab);
 }
