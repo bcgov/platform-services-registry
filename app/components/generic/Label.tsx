27e218782427c8f1527d229a652adb55bc1b1d3c
j.dev <junmin.dev@outlook.com>
2024-09-20 16:51:04 -0700

    feat(3165): add label and select generic components

---
+++ app/components/generic/Label.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import classNames from 'classnames';
+import React from 'react';
+
+export default function Label({
+  children,
+  htmlFor,
+  className = '',
+  required = false,
+}: {
+  children: React.ReactNode;
+  htmlFor: string;
+  className?: string;
+  required?: boolean;
+}) {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={classNames('block text-sm font-medium leading-6 text-gray-900 mb--1', className)}
+    >
+      {children}
+      {required && <span className="text-red-500">*</span>}
+    </label>
+  );
+}
