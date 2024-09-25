959056aac746213ad2207a752c70fdd74584baa0
j.dev <junmin.dev@outlook.com>
2024-09-25 15:46:25 -0700

    chore(2827): reuse FormError in HookFormMultiSelect

--- app/components/generic/select/HookFormMultiSelect.tsx
+++ app/components/generic/select/HookFormMultiSelect.tsx
@@ -2,6 +2,7 @@
 
 import _kebabCase from 'lodash-es/kebabCase';
 import { Controller, FieldValues, Path, useFormContext } from 'react-hook-form';
+import FormError from '../FormError';
 import { HookFormRules } from '../types';
 import FormMultiSelect, { FormMultiSelectProps } from './FormMultiSelect';
 
@@ -13,32 +14,35 @@ export default function HookFormMultiSelect<T extends FieldValues>({
   data,
   classNames,
   disabled = false,
-}: Omit<FormMultiSelectProps, 'name'> & {
+}: Omit<FormMultiSelectProps, 'name' | 'onChange' | 'onBlur' | 'value'> & {
   rules?: HookFormRules<T>;
   name: Path<T>;
 }) {
   const { control } = useFormContext<T>();
 
   return (
-    <Controller
-      control={control}
-      name={name}
-      rules={rules}
-      render={({ field: { onChange, onBlur, value } }) => {
-        return (
-          <FormMultiSelect
-            id={id}
-            name={name}
-            label={label}
-            data={data}
-            onChange={onChange}
-            onBlur={onBlur}
-            value={value}
-            disabled={disabled}
-            classNames={classNames}
-          />
-        );
-      }}
-    />
+    <>
+      <Controller
+        control={control}
+        name={name}
+        rules={rules}
+        render={({ field: { onChange, onBlur, value } }) => {
+          return (
+            <FormMultiSelect
+              id={id}
+              name={name}
+              label={label}
+              data={data}
+              onChange={onChange}
+              onBlur={onBlur}
+              value={value}
+              disabled={disabled}
+              classNames={classNames}
+            />
+          );
+        }}
+      />
+      <FormError field={name} className="mt-1" />
+    </>
   );
 }
