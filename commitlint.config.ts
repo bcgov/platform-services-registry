055e3b5b932d594dd39893c751459b4ebd71d29d
Chinedu Olebu <clashofdunes@gmail.com>
2024-09-28 00:00:25 -0700

    fix(fixing): this is fixing

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,8 +2,21 @@ import type { UserConfig } from '@commitlint/types';
 
 const config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  'scope-numeric': [
+    2,
+    'always',
+    (parsed: any) => {
+      const scope = parsed.scope;
+      return scope && /^[0-9]+$/.test(scope) ? [true] : [false, 'Scope must be a number'];
+    },
+  ],
+  allowCustomScopes: true,
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'update']],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'update', 'ci', 'build'],
+    ],
   },
 };
 
