b0c7f73d9a7a5a4283d7be0e7be42c50d329fe29
Chinedu Olebu <clashofdunes@gmail.com>
2024-09-28 00:01:22 -0700

    feat: feating

--- app/app/api/admin/task/sign-mou/resend/route.ts
+++ app/app/api/admin/task/sign-mou/resend/route.ts
@@ -16,6 +16,8 @@ const apiHandler = createApiHandler({
 export const GET = apiHandler(async () => {
   const unsignedBillings = await prisma.billing.findMany({ where: { signed: false } });
 
+  console.log('Thanks');
+
   const result = {
     unsignedBillings: 0,
     tasks: 0,
