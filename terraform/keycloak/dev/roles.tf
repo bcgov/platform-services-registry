43786845a913876ef4d8bb999991da17c94c7a31
j.dev <junmin.dev@outlook.com>
2024-10-10 16:08:23 -0700

    feat(3937): add a billing reader role

--- terraform/keycloak/dev/roles.tf
+++ terraform/keycloak/dev/roles.tf
@@ -101,3 +101,11 @@ resource "keycloak_role" "pltsvc_billing_reviewer" {
   name        = "billing-reviewer"
   description = "Registry Billing Reviewer"
 }
+
+resource "keycloak_role" "pltsvc_billing_reader" {
+  realm_id  = data.keycloak_realm.pltsvc.id
+  client_id = keycloak_openid_client.pltsvc.id
+
+  name        = "billing-reader"
+  description = "Registry Billing Reader"
+}
