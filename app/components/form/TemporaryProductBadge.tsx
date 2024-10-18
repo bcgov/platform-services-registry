13056d7b08e13d59a51b87bb9f46862156ec4c9f
j.dev <junmin.dev@outlook.com>
2024-10-18 13:51:45 -0700

    chore(4018): enhance minor UI elements

--- app/components/form/TemporaryProductBadge.tsx
+++ app/components/form/TemporaryProductBadge.tsx
@@ -12,18 +12,19 @@ export default function TemporaryProductBadge({
   className?: string;
 }) {
   let label = null;
+  let color = 'gray';
   if (data?.createdAt) {
     const diffInDays = 30 - differenceInDays(new Date(), new Date(data.createdAt));
-    label = `${Math.abs(diffInDays)} ${
-      diffInDays > 0 ? 'days until product deletion' : 'days overdue for automatic deletion'
-    }`;
+    const active = diffInDays > 0;
+    label = `${Math.abs(diffInDays)} ${active ? 'days until product deletion' : 'days overdue for automatic deletion'}`;
+    color = active ? 'warning' : 'danger';
   } else {
     label = 'This product has not been set up yet';
   }
 
   return (
     <Tooltip label={label} position="top" offset={10} hidden={!label}>
-      <Badge autoContrast size="md" color="yellow" radius="sm" className={classNames('mt-1', className)}>
+      <Badge autoContrast size="md" color={color} radius="sm" className={classNames('mt-1', className)}>
         Temp
       </Badge>
     </Tooltip>
