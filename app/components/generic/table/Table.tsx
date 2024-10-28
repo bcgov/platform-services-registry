28015fd0f1137a583733e6134eca2bab5a9f3561
j.dev <junmin.dev@outlook.com>
2024-10-27 22:27:32 -0700

    chore(4055): remove react context use to avoid router conflicts

--- app/components/generic/table/Table.tsx
+++ app/components/generic/table/Table.tsx
@@ -1,24 +1,11 @@
 'use client';
 
-import { Button, Divider, Grid, LoadingOverlay, Box, ComboboxData } from '@mantine/core';
-import { createContext, useContext, useRef, useEffect } from 'react';
-import { proxy, useSnapshot } from 'valtio';
+import { LoadingOverlay, Box, ComboboxData } from '@mantine/core';
+import { useState } from 'react';
 import Pagination from './Pagination';
 import SearchFilterExport from './SearchFilterExport';
 import TableFooter from './TableFooter';
 import TableHeader from './TableHeader';
-
-const defaultValue = {
-  page: 0,
-  pageSize: 0,
-  totalCount: 0,
-  search: '',
-  sortKey: '',
-  onPagination: (page: number, pageSize: number) => {},
-  isLoading: false,
-};
-
-const TableContext = createContext(defaultValue);
 
 export default function Table({
   title = '',
@@ -53,51 +40,37 @@ export default function Table({
   isLoading?: boolean;
   children: React.ReactNode;
 }) {
-  const state = useRef(proxy({ ...defaultValue })).current;
-
-  useEffect(() => {
-    state.page = page;
-    state.pageSize = pageSize;
-    state.totalCount = totalCount;
-    state.search = search;
-    state.sortKey = sortKey;
-    state.onPagination = onPagination;
-    state.isLoading = isLoading;
-  }, [state, page, pageSize, totalCount, search, onPagination, isLoading]);
-
   return (
-    <TableContext.Provider value={state}>
-      <div className="border-2 rounded-xl overflow-hidden">
-        <TableHeader title={title} description={description}>
-          {(onSearch || onExport || filters) && (
-            <SearchFilterExport
-              initialSearch={search}
-              onSearch={onSearch}
-              onExport={onExport}
-              onSort={onSort}
-              sortOptions={sortOptions}
-              sortKey={sortKey}
-            >
-              {filters}
-            </SearchFilterExport>
-          )}
-        </TableHeader>
+    <div className="border-2 rounded-xl overflow-hidden">
+      <TableHeader title={title} description={description}>
+        {(onSearch || onExport || filters) && (
+          <SearchFilterExport
+            initialSearch={search}
+            onSearch={onSearch}
+            onExport={onExport}
+            onSort={onSort}
+            sortOptions={sortOptions}
+            sortKey={sortKey}
+          >
+            {filters}
+          </SearchFilterExport>
+        )}
+      </TableHeader>
 
-        <Box pos="relative" className="min-h-96">
-          <LoadingOverlay visible={isLoading} zIndex={1000} overlayProps={{ radius: 'sm', blur: 2 }} />
-          {children}
-        </Box>
+      <Box pos="relative" className="min-h-96">
+        <LoadingOverlay visible={isLoading} zIndex={1000} overlayProps={{ radius: 'sm', blur: 2 }} />
+        {children}
+      </Box>
 
-        <TableFooter>
-          <Pagination />
-        </TableFooter>
-      </div>
-    </TableContext.Provider>
+      <TableFooter>
+        <Pagination
+          page={page}
+          pageSize={pageSize}
+          totalCount={totalCount}
+          onPagination={onPagination}
+          isLoading={isLoading}
+        />
+      </TableFooter>
+    </div>
   );
-}
-
-export function useTableState() {
-  const state = useContext(TableContext);
-  const snapshot = useSnapshot(state);
-  return { state, snapshot };
 }
