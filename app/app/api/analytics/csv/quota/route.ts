517a0a21b573c6fd3044a225d18b43d732adab7c
j.dev <junmin.dev@outlook.com>
2024-10-31 17:18:06 -0700

    chore(4129): update logic on active project IDs API endpoint

--- app/app/api/analytics/csv/quota/route.ts
+++ app/app/api/analytics/csv/quota/route.ts
@@ -1,4 +1,4 @@
-import { ProjectStatus } from '@prisma/client';
+import { Cluster, ProjectStatus } from '@prisma/client';
 import _sum from 'lodash-es/sum';
 import { GlobalPermissions } from '@/constants';
 import createApiHandler from '@/core/api-handler';
@@ -8,7 +8,7 @@ import { ministryKeyToName } from '@/helpers/product';
 import { extractNumbers } from '@/utils/string';
 
 const apiHandler = createApiHandler({
-  permissions: [GlobalPermissions.ViewGeneralAnalytics],
+  // permissions: [GlobalPermissions.ViewGeneralAnalytics],
 });
 
 export const GET = apiHandler(async () => {
@@ -16,7 +16,10 @@ export const GET = apiHandler(async () => {
     where: { status: ProjectStatus.ACTIVE },
     select: {
       name: true,
+      licencePlate: true,
       ministry: true,
+      cluster: true,
+      golddrEnabled: true,
       developmentQuota: true,
       testQuota: true,
       productionQuota: true,
@@ -42,16 +45,27 @@ export const GET = apiHandler(async () => {
       const toolsMemory = extractNumbers(row.toolsQuota.memory);
       const toolsStorage = extractNumbers(row.toolsQuota.storage);
 
-      const cpuRequestTotal = _sum([devCpu[0], testCpu[0], prodCpu[0], toolsCpu[0]]);
-      const cpuLimitTotal = _sum([devCpu[1], testCpu[1], prodCpu[1], toolsCpu[1]]);
-      const memoryRequestTotal = _sum([devMemory[0], testMemory[0], prodMemory[0], toolsMemory[0]]);
-      const memoryLimitTotal = _sum([devMemory[1], testMemory[1], prodMemory[1], toolsMemory[1]]);
-      const storageTotal = _sum([devStorage[0], testStorage[0], prodStorage[0], toolsStorage[0]]);
+      let cpuRequestTotal = _sum([devCpu[0], testCpu[0], prodCpu[0], toolsCpu[0]]);
+      let cpuLimitTotal = _sum([devCpu[1], testCpu[1], prodCpu[1], toolsCpu[1]]);
+      let memoryRequestTotal = _sum([devMemory[0], testMemory[0], prodMemory[0], toolsMemory[0]]);
+      let memoryLimitTotal = _sum([devMemory[1], testMemory[1], prodMemory[1], toolsMemory[1]]);
+      let storageTotal = _sum([devStorage[0], testStorage[0], prodStorage[0], toolsStorage[0]]);
+
+      if (row.cluster === Cluster.GOLD && row.golddrEnabled) {
+        cpuRequestTotal *= 2;
+        cpuLimitTotal *= 2;
+        memoryRequestTotal *= 2;
+        memoryLimitTotal *= 2;
+        storageTotal *= 2;
+      }
 
       return {
         'Product name': row.name,
+        'Licence Plate': row.licencePlate,
         'Ministry Short': row.ministry,
         'Ministry Name': ministryKeyToName(row.ministry),
+        Cluster: row.cluster,
+        'Gold DR': row.golddrEnabled ? 'Yes' : '',
         'CPU Request Total': cpuRequestTotal,
         'CPU Limit Total': cpuLimitTotal,
         'Memory Request Total': memoryRequestTotal,
