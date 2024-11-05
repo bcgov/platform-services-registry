5e7650b746170d9ce5d1969e054f49ec07d2c94b
j.dev <junmin.dev@outlook.com>
2024-11-05 12:01:15 -0800

    fix(4165): replace generic select with dropdown for cluster

--- app/components/form/ProjectDescriptionPrivate.tsx
+++ app/components/form/ProjectDescriptionPrivate.tsx
@@ -10,6 +10,7 @@ import ExternalLink from '@/components/generic/button/ExternalLink';
 import MailLink from '@/components/generic/button/MailLink';
 import FormSelect from '@/components/generic/select/FormSelect';
 import { clusters, ministryOptions, privateCloudTeamEmail } from '@/constants';
+import HookFormSingleSelect from '@/components/generic/select/HookFormSingleSelect';
 
 export default function ProjectDescriptionPrivate({
   mode,
@@ -38,7 +39,7 @@ export default function ProjectDescriptionPrivate({
     if (session && !session?.permissions.viewAllPrivateCloudProducts) {
       setClustersList(clusters.filter((cluster) => cluster.indexOf('LAB') === -1));
     }
-  }, [session]);
+  }, [session, setClustersList]);
 
   let temporaryProduct = null;
   if (mode === 'create') {
@@ -123,15 +124,11 @@ export default function ProjectDescriptionPrivate({
           {['create', 'edit'].includes(mode) && <AGMinistryCheckBox disabled={disabled} />}
         </div>
         <div className="sm:col-span-3 sm:ml-10">
-          <FormSelect
-            id="cluster"
+          <HookFormSingleSelect
+            name="cluster"
             label="Hosting tier"
             disabled={disabled || clusterDisabled}
-            options={[
-              { label: 'Select Hosting tier', value: '' },
-              ...clustersList.map((v) => ({ label: v, value: v })),
-            ]}
-            selectProps={register('cluster')}
+            data={[{ label: 'Select Hosting tier', value: '' }, ...clustersList.map((v) => ({ label: v, value: v }))]}
           />
           <p className={classNames(errors.cluster ? 'text-red-400' : '', 'mt-3 text-sm leading-6 text-gray-600')}>
             {session?.isAdmin
---
+++ app/components/generic/select/HookFormSingleSelect.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { Controller, FieldValues, Path, useFormContext } from 'react-hook-form';
+import FormError from '../FormError';
+import { HookFormRules } from '../types';
+import FormSingleSelect, { FormSingleSelectProps } from './FormSingleSelect';
+
+export default function HookFormSingleSelect<T extends FieldValues>({
+  id,
+  name,
+  label,
+  rules,
+  data,
+  classNames,
+  disabled = false,
+  showError = true,
+}: Omit<FormSingleSelectProps, 'name' | 'onChange' | 'onBlur' | 'value'> & {
+  rules?: HookFormRules<T>;
+  name: Path<T>;
+  showError?: boolean;
+}) {
+  const { control } = useFormContext<T>();
+
+  return (
+    <>
+      <Controller
+        control={control}
+        name={name}
+        rules={rules}
+        render={({ field: { onChange, onBlur, value } }) => {
+          return (
+            <FormSingleSelect
+              id={id}
+              name={name}
+              label={label}
+              data={data}
+              onChange={onChange}
+              onBlur={onBlur}
+              value={value}
+              disabled={disabled}
+              classNames={classNames}
+            />
+          );
+        }}
+      />
+      {showError && <FormError field={name} className="mt-1" />}
+    </>
+  );
+}
