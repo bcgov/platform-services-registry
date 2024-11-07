523725d0ca744e2eacc7410bba33eab996a3f14a
j.dev <junmin.dev@outlook.com>
2024-11-07 09:42:43 -0800

    ci(4128): add commitlint configuration

--- .bin/commitlint.sh
+++ .bin/commitlint.sh
@@ -3,4 +3,4 @@
 editmsg="$(git rev-parse --git-path COMMIT_EDITMSG)"
 
 # shellcheck disable=SC2086
-npx commitlint --edit "$editmsg" --verbose
+npx commitlint --config commitlint.config.mjs --edit "$editmsg" --verbose
