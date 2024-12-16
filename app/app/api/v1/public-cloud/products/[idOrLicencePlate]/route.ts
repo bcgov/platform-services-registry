72f7dd417a2f078bc25750ba625f17168b09e7ef
j.dev <junmin.dev@outlook.com>
2024-12-16 09:28:09 -0800

    chore(4397): update GET product API endpoint permissions

--- app/app/api/v1/public-cloud/products/[idOrLicencePlate]/route.ts
+++ app/app/api/v1/public-cloud/products/[idOrLicencePlate]/route.ts
@@ -1,11 +1,10 @@
 import { z } from 'zod';
-import { GlobalRole } from '@/constants';
 import createApiHandler from '@/core/api-handler';
-import { OkResponse } from '@/core/responses';
 import readOp from '../_operations/read';
 
 export const GET = createApiHandler({
-  roles: [GlobalRole.User],
+  roles: ['service-account user'],
+  useServiceAccount: true,
   validations: {
     pathParams: z.object({
       idOrLicencePlate: z.string().max(24),
@@ -14,6 +13,6 @@ export const GET = createApiHandler({
 })(async ({ pathParams, session }) => {
   const { idOrLicencePlate } = pathParams;
 
-  const result = await readOp({ session, idOrLicencePlate });
-  return OkResponse(result);
+  const response = await readOp({ session, idOrLicencePlate });
+  return response;
 });
