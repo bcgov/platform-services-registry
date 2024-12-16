1864c8db1b1f3317859780e1be3b67381ea36bc6
j.dev <junmin.dev@outlook.com>
2024-12-16 14:18:54 -0800

    chore(4397): fix builds

--- app/helpers/mock-resources/private-cloud-product.ts
+++ app/helpers/mock-resources/private-cloud-product.ts
@@ -125,6 +125,7 @@ export function createSamplePrivateCloudProduct(args?: {
     temporaryProductNotificationDate: new Date(),
     requests: [],
     activeRequest: null,
+    webhookUrl: '',
     ...data,
   };
 
