f0d06389e65339c7db901d0264b255fabb82be94
Kolezhanchik <zhanna.kolesnyk@gov.bc.ca>
2025-02-20 17:27:37 -0800

    chore(4876): remove types from page state

--- app/app/analytics/general/state.ts
+++ app/app/analytics/general/state.ts
@@ -1,10 +1,8 @@
-import { EventType } from '@prisma/client';
 import { proxy } from 'valtio';
 import { deepClone } from 'valtio/utils';
 import { AnalyticsGeneralFilterBody } from '@/validation-schemas/analytics-general';
 
 const initialValue = {
-  types: [EventType.LOGIN],
   dates: [new Date(new Date().setMonth(new Date().getMonth() - 3)).toISOString(), new Date().toISOString()],
   userId: '',
 };
