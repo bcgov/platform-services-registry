8d6baded11a077c74486e7025ff9174eed3d5f78
Kolezhanchik <zhanna.kolesnyk@gov.bc.ca>
2025-03-17 09:15:47 -0700

    chore(4851): revert variable name

--- app/services/db/models/private-cloud-product-webhook.ts
+++ app/services/db/models/private-cloud-product-webhook.ts
@@ -48,7 +48,7 @@ async function decorate<T extends PrivateCloudProductWebhookSimple | PrivateClou
   doc: T,
   session: Session,
 ) {
-  const decorated = doc as T & PrivateCloudProductWebhookDecorate;
+  const decoratedDoc = doc as T & PrivateCloudProductWebhookDecorate;
   const userId = session.user.id;
 
   const product = await prisma.privateCloudProject.findUnique({
@@ -89,72 +89,13 @@ async function decorate<T extends PrivateCloudProductWebhookSimple | PrivateClou
         member.userId === userId && arraysIntersect(member.roles, [PrivateCloudProductMemberRole.EDITOR]),
     );
 
-  decorated._permissions = {
+  decoratedDoc._permissions = {
     view: canView,
     edit: canEdit,
     delete: canEdit,
   };
 
-  return decorated;
-  async function decorate<T extends PrivateCloudProductWebhookSimple | PrivateCloudProductWebhookDetail>(
-    doc: T,
-    session: Session,
-  ) {
-    const decorated = doc as T & PrivateCloudProductWebhookDecorate;
-    const userId = session.user.id;
-
-    // Use the global permissions directly.
-    const canEditGlobal = session.permissions.editWebhook;
-    const canViewGlobal = session.permissions.viewWebhook;
-
-    // Fetch associated product to determine product-based permissions.
-    const product = await prisma.privateCloudProject.findUnique({
-      where: { licencePlate: doc.licencePlate },
-      select: {
-        projectOwnerId: true,
-        primaryTechnicalLeadId: true,
-        secondaryTechnicalLeadId: true,
-        ministry: true,
-        members: true,
-      },
-    });
-
-    const isMyProduct = product
-      ? product.projectOwnerId === userId ||
-        product.primaryTechnicalLeadId === userId ||
-        product.secondaryTechnicalLeadId === userId
-      : false;
-    const members = product?.members || [];
-
-    const canEdit =
-      canEditGlobal ||
-      isMyProduct ||
-      (product ? session.ministries.editor.includes(product.ministry) : false) ||
-      members.some(
-        (member: any) =>
-          member.userId === userId && arraysIntersect(member.roles, [PrivateCloudProductMemberRole.EDITOR]),
-      );
-
-    const canView =
-      canViewGlobal ||
-      isMyProduct ||
-      (product
-        ? session.ministries.reader.includes(product.ministry) || session.ministries.editor.includes(product.ministry)
-        : false) ||
-      members.some(
-        (member: any) =>
-          member.userId === userId &&
-          arraysIntersect(member.roles, [PrivateCloudProductMemberRole.EDITOR, PrivateCloudProductMemberRole.VIEWER]),
-      );
-
-    decorated._permissions = {
-      view: canView,
-      edit: canEdit,
-      delete: canEdit,
-    };
-
-    return decorated;
-  }
+  return decoratedDoc;
 }
 
 export const privateCloudProductWebhookModel = createSessionModel<
