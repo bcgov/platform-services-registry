55b75a1fafc63932163f91bbd2e995aa992ccab4
Chinedu Olebu <clashofdunes@gmail.com>
2025-04-08 08:30:47 -0700

    chore(5124): update db users using valid azure user data

--- helm/tools/dags/_sync_azure_ad_with_db.py
+++ helm/tools/dags/_sync_azure_ad_with_db.py
@@ -7,8 +7,9 @@ def sync_db_users_with_azure_ad(
 ):
     try:
         db = get_mongo_db(mongo_conn_id)
-        excluded_fields = {"image": 0, "ministry": 0, "lastseen": 0, "updatedAt": 0, "createdAt": 0}
-        db_users = db.User.find({}, excluded_fields)
+        users_collection = db["User"]
+        projection = {"image": 0, "ministry": 0, "lastseen": 0, "updatedAt": 0, "createdAt": 0}
+        db_users = users_collection.find({}, projection)
 
         ms_graph = MsGraph(ms_graph_api_tenant_id, ms_graph_api_client_id, ms_graph_api_client_secret)
         azure_users = ms_graph.get_azure_users()
@@ -18,20 +19,21 @@ def sync_db_users_with_azure_ad(
             db_user_email = db_user["email"].lower()
 
             if db_user_email in azure_emails:
-                azure_data = azure_emails[db_user_email]
-                db.User.update_one(
-                    {"_id": db_user["_id"]},
-                    {
-                        "$set": {
-                            "officeLocation": azure_data["officeLocation"],
-                            "jobTitle": azure_data["jobTitle"],
-                            "upn": azure_data["userPrincipalName"],
-                            "providerUserId": azure_data["id"],
-                        }
-                    },
-                )
+                azure_user_data = azure_emails[db_user_email]
+                valid_data_to_update = {
+                    k: v
+                    for k, v in {
+                        "officeLocation": azure_user_data.get("officeLocation"),
+                        "jobTitle": azure_user_data.get("jobTitle"),
+                        "upn": azure_user_data.get("userPrincipalName"),
+                        "providerUserId": azure_user_data.get("id"),
+                    }.items()
+                    if v is not None
+                }
+                if valid_data_to_update:
+                    users_collection.update_one({"_id": db_user["_id"]}, {"$set": valid_data_to_update})
             else:
-                db.User.update_one({"_id": db_user["_id"]}, {"$set": {"archived": False}})
+                users_collection.update_one({"_id": db_user["_id"]}, {"$set": {"archived": False}})
 
     except Exception as e:
         print(f"[sync_users_in_db_and_azure_ad] Error: {e}")
