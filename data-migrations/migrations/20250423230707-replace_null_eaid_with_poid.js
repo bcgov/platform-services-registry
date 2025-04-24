bd4fa7d62e798e72e9111285557530046e77c3d5
Chinedu Olebu <clashofdunes@gmail.com>
2025-04-24 15:09:07 -0700

    chore(5473): make virtual field mandatory

--- data-migrations/migrations/20250423230707-replace_null_eaid_with_poid.js
+++ data-migrations/migrations/20250423230707-replace_null_eaid_with_poid.js
@@ -3,7 +3,6 @@ export const up = async (db, client) => {
   const filteringRules = {
     $or: [{ expenseAuthorityId: null }, { expenseAuthorityId: { $exists: false } }, { expenseAuthorityId: '' }],
   };
-
   const updatePipeline = [
     {
       $set: {
@@ -11,23 +10,19 @@ export const up = async (db, client) => {
       },
     },
   ];
-
   const option = { session };
-  try {
-    await session.withTransaction(async () => {
-      const [productResult, requestDataResult] = await Promise.all([
-        db.collection('PublicCloudProduct').updateMany(filteringRules, updatePipeline, option),
-        db.collection('PublicCloudRequestData').updateMany(filteringRules, updatePipeline, option),
-      ]);
 
-      console.log(`Updated ${productResult.modifiedCount} documents in PublicCloudProduct`);
-      console.log(`Updated ${requestDataResult.modifiedCount} documents in PublicCloudRequestData`);
-    });
-  } catch (error) {
-    console.error('Transaction aborted due to error:', error);
-  } finally {
-    await session.endSession();
-  }
+  await session.withTransaction(async () => {
+    const [productResult, requestDataResult] = await Promise.all([
+      db.collection('PublicCloudProduct').updateMany(filteringRules, updatePipeline, option),
+      db.collection('PublicCloudRequestData').updateMany(filteringRules, updatePipeline, option),
+    ]);
+
+    console.log(`Updated ${productResult.modifiedCount} documents in PublicCloudProduct`);
+    console.log(`Updated ${requestDataResult.modifiedCount} documents in PublicCloudRequestData`);
+  });
+
+  session.endSession();
 };
 
 export const down = async (db, client) => {};
