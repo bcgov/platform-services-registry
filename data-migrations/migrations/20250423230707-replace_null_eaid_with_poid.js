ba1416783f95fc9787e624f294fd63ffe5584679
Chinedu Olebu <clashofdunes@gmail.com>
2025-04-24 14:21:39 -0700

    chore(5473): bundle operation in a transaction

--- data-migrations/migrations/20250423230707-replace_null_eaid_with_poid.js
+++ data-migrations/migrations/20250423230707-replace_null_eaid_with_poid.js
@@ -1,34 +1,42 @@
 export const up = async (db, client) => {
-  const PublicCloudProductProm = db.collection('PublicCloudProduct').updateMany(
-    {
-      $or: [{ expenseAuthorityId: null }, { expenseAuthorityId: { $exists: false } }, { expenseAuthorityId: '' }],
-    },
-    [
-      {
-        $set: {
-          expenseAuthorityId: '$projectOwnerId',
+  const session = client.startSession();
+  try {
+    await session.withTransaction(async () => {
+      const productResult = await db.collection('PublicCloudProduct').updateMany(
+        {
+          $or: [{ expenseAuthorityId: null }, { expenseAuthorityId: { $exists: false } }, { expenseAuthorityId: '' }],
         },
-      },
-    ],
-  );
+        [
+          {
+            $set: {
+              expenseAuthorityId: '$projectOwnerId',
+            },
+          },
+        ],
+        { session },
+      );
 
-  const PublicCloudRequestDataProm = db.collection('PublicCloudRequestData').updateMany(
-    {
-      $or: [{ expenseAuthorityId: null }, { expenseAuthorityId: { $exists: false } }, { expenseAuthorityId: '' }],
-    },
-    [
-      {
-        $set: {
-          expenseAuthorityId: '$projectOwnerId',
+      const requestDataResult = await db.collection('PublicCloudRequestData').updateMany(
+        {
+          $or: [{ expenseAuthorityId: null }, { expenseAuthorityId: { $exists: false } }, { expenseAuthorityId: '' }],
         },
-      },
-    ],
-  );
-
-  const [productResult, requestDataResult] = await Promise.all([PublicCloudProductProm, PublicCloudRequestDataProm]);
-
-  console.log(`Updated ${productResult.modifiedCount} documents in PublicCloudProject`);
-  console.log(`Updated ${requestDataResult.modifiedCount} documents in PublicCloudProject`);
+        [
+          {
+            $set: {
+              expenseAuthorityId: '$projectOwnerId',
+            },
+          },
+        ],
+        { session },
+      );
+      console.log(`Updated ${productResult.modifiedCount} documents in PublicCloudProject`);
+      console.log(`Updated ${requestDataResult.modifiedCount} documents in PublicCloudProject`);
+    });
+  } catch (error) {
+    console.error('Transaction aborted due to error:', error);
+  } finally {
+    await session.endSession();
+  }
 };
 
 export const down = async (db, client) => {};
