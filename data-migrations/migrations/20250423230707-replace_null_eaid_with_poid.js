b3b1e8ad2e9029b17191b55f905f4b30fa8c7d6e
Chinedu Olebu <clashofdunes@gmail.com>
2025-04-24 14:35:51 -0700

    chore(5473): make codebase more elegant

--- data-migrations/migrations/20250423230707-replace_null_eaid_with_poid.js
+++ data-migrations/migrations/20250423230707-replace_null_eaid_with_poid.js
@@ -1,36 +1,27 @@
 export const up = async (db, client) => {
   const session = client.startSession();
+  const filteringRules = {
+    $or: [{ expenseAuthorityId: null }, { expenseAuthorityId: { $exists: false } }, { expenseAuthorityId: '' }],
+  };
+
+  const updatePipeline = [
+    {
+      $set: {
+        expenseAuthorityId: '$projectOwnerId',
+      },
+    },
+  ];
+
+  const option = { session };
   try {
     await session.withTransaction(async () => {
-      const productResult = await db.collection('PublicCloudProduct').updateMany(
-        {
-          $or: [{ expenseAuthorityId: null }, { expenseAuthorityId: { $exists: false } }, { expenseAuthorityId: '' }],
-        },
-        [
-          {
-            $set: {
-              expenseAuthorityId: '$projectOwnerId',
-            },
-          },
-        ],
-        { session },
-      );
+      const [productResult, requestDataResult] = await Promise.all([
+        db.collection('PublicCloudProduct').updateMany(filteringRules, updatePipeline, option),
+        db.collection('PublicCloudRequestData').updateMany(filteringRules, updatePipeline, option),
+      ]);
 
-      const requestDataResult = await db.collection('PublicCloudRequestData').updateMany(
-        {
-          $or: [{ expenseAuthorityId: null }, { expenseAuthorityId: { $exists: false } }, { expenseAuthorityId: '' }],
-        },
-        [
-          {
-            $set: {
-              expenseAuthorityId: '$projectOwnerId',
-            },
-          },
-        ],
-        { session },
-      );
-      console.log(`Updated ${productResult.modifiedCount} documents in PublicCloudProject`);
-      console.log(`Updated ${requestDataResult.modifiedCount} documents in PublicCloudProject`);
+      console.log(`Updated ${productResult.modifiedCount} documents in PublicCloudProduct`);
+      console.log(`Updated ${requestDataResult.modifiedCount} documents in PublicCloudRequestData`);
     });
   } catch (error) {
     console.error('Transaction aborted due to error:', error);
