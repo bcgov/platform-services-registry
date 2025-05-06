4009a221c5cdc3626fa65220c89d59c642185be3
j.dev <junmin.dev@outlook.com>
2025-05-06 09:29:31 -0700

    feat(5592): disable Sysdig alerts

--- terraform/sysdig/silver-101ed4-team/alerts-dev.tf
+++ terraform/sysdig/silver-101ed4-team/alerts-dev.tf
@@ -2,7 +2,7 @@ resource "sysdig_monitor_alert_metric" "dev_pltsvc_db_pod_memory" {
   name        = "[DEV] Registry MongoDB - Memory"
   description = "DEV: Registry MongoDB pod is using 90% of the memory limit"
   severity    = 4
-  enabled     = true
+  enabled     = false
 
   metric                = "avg(avg(sysdig_container_memory_limit_used_percent)) > 90"
   trigger_after_minutes = 5
@@ -20,7 +20,7 @@ resource "sysdig_monitor_alert_metric" "dev_pltsvc_db_pod_cpu" {
   name        = "[DEV] Registry MongoDB - CPU"
   description = "DEV: Any of Registry MongoDB pod is using more than 80% of limited CPU"
   severity    = 4
-  enabled     = true
+  enabled     = false
 
   metric                = "avg(avg(sysdig_container_cpu_cores_used_percent)) > 80"
   trigger_after_minutes = 5
@@ -38,7 +38,7 @@ resource "sysdig_monitor_alert_metric" "dev_pltsvc_app_pod_memory" {
   name        = "[DEV] Registry App - Memory"
   description = "DEV: Registry app pod is using 90% of the memory limit"
   severity    = 4
-  enabled     = true
+  enabled     = false
 
   metric                = "avg(avg(sysdig_container_memory_limit_used_percent)) > 90"
   trigger_after_minutes = 5
@@ -56,7 +56,7 @@ resource "sysdig_monitor_alert_metric" "dev_pltsvc_app_pod_cpu" {
   name        = "[DEV] Registry App - CPU"
   description = "DEV: Any of Registry App pod is using more than 80% of limited CPU"
   severity    = 4
-  enabled     = true
+  enabled     = false
 
   metric                = "avg(avg(sysdig_container_cpu_cores_used_percent)) > 80"
   trigger_after_minutes = 5
