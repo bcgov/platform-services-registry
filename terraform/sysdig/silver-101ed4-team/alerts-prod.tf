c4a337e401cbc28738e394188188920acf905119
j.dev <junmin.dev@outlook.com>
2025-05-23 10:17:09 -0700

    feat(5689): update Sysdig Terraform package version

--- terraform/sysdig/silver-101ed4-team/alerts-prod.tf
+++ terraform/sysdig/silver-101ed4-team/alerts-prod.tf
@@ -1,71 +1,171 @@
-resource "sysdig_monitor_alert_metric" "prod_pltsvc_db_pod_memory" {
+resource "sysdig_monitor_alert_v2_metric" "prod_pltsvc_db_pod_memory" {
+  enabled     = false
   name        = "[Prod] Registry MongoDB - Memory"
   description = "Prod: Registry MongoDB pod is using 90% of the memory limit"
-  severity    = 2
-  enabled     = false
+  severity    = "low"
 
-  metric                = "avg(avg(sysdig_container_memory_limit_used_percent)) > 90"
-  trigger_after_minutes = 5
+  metric            = "sysdig_container_memory_limit_used_percent"
+  group_aggregation = "avg"
+  time_aggregation  = "avg"
+  operator          = ">"
+  threshold         = 90
 
-  scope                 = "kubernetes.cluster.name in (\"silver\") and kube_namespace_name in (\"101ed4-prod\") and kube_workload_name in (\"pltsvc-mongodb\")"
-  multiple_alerts_by    = []
-  notification_channels = [148742]
+  range_seconds = 300
+
+  scope {
+    label    = "kube_cluster_name"
+    operator = "in"
+    values   = ["silver"]
+  }
+
+  scope {
+    label    = "kube_namespace_name"
+    operator = "in"
+    values   = ["101ed4-prod"]
+  }
+
+  scope {
+    label    = "kube_workload_name"
+    operator = "in"
+    values   = ["pltsvc-mongodb"]
+  }
+
+  notification_channels {
+    id                     = 148742
+    renotify_every_minutes = 60
+  }
 
   custom_notification {
-    title = "{{__alert_name__}} is {{__alert_status__}}"
+    subject = "{{__alert_name__}} is {{__alert_status__}}"
   }
 }
 
-resource "sysdig_monitor_alert_metric" "prod_pltsvc_db_pod_cpu" {
+resource "sysdig_monitor_alert_v2_metric" "prod_pltsvc_db_pod_cpu" {
+  enabled     = false
   name        = "[Prod] Registry MongoDB - CPU"
   description = "Prod: Any of Registry MongoDB pod is using more than 80% of limited CPU"
-  severity    = 2
-  enabled     = false
 
-  metric                = "avg(avg(sysdig_container_cpu_cores_used_percent)) > 80"
-  trigger_after_minutes = 5
+  severity = "low"
 
-  scope                 = "kubernetes.cluster.name in (\"silver\") and kube_namespace_name in (\"101ed4-prod\") and kube_workload_name in (\"pltsvc-mongodb\")"
-  multiple_alerts_by    = []
-  notification_channels = [148742]
+  metric            = "sysdig_container_cpu_cores_used_percent"
+  group_aggregation = "avg"
+  time_aggregation  = "avg"
+  operator          = ">"
+  threshold         = 80
+
+  range_seconds = 300
+
+  scope {
+    label    = "kube_cluster_name"
+    operator = "in"
+    values   = ["silver"]
+  }
+
+  scope {
+    label    = "kube_namespace_name"
+    operator = "in"
+    values   = ["101ed4-prod"]
+  }
+
+  scope {
+    label    = "kube_workload_name"
+    operator = "in"
+    values   = ["pltsvc-mongodb"]
+  }
+
+  notification_channels {
+    id                     = 148742
+    renotify_every_minutes = 60
+  }
 
   custom_notification {
-    title = "{{__alert_name__}} is {{__alert_status__}}"
+    subject = "{{__alert_name__}} is {{__alert_status__}}"
   }
 }
 
-resource "sysdig_monitor_alert_metric" "prod_pltsvc_app_pod_memory" {
+resource "sysdig_monitor_alert_v2_metric" "prod_pltsvc_app_pod_memory" {
+  enabled     = false
   name        = "[Prod] Registry App - Memory"
   description = "Prod: Registry app pod is using 90% of the memory limit"
-  severity    = 2
-  enabled     = false
 
-  metric                = "avg(avg(sysdig_container_memory_limit_used_percent)) > 90"
-  trigger_after_minutes = 5
+  severity = "low"
 
-  scope                 = "kubernetes.cluster.name in (\"silver\") and kube_namespace_name in (\"101ed4-prod\") and kube_workload_name in (\"pltsvc-app\")"
-  multiple_alerts_by    = []
-  notification_channels = [148742]
+  metric            = "sysdig_container_memory_limit_used_percent"
+  group_aggregation = "avg"
+  time_aggregation  = "avg"
+  operator          = ">"
+  threshold         = 90
+
+  range_seconds = 300
+
+  scope {
+    label    = "kube_cluster_name"
+    operator = "in"
+    values   = ["silver"]
+  }
+
+  scope {
+    label    = "kube_namespace_name"
+    operator = "in"
+    values   = ["101ed4-prod"]
+  }
+
+  scope {
+    label    = "kube_workload_name"
+    operator = "in"
+    values   = ["pltsvc-app"]
+  }
+
+  notification_channels {
+    id                     = 148742
+    renotify_every_minutes = 60
+  }
 
   custom_notification {
-    title = "{{__alert_name__}} is {{__alert_status__}}"
+    subject = "{{__alert_name__}} is {{__alert_status__}}"
   }
 }
 
-resource "sysdig_monitor_alert_metric" "prod_pltsvc_app_pod_cpu" {
+resource "sysdig_monitor_alert_v2_metric" "prod_pltsvc_app_pod_cpu" {
+  enabled     = false
   name        = "[Prod] Registry App - CPU"
   description = "Prod: Any of Registry App pod is using more than 80% of limited CPU"
-  severity    = 2
-  enabled     = false
 
-  metric                = "avg(avg(sysdig_container_cpu_cores_used_percent)) > 80"
-  trigger_after_minutes = 5
 
-  scope                 = "kubernetes.cluster.name in (\"silver\") and kube_namespace_name in (\"101ed4-prod\") and kube_workload_name in (\"pltsvc-app\")"
-  multiple_alerts_by    = []
-  notification_channels = [148742]
+  severity = "low"
+
+  metric            = "sysdig_container_cpu_cores_used_percent"
+  group_aggregation = "avg"
+  time_aggregation  = "avg"
+  operator          = ">"
+  threshold         = 80
+
+  range_seconds = 300
+
+  scope {
+    label    = "kube_cluster_name"
+    operator = "in"
+    values   = ["silver"]
+  }
+
+  scope {
+    label    = "kube_namespace_name"
+    operator = "in"
+    values   = ["101ed4-prod"]
+  }
+
+  scope {
+    label    = "kube_workload_name"
+    operator = "in"
+    values   = ["pltsvc-app"]
+  }
+
+  notification_channels {
+    id                     = 148742
+    renotify_every_minutes = 60
+  }
 
   custom_notification {
-    title = "{{__alert_name__}} is {{__alert_status__}}"
+    subject = "{{__alert_name__}} is {{__alert_status__}}"
   }
 }
--- terraform/sysdig/silver-101ed4-team/config.tf
+++ terraform/sysdig/silver-101ed4-team/config.tf
@@ -2,9 +2,10 @@ terraform {
   required_version = "1.12.0"
 
   required_providers {
+    # See https://registry.terraform.io/providers/sysdiglabs/sysdig/latest/docs
     sysdig = {
       source  = "sysdiglabs/sysdig"
-      version = ">= 1.20.0"
+      version = ">= 1.56.3"
     }
   }
 }
