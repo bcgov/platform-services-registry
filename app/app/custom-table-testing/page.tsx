2ef19207440b854619f0891194cd49c41f86c415
Chinedu Olebu <clashofdunes@gmail.com>
2025-06-12 09:36:05 -0700

    chore(5574): remove session

--- app/app/custom-table-testing/page.tsx
+++ app/app/custom-table-testing/page.tsx
@@ -4,7 +4,6 @@ import { Tooltip } from '@mantine/core';
 import { MonthPickerInput } from '@mantine/dates';
 import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
-import { Session } from 'next-auth';
 import { useState } from 'react';
 import LoadingBox from '@/components/generic/LoadingBox';
 import GenericTable from '@/components/generic/table/GenericTable';
@@ -14,7 +13,7 @@ import { getTableData } from '@/helpers/product';
 import { getMonthlyCosts } from '@/services/backend/private-cloud/products';
 import { tableDataBodySchema, tableDataSummaryBodySchema } from '@/validation-schemas';
 
-export default function CustomTableTesting({ session }: { session: Session }) {
+export default function CustomTableTesting() {
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
 
   const licencePlate = 'caf09b';
