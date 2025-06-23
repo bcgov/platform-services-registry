e1d4df1a42542f97cdc06e844ab25dc9cfbcaba1
Chinedu Olebu <clashofdunes@gmail.com>
2025-06-23 16:58:14 -0700

    chore(5877): increase font size

--- app/components/shared/SupportContact.tsx
+++ app/components/shared/SupportContact.tsx
@@ -3,7 +3,7 @@ import HookFormMaskInput from '@/components/generic/input/HookFormMaskInput';
 export default function SupportContact({ disabled }: { disabled?: boolean }) {
   return (
     <div className="mt-6 flex flex-col flex-start text-input">
-      <h3 className="text-base 2xl:text-sm font-semibold leading-7">After-Hours support contact (optional)</h3>
+      <h3 className="text-base lg:text-lg font-semibold leading-7">After-Hours support contact (optional)</h3>
       <p className="my-4 text-base leading-6 text-gray-600">
         For Business Mission Critical Applications Only. You can specify a phone number of your team member who should
         be contacted by the BC Government&apos;s Service Desk (7-7000) if an issue is reported for your product outside
