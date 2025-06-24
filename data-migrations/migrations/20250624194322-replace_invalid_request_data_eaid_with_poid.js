eb8811c63d74d8bd79501962285c5e847e5bdb5d
Kolezhanchik <zhanna.kolesnyk@gov.bc.ca>
2025-06-24 14:35:32 -0700

    chore(5930): reduce cognitive complexity of up() by extracting helper functions

--- data-migrations/migrations/20250624194322-replace_invalid_request_data_eaid_with_poid.js
+++ data-migrations/migrations/20250624194322-replace_invalid_request_data_eaid_with_poid.js
@@ -4,6 +4,35 @@ export const up = async (db, client) => {
   let skip = 0;
   let totalReplaced = 0;
 
+  const userExists = async (id) => {
+    if (!id) return false;
+    return await db.collection('User').findOne({ _id: id });
+  };
+
+  const handleRequest = async (requestData) => {
+    const { _id, expenseAuthorityId, projectOwnerId } = requestData;
+
+    if (!expenseAuthorityId) {
+      console.warn(`Request ${_id} has no expenseAuthorityId`);
+      return 0;
+    }
+
+    const expenseUser = await userExists(expenseAuthorityId);
+    if (expenseUser) return 0;
+
+    console.warn(`User ${expenseAuthorityId} not found for request ${_id}`);
+
+    const projectUser = await userExists(projectOwnerId);
+    if (!projectUser) {
+      console.warn(`Project owner ${projectOwnerId} also not found for request ${_id}`);
+      return 0;
+    }
+
+    await collection.updateOne({ _id }, { $set: { expenseAuthorityId: projectOwnerId } });
+    console.log(`Replaced expenseAuthorityId with projectOwnerId for request ${_id}`);
+    return 1;
+  };
+
   while (true) {
     const batch = await collection
       .find({}, { projection: { _id: 1, expenseAuthorityId: 1, projectOwnerId: 1 } })
@@ -14,33 +43,11 @@ export const up = async (db, client) => {
     if (batch.length === 0) break;
 
     for (const requestData of batch) {
-      const { _id, expenseAuthorityId, projectOwnerId } = requestData;
-
-      if (!expenseAuthorityId) {
-        console.warn(`Request ${_id} has no expenseAuthorityId`);
-        continue;
-      }
-
-      const userExists = await db.collection('User').findOne({ _id: expenseAuthorityId });
-
-      if (!userExists) {
-        console.warn(`User ${expenseAuthorityId} not found for request ${_id}`);
-
-        if (projectOwnerId) {
-          const projectOwnerExists = await db.collection('User').findOne({ _id: projectOwnerId });
-
-          if (projectOwnerExists) {
-            await collection.updateOne({ _id }, { $set: { expenseAuthorityId: projectOwnerId } });
-            console.log(`Replaced expenseAuthorityId with projectOwnerId for request ${_id}`);
-            totalReplaced++;
-          } else {
-            console.warn(`Project owner ${projectOwnerId} also not found for request ${_id}`);
-          }
-        }
-      }
+      totalReplaced += await handleRequest(requestData);
     }
+
     skip += batch.length;
   }
 
-  console.log(`Updated  ${totalReplaced} publicCloudRequestData - expenseAuthorityId replaced with projectOwnerId:`);
+  console.log(`Updated ${totalReplaced} publicCloudRequestData - expenseAuthorityId replaced with projectOwnerId.`);
 };
