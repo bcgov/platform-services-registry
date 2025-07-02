79e6841d5da6d4aa3a568e78974cb205e830817f
j.dev <junmin.dev@outlook.com>
2025-07-02 13:27:54 -0700

    refactor(5845): clean up environment variables about MS graph

--- app/app/api/token-check/validations/ms-graph-service-account-credentials.ts
+++ app/app/api/token-check/validations/ms-graph-service-account-credentials.ts
@@ -1,9 +1,9 @@
-import { MS_GRAPH_API_CLIENT_ID, MS_GRAPH_API_CLIENT_SECRET, MS_GRAPH_API_AUTHORITY } from '@/config';
+import { MS_GRAPH_API_TOKEN_ENDPOINT, MS_GRAPH_API_CLIENT_ID, MS_GRAPH_API_CLIENT_SECRET } from '@/config';
 import { validateClientCredentials } from '@/utils/node/oauth2';
 
 export async function validateMsGraphServiceAccountCredentials() {
   const isValid = await validateClientCredentials(
-    `${MS_GRAPH_API_AUTHORITY}/oauth2/v2.0/token`,
+    MS_GRAPH_API_TOKEN_ENDPOINT,
     MS_GRAPH_API_CLIENT_ID,
     MS_GRAPH_API_CLIENT_SECRET,
     'https://graph.microsoft.com/.default',
