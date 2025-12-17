c5cf50e3547478fe2ea4689fb63ae8c870b2f7a7
Zhanna <65827870+Kolezhanchik@users.noreply.github.com>
2025-12-16 16:20:43 -0800

    Revert "Fix/2590"

--- app/emails/_components/layout/Layout.tsx
+++ app/emails/_components/layout/Layout.tsx
@@ -8,56 +8,56 @@ import Header from './Header';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Html>
-      <Head>
-        <Font
-          fontFamily="BCSans"
-          fallbackFontFamily="Verdana"
-          webFont={{
-            url: `${BASE_URL}/fonts/bcsans-regular.woff`,
-            format: 'woff',
-          }}
-          fontWeight={400}
-          fontStyle="normal"
-        />
-        <Font
-          fontFamily="BCSans"
-          fallbackFontFamily="Verdana"
-          webFont={{
-            url: `${BASE_URL}/fonts/bcsans-italic.woff`,
-            format: 'woff',
-          }}
-          fontWeight={400}
-          fontStyle="italic"
-        />
-        <Font
-          fontFamily="BCSans"
-          fallbackFontFamily="Verdana"
-          webFont={{
-            url: `${BASE_URL}/fonts/bcsans-bold.woff`,
-            format: 'woff',
-          }}
-          fontWeight={700}
-          fontStyle="normal"
-        />
-        <Font
-          fontFamily="BCSans"
-          fallbackFontFamily="Verdana"
-          webFont={{
-            url: `${BASE_URL}/fonts/bcsans-bold-italic.woff`,
-            format: 'woff',
-          }}
-          fontWeight={700}
-          fontStyle="italic"
-        />
-      </Head>
-      <Body className="bg-grey my-auto mx-auto font-sans text-xs text-darkergrey">
-        <Tailwind config={tailwindConfig}>
-          <div className="border border-solid border-[#eaeaea] rounded-sm my-4 mx-auto p-4 max-w-xl bg-grey">
-            <Header />
+      <Tailwind config={tailwindConfig}>
+        <Head>
+          <Font
+            fontFamily="BCSans"
+            fallbackFontFamily="Verdana"
+            webFont={{
+              url: `${BASE_URL}/fonts/bcsans-regular.woff`,
+              format: 'woff',
+            }}
+            fontWeight={400}
+            fontStyle="normal"
+          />
+          <Font
+            fontFamily="BCSans"
+            fallbackFontFamily="Verdana"
+            webFont={{
+              url: `${BASE_URL}/fonts/bcsans-italic.woff`,
+              format: 'woff',
+            }}
+            fontWeight={400}
+            fontStyle="italic"
+          />
+          <Font
+            fontFamily="BCSans"
+            fallbackFontFamily="Verdana"
+            webFont={{
+              url: `${BASE_URL}/fonts/bcsans-bold.woff`,
+              format: 'woff',
+            }}
+            fontWeight={700}
+            fontStyle="normal"
+          />
+          <Font
+            fontFamily="BCSans"
+            fallbackFontFamily="Verdana"
+            webFont={{
+              url: `${BASE_URL}/fonts/bcsans-bold-italic.woff`,
+              format: 'woff',
+            }}
+            fontWeight={700}
+            fontStyle="italic"
+          />
+        </Head>
+        <div className="border border-solid border-[#eaeaea] rounded-sm my-4 mx-auto p-4 max-w-xl">
+          <Header />
+          <Body className="bg-white my-auto mx-auto font-sans text-xs text-darkergrey">
             <div className="m-12">{children}</div>
-          </div>
-        </Tailwind>
-      </Body>
+          </Body>
+        </div>
+      </Tailwind>
     </Html>
   );
 }
