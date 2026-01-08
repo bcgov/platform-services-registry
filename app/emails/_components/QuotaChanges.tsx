77d2b6bd8750a5c3bac30e9c700abf9fb00da941
Kolezhanchik <zhanna.kolesnyk@gov.bc.ca>
2026-01-08 14:53:06 -0800

    chore(2464): update public emails links, fix hooking react-email sandbox sync for build and preview

--- app/emails/_components/QuotaChanges.tsx
+++ app/emails/_components/QuotaChanges.tsx
@@ -1,5 +1,5 @@
 import { Heading, Link, Text } from '@react-email/components';
-import { ResourceRequests } from '@/prisma/client';
+import type { ResourceRequests } from '@/prisma/client';
 
 interface QuotaChangesProps {
   licencePlate: string;
