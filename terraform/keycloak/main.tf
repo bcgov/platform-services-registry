3f7f706692ee98c524e3489852cf8d00cb82deab
zhanna <kolezhannochka@gmail.com>
2026-04-02 15:04:20 -0700

    fix(0000): use realm endpoint in keycloak url to avoid redirect during provider initialization

--- terraform/keycloak/main.tf
+++ terraform/keycloak/main.tf
@@ -2,22 +2,19 @@ module "keycloak_dev" {
   source        = "./dev"
   client_id     = var.dev_client_id
   client_secret = var.dev_client_secret
-  url           = "https://dev.loginproxy.gov.bc.ca"
-  base_path     = "/auth"
+  url           = "https://dev.loginproxy.gov.bc.ca/auth/realms/platform-services"
 }
 
 module "keycloak_test" {
   source        = "./test"
   client_id     = var.test_client_id
   client_secret = var.test_client_secret
-  url           = "https://test.loginproxy.gov.bc.ca"
-  base_path     = "/auth"
+  url           = "https://test.loginproxy.gov.bc.ca/auth/realms/platform-services"
 }
 
 module "keycloak_prod" {
   source        = "./prod"
   client_id     = var.test_client_id
   client_secret = var.prod_client_secret
-  url           = "https://loginproxy.gov.bc.ca"
-  base_path     = "/auth"
+  url           = "https://loginproxy.gov.bc.ca/auth/realms/platform-services"
 }
