7b343842653baae0c51a560ecff97d267a1d4ab3
zhanna <kolezhannochka@gmail.com>
2026-08-19 15:51:45 -0700

    chore(2951):  include GPU in quota upgrade detection

--- app/helpers/quota-change.ts
+++ app/helpers/quota-change.ts
@@ -10,15 +10,19 @@ export const isQuotaUpgrade = (oldval: ResourceRequestsEnv, newval: ResourceRequ
     oldval.development.cpu < newval.development.cpu ||
     oldval.development.memory < newval.development.memory ||
     oldval.development.storage < newval.development.storage ||
+    (oldval.development.gpu ?? 0) < (newval.development.gpu ?? 0) ||
     oldval.test.cpu < newval.test.cpu ||
     oldval.test.memory < newval.test.memory ||
     oldval.test.storage < newval.test.storage ||
+    (oldval.test.gpu ?? 0) < (newval.test.gpu ?? 0) ||
     oldval.production.cpu < newval.production.cpu ||
     oldval.production.memory < newval.production.memory ||
     oldval.production.storage < newval.production.storage ||
+    (oldval.production.gpu ?? 0) < (newval.production.gpu ?? 0) ||
     oldval.tools.cpu < newval.tools.cpu ||
     oldval.tools.memory < newval.tools.memory ||
-    oldval.tools.storage < newval.tools.storage
+    oldval.tools.storage < newval.tools.storage ||
+    (oldval.tools.gpu ?? 0) < (newval.tools.gpu ?? 0)
   );
 };
 
